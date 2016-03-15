1d2b4c11873d097f1631fc3bc29d5d4e73c7bb26
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-03-15 15:04:14 +0000

    Add unique id to all http requests

---
+++ server/api/uniq.go
@@ -0,0 +1,47 @@
+// Copyright © 2016 Abcum Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"github.com/labstack/echo"
+
+	"github.com/abcum/surreal/util/uuid"
+)
+
+// UniqOpts defines options for the Uniq middleware
+type UniqOpts struct {
+	HeaderKey string
+}
+
+// Uniq defines middleware for assigning a unique request id
+func Uniq(opts *UniqOpts) echo.MiddlewareFunc {
+	return func(h echo.HandlerFunc) echo.HandlerFunc {
+		return func(c *echo.Context) error {
+
+			// Set default values for opts.HeaderKey
+			headerKey := opts.HeaderKey
+			if headerKey == "" {
+				headerKey = "Request-Id"
+			}
+
+			id := uuid.NewV4().String()
+
+			c.Response().Header().Set(headerKey, id)
+
+			return h(c)
+
+		}
+	}
+}
