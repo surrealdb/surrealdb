248bc576ddaf9a24d3a06ced8f986a472980d898
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-04-04 10:14:16 +0100

    Add other http endpoints

--- web/routes.go
+++ web/routes.go
@@ -40,131 +40,135 @@ func routes(s *fibre.Fibre) {
 
 	s.Rpc("/rpc", &rpc{})
 
+	// --------------------------------------------------
+	// Endpoints for submitting sql queries
+	// --------------------------------------------------
+
 	s.Options("/sql", func(c *fibre.Context) error {
 		return c.Code(200)
 	})
+
+	s.Post("/sql", func(c *fibre.Context) error {
+		res, err := db.Execute(c, c.Request().Body)
+		if err != nil {
+			return fibre.NewHTTPError(400)
+		}
+		return c.Send(200, res)
+	})
+
+	// --------------------------------------------------
+	// Endpoints for manipulating multiple records
+	// --------------------------------------------------
 
 	s.Options("/key/:class", func(c *fibre.Context) error {
 		return c.Code(200)
 	})
 
+	s.Get("/key/:class", func(c *fibre.Context) error {
+		sql := db.Prepare("SELECT * FROM %v", c.Param("class"))
+		res, err := db.Execute(c, sql)
+		if err != nil {
+			return fibre.NewHTTPError(400)
+		}
+		return output(c, res[0])
+	})
+
+	s.Post("/key/:class", func(c *fibre.Context) error {
+		sql := db.Prepare("CREATE %v CONTENT %v RETURN AFTER", c.Param("class"), string(c.Body()))
+		res, err := db.Execute(c, sql)
+		if err != nil {
+			return fibre.NewHTTPError(400)
+		}
+		return output(c, res[0])
+	})
+
+	s.Delete("/key/:class", func(c *fibre.Context) error {
+		sql := db.Prepare("DELETE FROM %v", c.Param("class"))
+		res, err := db.Execute(c, sql)
+		if err != nil {
+			return fibre.NewHTTPError(400)
+		}
+		return output(c, res[0])
+	})
+
+	// --------------------------------------------------
+	// Endpoints for manipulating a single record
+	// --------------------------------------------------
+
 	s.Options("/key/:class/:id", func(c *fibre.Context) error {
 		return c.Code(200)
 	})
 
-	s.Post("/sql", func(c *fibre.Context) error {
-
-		res, err := db.Execute(c, c.Request().Body)
-		if err != nil {
-			return fibre.NewHTTPError(400)
-		}
-
-		return c.Send(200, res)
-
-	})
-
-	s.Get("/key/:class", func(c *fibre.Context) error {
-
-		sql := db.Prepare("SELECT * FROM %v", c.Param("class"))
-
-		res, err := db.Execute(c, sql)
-		if err != nil {
-			return fibre.NewHTTPError(400)
-		}
-
-		return output(c, res[0])
-
-	})
-
 	s.Get("/key/:class/:id", func(c *fibre.Context) error {
-
 		sql := db.Prepare("SELECT * FROM @%v:%v", c.Param("class"), c.Param("id"))
-
 		res, err := db.Execute(c, sql)
 		if err != nil {
 			return fibre.NewHTTPError(400)
 		}
-
 		return output(c, res[0])
-
 	})
 
 	s.Put("/key/:class/:id", func(c *fibre.Context) error {
-
 		sql := db.Prepare("CREATE @%v:%v CONTENT %v RETURN AFTER", c.Param("class"), c.Param("id"), string(c.Body()))
-
 		res, err := db.Execute(c, sql)
 		if err != nil {
 			return fibre.NewHTTPError(400)
 		}
-
 		return output(c, res[0])
-
 	})
 
 	s.Post("/key/:class/:id", func(c *fibre.Context) error {
-
 		sql := db.Prepare("UPDATE @%v:%v CONTENT %v RETURN AFTER", c.Param("class"), c.Param("id"), string(c.Body()))
-
 		res, err := db.Execute(c, sql)
 		if err != nil {
 			return fibre.NewHTTPError(400)
 		}
-
 		return output(c, res[0])
-
 	})
 
 	s.Patch("/key/:class/:id", func(c *fibre.Context) error {
-
 		sql := db.Prepare("MODIFY @%v:%v DIFF %v RETURN AFTER", c.Param("class"), c.Param("id"), string(c.Body()))
-
 		res, err := db.Execute(c, sql)
 		if err != nil {
 			return fibre.NewHTTPError(400)
 		}
-
 		return output(c, res[0])
-
 	})
 
 	s.Trace("/key/:class/:id", func(c *fibre.Context) error {
-
 		sql := db.Prepare("SELECT HISTORY FROM @%v:%v", c.Param("class"), c.Param("id"))
-
 		res, err := db.Execute(c, sql)
 		if err != nil {
 			return fibre.NewHTTPError(400)
 		}
-
 		return output(c, res[0])
-
 	})
 
 	s.Delete("/key/:class/:id", func(c *fibre.Context) error {
-
 		sql := db.Prepare("DELETE @%v:%v", c.Param("class"), c.Param("id"))
-
 		res, err := db.Execute(c, sql)
 		if err != nil {
 			return fibre.NewHTTPError(400)
 		}
-
 		return output(c, res[0])
+	})
 
+	s.Connect("/key/:class/:id/:type/:fk", func(c *fibre.Context) error {
+		sql := db.Prepare("RELATE %v FROM @%v:%v TO %v", c.Param("type"), c.Param("class"), c.Param("id"), c.Param("fk"))
+		res, err := db.Execute(c, sql)
+		if err != nil {
+			return fibre.NewHTTPError(400)
+		}
+		return output(c, res[0])
 	})
 
 	s.Get("/key/:class/:id/:edge/:type", func(c *fibre.Context) error {
-
 		sql := db.Prepare("SELECT :%v/:%v @%v:%v", c.Param("edge"), c.Param("type"), c.Param("class"), c.Param("id"))
-
 		res, err := db.Execute(c, sql)
 		if err != nil {
 			return fibre.NewHTTPError(400)
 		}
-
 		return output(c, res[0])
-
 	})
 
 }
