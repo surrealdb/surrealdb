f0108df6b45f9522395e6ccc9fab414871c9a0bc
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-04-08 15:16:16 +0100

    Add go comments to log package

--- log/log.go
+++ log/log.go
@@ -33,112 +33,139 @@ func init() {
 	log = New()
 }
 
+// Instance returns the underlying logger instance
 func Instance() *logrus.Logger {
 	return log.Logger
 }
 
-func Debug(v ...interface{}) {
-	log.Debug(v...)
-}
-
-func Debugf(format string, v ...interface{}) {
-	log.Debugf(format, v...)
-}
-
-func Debugln(v ...interface{}) {
-	log.Debugln(v...)
-}
-
-func Error(v ...interface{}) {
-	log.Error(v...)
-}
-
-func Errorf(format string, v ...interface{}) {
-	log.Errorf(format, v...)
-}
-
-func Errorln(v ...interface{}) {
-	log.Errorln(v...)
-}
-
-func Fatal(v ...interface{}) {
-	log.Fatal(v...)
-}
-
-func Fatalf(format string, v ...interface{}) {
-	log.Fatalf(format, v...)
-}
-
-func Fatalln(v ...interface{}) {
-	log.Fatalln(v...)
-}
-
-func Info(v ...interface{}) {
-	log.Info(v...)
-}
-
-func Infof(format string, v ...interface{}) {
-	log.Infof(format, v...)
-}
-
-func Infoln(v ...interface{}) {
-	log.Infoln(v...)
-}
-
-func Panic(v ...interface{}) {
-	log.Panic(v...)
-}
-
-func Panicf(format string, v ...interface{}) {
-	log.Panicf(format, v...)
-}
-
-func Panicln(v ...interface{}) {
-	log.Panicln(v...)
-}
-
-func Print(v ...interface{}) {
-	log.Print(v...)
-}
-
-func Printf(format string, v ...interface{}) {
-	log.Printf(format, v...)
-}
-
-func Println(v ...interface{}) {
-	log.Println(v...)
-}
-
-func Warn(v ...interface{}) {
-	log.Warn(v...)
-}
-
-func Warnf(format string, v ...interface{}) {
-	log.Warnf(format, v...)
-}
-
-func Warnln(v ...interface{}) {
-	log.Warnln(v...)
-}
-
-func WithField(key string, value interface{}) *logrus.Entry {
-	return log.WithField(key, value)
-}
-
-func WithFields(fields map[string]interface{}) *logrus.Entry {
-	return log.WithFields(fields)
-}
-
+// SetLevel sets the level of the standard logger
 func SetLevel(v string) {
 	log.SetLevel(v)
 }
 
+// SetFormat sets the format of the standard logger
 func SetFormat(v string) {
 	log.SetFormat(v)
 }
 
+// SetOutput sets the output of the standard logger
 func SetOutput(v string) {
 	log.SetOutput(v)
+}
+
+// Debug logs a message at level Debug on the standard logger.
+func Debug(v ...interface{}) {
+	log.Debug(v...)
+}
+
+// Debugf logs a message at level Debug on the standard logger.
+func Debugf(format string, v ...interface{}) {
+	log.Debugf(format, v...)
+}
+
+// Debugln logs a message at level Debug on the standard logger.
+func Debugln(v ...interface{}) {
+	log.Debugln(v...)
+}
+
+// Error loggs a message at level Error on the standard logger.
+func Error(v ...interface{}) {
+	log.Error(v...)
+}
+
+// Errorf loggs a message at level Error on the standard logger.
+func Errorf(format string, v ...interface{}) {
+	log.Errorf(format, v...)
+}
+
+// Errorln loggs a message at level Error on the standard logger.
+func Errorln(v ...interface{}) {
+	log.Errorln(v...)
+}
+
+// Fatal loggs a message at level Fatal on the standard logger.
+func Fatal(v ...interface{}) {
+	log.Fatal(v...)
+}
+
+// Fatalf loggs a message at level Fatal on the standard logger.
+func Fatalf(format string, v ...interface{}) {
+	log.Fatalf(format, v...)
+}
+
+// Fatalln loggs a message at level Fatal on the standard logger.
+func Fatalln(v ...interface{}) {
+	log.Fatalln(v...)
+}
+
+// Info loggs a message at level Info on the standard logger.
+func Info(v ...interface{}) {
+	log.Info(v...)
+}
+
+// Infof loggs a message at level Info on the standard logger.
+func Infof(format string, v ...interface{}) {
+	log.Infof(format, v...)
+}
+
+// Infoln loggs a message at level Info on the standard logger.
+func Infoln(v ...interface{}) {
+	log.Infoln(v...)
+}
+
+// Panic loggs a message at level Panic on the standard logger.
+func Panic(v ...interface{}) {
+	log.Panic(v...)
+}
+
+// Panicf loggs a message at level Panic on the standard logger.
+func Panicf(format string, v ...interface{}) {
+	log.Panicf(format, v...)
+}
+
+// Panicln loggs a message at level Panic on the standard logger.
+func Panicln(v ...interface{}) {
+	log.Panicln(v...)
+}
+
+// Print loggs a message at level Print on the standard logger.
+func Print(v ...interface{}) {
+	log.Print(v...)
+}
+
+// Printf loggs a message at level Print on the standard logger.
+func Printf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+// Println loggs a message at level Print on the standard logger.
+func Println(v ...interface{}) {
+	log.Println(v...)
+}
+
+// Warn loggs a message at level Warn on the standard logger.
+func Warn(v ...interface{}) {
+	log.Warn(v...)
+}
+
+// Warnf loggs a message at level Warn on the standard logger.
+func Warnf(format string, v ...interface{}) {
+	log.Warnf(format, v...)
+}
+
+// Warnln loggs a message at level Warn on the standard logger.
+func Warnln(v ...interface{}) {
+	log.Warnln(v...)
+}
+
+// WithField prepares a log entry with a single data field.
+func WithField(key string, value interface{}) *logrus.Entry {
+	return log.WithField(key, value)
+}
+
+// WithFields prepares a log entry with multiple data fields.
+func WithFields(fields map[string]interface{}) *logrus.Entry {
+	return log.WithFields(fields)
 }
 
 // New returns a new Logger instance.
@@ -156,7 +183,7 @@ func New() *Logger {
 	}
 }
 
-// SetLevel sets the logging level.
+// SetLevel sets the logging level of the logger instance.
 func (l *Logger) SetLevel(v string) {
 	switch v {
 	case "debug", "DEBUG":
@@ -174,7 +201,7 @@ func (l *Logger) SetLevel(v string) {
 	}
 }
 
-// SetFormat sets the logging format.
+// SetFormat sets the logging format of the logger instance.
 func (l *Logger) SetFormat(v string) {
 	switch v {
 	case "json":
@@ -186,7 +213,7 @@ func (l *Logger) SetFormat(v string) {
 	}
 }
 
-// SetOutput sets the logging output.
+// SetOutput sets the logging output of the logger instance.
 func (l *Logger) SetOutput(v string) {
 	switch v {
 	case "stdout":
