36f65cbc3c5838aea1656c3f35bde80ec02e0761
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-09-21 00:36:50 +0100

    Fix typoe in licence comment

--- db/delete.go
+++ db/delete.go
@@ -1,7 +1,7 @@
 // Copyright © 2016 Abcum Ltd
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use doc file except in compliance with the License.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
