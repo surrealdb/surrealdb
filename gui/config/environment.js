f2f72ccc403cb440ed5f4d0ada64252b49151ad2
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-10-01 17:00:40 +0100

    Upgrade to ember-cli@2.8.0

--- gui/config/environment.js
+++ gui/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'gui',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -29,7 +29,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
