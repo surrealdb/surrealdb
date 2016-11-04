df7ee71cf69b602479e7dca121f48357f8bb7d46
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-11-04 11:25:00 +0000

    Remove unused import + export routes

--- web/routes.go
+++ web/routes.go
@@ -60,18 +60,6 @@ func routes(s *fibre.Fibre) {
 	})
 
 	// --------------------------------------------------
-	// Endpoint for database backups
-	// --------------------------------------------------
-
-	s.Get("/import", func(c *fibre.Context) error {
-		return c.Code(200)
-	})
-
-	s.Get("/export", func(c *fibre.Context) error {
-		return c.Code(200)
-	})
-
-	// --------------------------------------------------
 	// Endpoints for submitting rpc queries
 	// --------------------------------------------------
 
