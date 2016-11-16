d30975d549a2132fbafc29ce8c18047709c522f5
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-11-16 17:31:25 +0000

    Save with go fmt simplified

--- web/err.go
+++ web/err.go
@@ -64,73 +64,73 @@ type err struct {
 
 var errs = map[int]*err{
 
-	200: &err{
+	200: {
 		Code:        200,
 		Details:     "Information",
 		Description: "Visit the documentation for details on accessing the api.",
 	},
 
-	400: &err{
+	400: {
 		Code:        400,
 		Details:     "Request problems detected",
 		Description: "There is a problem with your request. Ensure that the request is valid.",
 	},
 
-	401: &err{
+	401: {
 		Code:        401,
 		Details:     "Authentication failed",
 		Description: "Your authentication details are invalid. Reauthenticate using a valid token.",
 	},
 
-	403: &err{
+	403: {
 		Code:        403,
 		Details:     "Request resource forbidden",
 		Description: "Your request was forbidden. Perhaps you don't have the necessary permissions to access this resource.",
 	},
 
-	404: &err{
+	404: {
 		Code:        404,
 		Details:     "Request resource not found",
 		Description: "The requested resource does not exist. Check that you have entered the url correctly.",
 	},
 
-	405: &err{
+	405: {
 		Code:        405,
 		Details:     "This method is not allowed",
 		Description: "The requested http method is not allowed for this resource. Refer to the documentation for allowed methods.",
 	},
 
-	409: &err{
+	409: {
 		Code:        409,
 		Details:     "Request conflict detected",
 		Description: "The request could not be processed because of a conflict in the request.",
 	},
 
-	413: &err{
+	413: {
 		Code:        413,
 		Details:     "Request content length too large",
 		Description: "All requests to the database must not exceed the predefined content length.",
 	},
 
-	415: &err{
+	415: {
 		Code:        415,
 		Details:     "Unsupported content type requested",
 		Description: "The request needs to adhere to certain constraints. Check your request settings and try again.",
 	},
 
-	422: &err{
+	422: {
 		Code:        422,
 		Details:     "Request problems detected",
 		Description: "There is a problem with your request. The request appears to contain invalid data.",
 	},
 
-	426: &err{
+	426: {
 		Code:        426,
 		Details:     "Upgrade required",
 		Description: "There is a problem with your request. The request is expected to upgrade to a websocket connection.",
 	},
 
-	500: &err{
+	500: {
 		Code:    500,
 		Details: "There was a problem with our servers, and we have been notified",
 	},
