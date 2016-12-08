fa40f52302dfc5799bfce6d7b8f0aaf96f79936d
Tobie Morgan Hitchcock <tobie@abcum.com>
2016-12-08 22:04:22 +0000

    Place build instruction before package name

--- db/gen.go
+++ db/gen.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package db
-
 //+build generate
+
+package db
 
 //go:generate codecgen -o db.gen.go db.go
--- sql/gen.go
+++ sql/gen.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sql
-
 //+build generate
+
+package sql
 
 //go:generate codecgen -o ast.gen.go ast.go
