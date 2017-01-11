e3c472b31a6e48a9310c52efdbdc6013856a3160
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-01-11 11:22:17 +0000

    Remove incorrect build tags

--- db/gen.go
+++ db/gen.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//+build generate
-
 package db
 
 //go:generate codecgen -o db.gen.go db.go
