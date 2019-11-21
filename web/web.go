02442fb6d9bbeedb2ab1295c95884f61424e11f6
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-11-21 08:55:14 +0000

    Override default timeout durations

--- web/web.go
+++ web/web.go
@@ -31,6 +31,8 @@ func Setup(opts *cnf.Options) (err error) {
 
 	routes(s)
 	s.SetName("web")
+	s.SetReadTimeout("5s")
+	s.SetWriteTimeout("60s")
 	s.SetIdleTimeout("650s")
 	s.SetHTTPErrorHandler(errors)
 	s.Logger().SetLogger(log.Instance())
