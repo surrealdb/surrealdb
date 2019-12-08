84a42c91a40ed41d11bcfc65ff55f6f5b63f42a0
Tobie Morgan Hitchcock <tobie@abcum.com>
2019-12-08 14:46:30 +0000

    Upload sha256 sum of files

--- build/windows.sh
+++ build/windows.sh
@@ -2,9 +2,15 @@
 
 VERS=$(git describe --tags --abbrev=0)
 NAME=surreal-${VERS}.windows-amd64
+HASH=${NAME}.txt
 FILE=${NAME}.tgz
 
 GOOS=windows GOARCH=amd64 go build -v -ldflags "$(bash build/flags.sh)"
 tar -zcvf $FILE -s "#^#${NAME}/#" surreal
+
+echo $(shasum -a 256 $FILE | cut -f1 -d' ') > $HASH
+
 aws s3 cp --region eu-west-2 --cache-control "no-store" ./$FILE s3://download.surrealdb.com/
-rm -rf $FILE surreal
+aws s3 cp --region eu-west-2 --cache-control "no-store" ./$HASH s3://download.surrealdb.com/
+
+rm -rf $FILE $HASH surreal.exe surreal
