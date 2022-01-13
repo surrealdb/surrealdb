ff6a41326a69afee4bd796de116de1e5f9d9c13e
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-01-13 17:39:17 +0000

    Add base library file for wasm build

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 #[macro_use]
-extern crate maplit;
-#[macro_use]
 extern crate log;
 
+mod cnf;
 mod ctx;
 mod dbs;
 mod doc;
 mod err;
 mod fnc;
+mod key;
 mod kvs;
 mod sql;
 
 fn main() {
-	// cli::init(); // Initiate the command line
+	// TODO: implement initiation from wasm
 }
