9e5e6efa6d1b3239e6e8dff7fc1235d7edc68527
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-01-13 07:00:50 +0000

    Add document package blueprint

--- src/doc/mod.rs
+++ src/doc/mod.rs
@@ -1,13 +1,19 @@
-mod doc;
+pub use self::doc::*;
 
 mod allow;
 mod check;
+mod create;
+mod delete;
+mod doc;
 mod event;
 mod grant;
+mod index;
+mod insert;
 mod lives;
 mod merge;
 mod perms;
+mod relate;
+mod select;
 mod table;
+mod update;
 mod r#yield;
-
-pub use self::doc::*;
