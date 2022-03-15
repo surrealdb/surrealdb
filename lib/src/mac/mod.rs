5781352058758151d44e827b49bf3c9c552013b4
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-03-15 08:34:40 +0000

    Remove unused macros

--- lib/src/mac/mod.rs
+++ lib/src/mac/mod.rs
@@ -11,19 +11,3 @@ macro_rules! map {
         m
     }};
 }
-
-macro_rules! hmap {
-    ($($k:expr => $v:expr),* $(,)?) => {{
-        let mut m = ::std::collections::HashMap::new();
-        $(m.insert($k, $v);)+
-        m
-    }};
-}
-
-macro_rules! bmap {
-    ($($k:expr => $v:expr),* $(,)?) => {{
-        let mut m = ::std::collections::BTreeMap::new();
-        $(m.insert($k, $v);)+
-        m
-    }};
-}
