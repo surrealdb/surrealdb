42fa91c7bd64ed11ed9cb084b5140b83adf1801e
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-05-04 22:40:41 +0100

    Implement SQL Script as a newtype tuple struct

--- lib/src/sql/script.rs
+++ lib/src/sql/script.rs
@@ -1,16 +1,22 @@
 use crate::sql::error::IResult;
 use serde::{Deserialize, Serialize};
 use std::fmt;
+use std::ops::Deref;
 use std::str;
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, PartialOrd, Serialize, Deserialize)]
-pub struct Script {
-	pub value: String,
+pub struct Script(pub String);
+
+impl Deref for Script {
+	type Target = String;
+	fn deref(&self) -> &Self::Target {
+		&self.0
+	}
 }
 
 impl fmt::Display for Script {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "\"{}\"", self.value)
+		write!(f, "\"{}\"", self.0)
 	}
 }
 
