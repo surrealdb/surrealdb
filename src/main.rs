f5687abeabf0f35ced4a4e1eb863e5b837d2e95b
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-05-10 11:53:30 +0100

    Add documentation comments to main binary package

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//! This binary is the web-platform server for [SurrealDB](https://surrealdb.com) the
+//! ultimate cloud database for tomorrow's applications. SurrealDB is a scalable,
+//! distributed, collaborative, document-graph database for the realtime web.
+//!
+//! This binary can be used to start a database server instance using an embedded
+//! in-memory datastore, or an embedded datastore persisted to disk. In addition, it
+//! can be used in distributed mode by connecting to a distributed [TiKV](https://tikv.org)
+//! key-value store.
+
 #![forbid(unsafe_code)]
 
 #[macro_use]
