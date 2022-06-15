17c8bdab11876f198b439e70423ef1535185fbd6
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-06-15 08:34:38 +0100

    Add derive macro for datastore key encoding

--- src/lib.rs
+++ src/lib.rs
@@ -45,3 +45,57 @@ pub fn store(input: TokenStream) -> TokenStream {
 
 	output.into()
 }
+
+#[proc_macro_derive(Key)]
+pub fn key(input: TokenStream) -> TokenStream {
+	// Parse the token stream
+	let input = parse_macro_input!(input as DeriveInput);
+	// Fetch the struct name
+	let name = &input.ident;
+	// Generate the output
+	let output = quote! {
+
+		impl From<#name> for Vec<u8> {
+			fn from(v: #name) -> Vec<u8> {
+				v.encode().unwrap_or_default()
+			}
+		}
+
+		impl From<&#name> for Vec<u8> {
+			fn from(v: &#name) -> Vec<u8> {
+				v.encode().unwrap_or_default()
+			}
+		}
+
+		impl From<Vec<u8>> for #name {
+			fn from(v: Vec<u8>) -> Self {
+				Self::decode(&v).unwrap()
+			}
+		}
+
+		impl From<&Vec<u8>> for #name {
+			fn from(v: &Vec<u8>) -> Self {
+				Self::decode(v).unwrap()
+			}
+		}
+
+		impl #name {
+
+			pub fn encode(&self) -> Result<Vec<u8>, crate::err::Error> {
+				crate::sql::serde::beg_internal_serialization();
+				let v = storekey::serialize(self);
+				crate::sql::serde::end_internal_serialization();
+				Ok(v?)
+			}
+
+			pub fn decode(v: &[u8]) -> Result<#name, crate::err::Error> {
+				let v = storekey::deserialize(v);
+				Ok(v?)
+			}
+
+		}
+
+	};
+
+	output.into()
+}
