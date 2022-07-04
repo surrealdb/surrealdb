410295c8002ab818d8ea2886c9d0c34f475d6405
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-07-04 02:01:22 +0100

    Improve map! macro to allow extending the map with other map

--- src/mac/mod.rs
+++ src/mac/mod.rs
@@ -1,6 +1,7 @@
 macro_rules! map {
-    ($($k:expr => $v:expr),* $(,)?) => {{
+    ($($k:expr => $v:expr),* $(,)? $( => $x:expr )?) => {{
         let mut m = ::std::collections::BTreeMap::new();
+        $(m.extend($x.iter().map(|(k, v)| (k.clone(), v.clone())));)?
         $(m.insert($k, $v);)+
         m
     }};
