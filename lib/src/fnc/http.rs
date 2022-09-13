d038331629790ccdfae2c86dc6a802d4996827ca
Finn Bear <finnbearlabs@gmail.com>
2022-09-12 17:52:47 -0700

    Eliminate low-hanging redundancy in http functions.

--- lib/src/fnc/http.rs
+++ lib/src/fnc/http.rs
@@ -1,5 +1,6 @@
 use crate::err::Error;
 use crate::sql::value::Value;
+use crate::sql::Strand;
 
 #[cfg(not(feature = "http"))]
 pub async fn head((_, _): (Value, Option<Value>)) -> Result<Value, Error> {
@@ -31,17 +32,20 @@ pub async fn delete((_, _): (Value, Option<Value>)) -> Result<Value, Error> {
 	Err(Error::HttpDisabled)
 }
 
+fn try_as_uri(fn_name: &str, value: Value) -> Result<Strand, Error> {
+	match value {
+		Value::Strand(uri) => Ok(uri),
+		_ => Err(Error::InvalidArguments {
+			name: fn_name.to_owned(),
+			// Assumption is that URI is first argument.
+			message: String::from("The first argument should be a string."),
+		}),
+	}
+}
+
 #[cfg(feature = "http")]
 pub async fn head((uri, opts): (Value, Option<Value>)) -> Result<Value, Error> {
-	let uri = match uri {
-		Value::Strand(uri) => uri,
-		_ => {
-			return Err(Error::InvalidArguments {
-				name: String::from("http::head"),
-				message: String::from("The first argument should be a string."),
-			})
-		}
-	};
+	let uri = try_as_uri("http::head", uri)?;
 
 	let opts = match opts {
 		Some(Value::Object(opts)) => Some(opts),
@@ -59,15 +63,7 @@ pub async fn head((uri, opts): (Value, Option<Value>)) -> Result<Value, Error> {
 
 #[cfg(feature = "http")]
 pub async fn get((uri, opts): (Value, Option<Value>)) -> Result<Value, Error> {
-	let uri = match uri {
-		Value::Strand(uri) => uri,
-		_ => {
-			return Err(Error::InvalidArguments {
-				name: String::from("http::get"),
-				message: String::from("The first argument should be a string."),
-			})
-		}
-	};
+	let uri = try_as_uri("http::get", uri)?;
 
 	let opts = match opts {
 		Some(Value::Object(opts)) => Some(opts),
@@ -85,15 +81,7 @@ pub async fn get((uri, opts): (Value, Option<Value>)) -> Result<Value, Error> {
 
 #[cfg(feature = "http")]
 pub async fn put((uri, body, opts): (Value, Option<Value>, Option<Value>)) -> Result<Value, Error> {
-	let uri = match uri {
-		Value::Strand(uri) => uri,
-		_ => {
-			return Err(Error::InvalidArguments {
-				name: String::from("http::put"),
-				message: String::from("The first argument should be a string."),
-			})
-		}
-	};
+	let uri = try_as_uri("http::put", uri)?;
 
 	let opts = match opts {
 		Some(Value::Object(opts)) => Some(opts),
@@ -113,15 +101,7 @@ pub async fn put((uri, body, opts): (Value, Option<Value>, Option<Value>)) -> Re
 pub async fn post(
 	(uri, body, opts): (Value, Option<Value>, Option<Value>),
 ) -> Result<Value, Error> {
-	let uri = match uri {
-		Value::Strand(uri) => uri,
-		_ => {
-			return Err(Error::InvalidArguments {
-				name: String::from("http::post"),
-				message: String::from("The first argument should be a string."),
-			})
-		}
-	};
+	let uri = try_as_uri("http::post", uri)?;
 
 	let opts = match opts {
 		Some(Value::Object(opts)) => Some(opts),
@@ -141,15 +121,7 @@ pub async fn post(
 pub async fn patch(
 	(uri, body, opts): (Value, Option<Value>, Option<Value>),
 ) -> Result<Value, Error> {
-	let uri = match uri {
-		Value::Strand(uri) => uri,
-		_ => {
-			return Err(Error::InvalidArguments {
-				name: String::from("http::patch"),
-				message: String::from("The first argument should be a string."),
-			})
-		}
-	};
+	let uri = try_as_uri("http::patch", uri)?;
 
 	let opts = match opts {
 		Some(Value::Object(opts)) => Some(opts),
@@ -167,15 +139,7 @@ pub async fn patch(
 
 #[cfg(feature = "http")]
 pub async fn delete((uri, opts): (Value, Option<Value>)) -> Result<Value, Error> {
-	let uri = match uri {
-		Value::Strand(uri) => uri,
-		_ => {
-			return Err(Error::InvalidArguments {
-				name: String::from("http::delete"),
-				message: String::from("The first argument should be a string."),
-			})
-		}
-	};
+	let uri = try_as_uri("http::delete", uri)?;
 
 	let opts = match opts {
 		Some(Value::Object(opts)) => Some(opts),
