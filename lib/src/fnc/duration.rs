0dfc3a0f7607d26fd9fead45d23c3c0d33212f66
Hugo Saracino <hugo.saracino@gmail.com>
2022-09-22 22:45:40 +0200

    Make duration functions require one argument

--- lib/src/fnc/duration.rs
+++ lib/src/fnc/duration.rs
@@ -1,62 +1,55 @@
 use crate::err::Error;
-use crate::sql::duration::Duration;
 use crate::sql::value::Value;
 
-pub fn secs((duration,): (Option<Value>,)) -> Result<Value, Error> {
+pub fn secs((duration,): (Value,)) -> Result<Value, Error> {
 	let duration = match duration {
-		Some(Value::Duration(d)) => d,
-		None => Duration::default(),
-		Some(_) => return Ok(Value::None),
+		Value::Duration(d) => d,
+		_ => return Ok(Value::None),
 	};
 
 	Ok(duration.secs())
 }
 
-pub fn mins((duration,): (Option<Value>,)) -> Result<Value, Error> {
+pub fn mins((duration,): (Value,)) -> Result<Value, Error> {
 	let duration = match duration {
-		Some(Value::Duration(d)) => d,
-		None => Duration::default(),
-		Some(_) => return Ok(Value::None),
+		Value::Duration(d) => d,
+		_ => return Ok(Value::None),
 	};
 
 	Ok(duration.mins())
 }
 
-pub fn hours((duration,): (Option<Value>,)) -> Result<Value, Error> {
+pub fn hours((duration,): (Value,)) -> Result<Value, Error> {
 	let duration = match duration {
-		Some(Value::Duration(d)) => d,
-		None => Duration::default(),
-		Some(_) => return Ok(Value::None),
+		Value::Duration(d) => d,
+		_ => return Ok(Value::None),
 	};
 
 	Ok(duration.hours())
 }
 
-pub fn days((duration,): (Option<Value>,)) -> Result<Value, Error> {
+pub fn days((duration,): (Value,)) -> Result<Value, Error> {
 	let duration = match duration {
-		Some(Value::Duration(d)) => d,
-		None => Duration::default(),
-		Some(_) => return Ok(Value::None),
+		Value::Duration(d) => d,
+		_ => return Ok(Value::None),
 	};
 
 	Ok(duration.days())
 }
 
-pub fn weeks((duration,): (Option<Value>,)) -> Result<Value, Error> {
+pub fn weeks((duration,): (Value,)) -> Result<Value, Error> {
 	let duration = match duration {
-		Some(Value::Duration(d)) => d,
-		None => Duration::default(),
-		Some(_) => return Ok(Value::None),
+		Value::Duration(d) => d,
+		_ => return Ok(Value::None),
 	};
 
 	Ok(duration.weeks())
 }
 
-pub fn years((duration,): (Option<Value>,)) -> Result<Value, Error> {
+pub fn years((duration,): (Value,)) -> Result<Value, Error> {
 	let duration = match duration {
-		Some(Value::Duration(d)) => d,
-		None => Duration::default(),
-		Some(_) => return Ok(Value::None),
+		Value::Duration(d) => d,
+		_ => return Ok(Value::None),
 	};
 
 	Ok(duration.years())
