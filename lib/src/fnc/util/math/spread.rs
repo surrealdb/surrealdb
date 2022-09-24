f5a329f7e39adb859f27ded7794c2f97d0bde928
Simon (Desktop) <celebrate.v.c@gmail.com>
2022-09-24 13:02:50 -0400

    implent Spread
    Assume this meant range difference as the other 2 common spread stats
    exist
    
    Signed-off-by: Simon (Desktop) <celebrate.v.c@gmail.com>

--- lib/src/fnc/util/math/spread.rs
+++ lib/src/fnc/util/math/spread.rs
@@ -1,11 +1,31 @@
 use crate::sql::number::Number;
 
 pub trait Spread {
+	/// (Implementing range since var and IQR are implemented)
+	/// O(n) time complex
 	fn spread(self) -> Number;
 }
 
 impl Spread for Vec<Number> {
 	fn spread(self) -> Number {
-		todo!()
+
+		let init = self.get(0);
+
+		let min_max = self.iter().fold((init, init), |(mut min, mut max), val|{
+				min = std::cmp::min(min, Some(val));
+				max = std::cmp::max(max,Some(val));
+
+				(min,max)
+		});
+
+		match min_max {
+
+			(Some(min),Some(max)) => {
+				max-min
+			},
+			_ => Number::Float(f64::NAN)
+
+		}
+
 	}
 }
