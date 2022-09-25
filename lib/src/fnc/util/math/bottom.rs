dfcb9381c3b380392f04cdd5ab4e98381dd33c43
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-09-25 08:17:55 +0100

    Improve performance of math::top() and math::bottom()

--- lib/src/fnc/util/math/bottom.rs
+++ lib/src/fnc/util/math/bottom.rs
@@ -2,24 +2,25 @@ use crate::sql::number::Number;
 use std::collections::BinaryHeap;
 
 pub trait Bottom {
-	/// Find the lowest `k` records from the collection in arbetrary order  
+	/// Find the lowest `k` records from the collection in arbetrary order
 	/// O(n*k*log(k)) time complex
 	fn bottom(self, k: i64) -> Vec<Number>;
 }
 
 impl Bottom for Vec<Number> {
 	fn bottom(self, k: i64) -> Vec<Number> {
+		// Convert to usize
+		let k = k as usize;
+		// Create a heap to store the numbers
 		let mut heap = BinaryHeap::new();
-
-		for idx in 0..self.len() {
-			let n = self.get(idx).unwrap().clone();
-			heap.push(n.clone());
-
-			if idx >= k as usize {
+		// Iterate and store the bottom numbers
+		for (i, v) in self.into_iter().enumerate() {
+			heap.push(v);
+			if i >= k {
 				heap.pop();
 			}
 		}
-
+		// Return the numbers as a vector
 		heap.into_iter().collect()
 	}
 }
