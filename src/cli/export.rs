c06489028420b8df65dafd6930dc2eb1c40fc73c
Rushmore Mushambi <rushmore@webenchanter.com>
2022-09-27 17:35:03 +0200

    Fix the build (#1248)

--- src/cli/export.rs
+++ src/cli/export.rs
@@ -1,7 +1,7 @@
 use crate::cli::LOG;
 use crate::err::Error;
 use reqwest::blocking::Client;
-use reqwest::header::CONTENT_TYPE;
+use reqwest::header::ACCEPT;
 use std::fs::OpenOptions;
 
 pub fn init(matches: &clap::ArgMatches) -> Result<(), Error> {
