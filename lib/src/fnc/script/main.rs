deb82df86206e1ba5193a9586a873d7432d20d91
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-09-29 08:10:24 +0100

    Explicitly set max stack size just in case

--- lib/src/fnc/script/main.rs
+++ lib/src/fnc/script/main.rs
@@ -25,9 +25,11 @@ pub async fn run(
 	let exe = Executor::default();
 	// Create an JavaScript context
 	let run = js::Runtime::new().unwrap();
-	// Note: the default max_stack_size of rquickjs can be left as-is, but there is no default
-	// memory limit.
+	// Explicitly set max stack size to 256 KiB
+	run.set_max_stack_size(262_144);
+	// Explicitly set max memory size to 2 MB
 	run.set_memory_limit(2_000_000);
+	// Ensure scripts are cancelled with context
 	let cancellation = ctx.cancellation();
 	run.set_interrupt_handler(Some(Box::new(move || cancellation.is_done())));
 	// Create an execution context
