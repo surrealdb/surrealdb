bec3d49b6d4cfe8d3578ba5cf81229bae9e758cf
Tobie Morgan Hitchcock <tobie@abcum.com>
2022-09-29 11:09:20 +0100

    Allow deep merging in UPDATE ... MERGE statements
    Closes #1225

--- lib/src/doc/clean.rs
+++ lib/src/doc/clean.rs
@@ -28,7 +28,7 @@ impl<'a> Document<'a> {
 				}
 			}
 			// Loop over every field in the document
-			for fd in self.current.every().iter() {
+			for fd in self.current.every(true).iter() {
 				if !keys.contains(fd) {
 					match fd {
 						fd if fd.is_id() => continue,
