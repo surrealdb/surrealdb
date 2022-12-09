4b44ea170885297be72ab4a3c0f6c603f8f49e09
Rinat Shaykhutdinov <mail@rinatshay.com>
2022-12-09 17:54:17 +0200

    Replace a permanent redirect with a temporary one

--- src/net/index.rs
+++ src/net/index.rs
@@ -3,5 +3,7 @@ use warp::http::Uri;
 use warp::Filter;
 
 pub fn config() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-	warp::path::end().and(warp::get()).map(|| warp::redirect(Uri::from_static(cnf::APP_ENDPOINT)))
+	warp::path::end()
+		.and(warp::get())
+		.map(|| warp::redirect::temporary(Uri::from_static(cnf::APP_ENDPOINT)))
 }
