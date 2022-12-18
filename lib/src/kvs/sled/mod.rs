cb55b71d997c2a52d872de9a883ca3a2cdb931f9
Emmanuel Keller <ekeller@open-search-server.com>
2022-12-18 21:03:27 +0000

    Implements test for transactions with sled

--- lib/src/kvs/sled/mod.rs
+++ lib/src/kvs/sled/mod.rs
@@ -115,6 +115,11 @@ impl Transaction {
 
 	/// Check if the key exists (without any pre-check)
 	fn _exi(&self, key: &Key) -> Result<bool, Error> {
+		if let Some(del) = &self.del {
+			if del.contains(key) {
+				return Ok(false);
+			}
+		}
 		// We check in key set in the transaction
 		if let Some(set) = &self.set {
 			if set.contains_key(key) {
@@ -264,6 +269,8 @@ impl Transaction {
 		}
 		if let Some(del) = &mut self.del {
 			del.insert(key);
+		} else {
+			self.del = Some(HashSet::from([key]));
 		}
 		Ok(())
 	}
@@ -314,7 +321,7 @@ impl Transaction {
 	}
 
 	/// Retrieve a range of keys from the databases
-	pub fn scan<K>(&mut self, rng: Range<K>, limit: u32) -> Result<Vec<(Key, Val)>, Error>
+	pub fn scan<K>(&self, rng: Range<K>, limit: u32) -> Result<Vec<(Key, Val)>, Error>
 	where
 		K: Into<Key>,
 	{
@@ -362,9 +369,14 @@ impl<'a> ParallelIterator<'a> {
 		// Extract the key/values meeting the range in the transaction
 		// and the initial key/value
 		let (tx_kvs, tx_next_kv) = if let Some(set) = &tx.set {
-			let mut range = set.range(rng.clone());
-			let next = range.next().map(|(key, val)| (key.clone(), val.clone()));
-			(Some(range), next)
+			// Treemap.range panics if start is greater than end
+			if rng.start.le(&rng.end) {
+				let mut range = set.range(rng.clone());
+				let next = range.next().map(|(key, val)| (key.clone(), val.clone()));
+				(Some(range), next)
+			} else {
+				(None, None)
+			}
 		} else {
 			(None, None)
 		};
@@ -442,8 +454,8 @@ impl<'a> ParallelIterator<'a> {
 
 #[cfg(test)]
 mod tests {
-	use sled::IVec;
-	use std::collections::HashMap;
+	use crate::kvs::tx::Transaction;
+	use crate::kvs::{Key, Val};
 	use std::fs;
 	use std::path::PathBuf;
 	use std::sync::atomic::{AtomicU16, Ordering};
@@ -483,48 +495,242 @@ mod tests {
 		datastore.transaction(true, false).await.unwrap()
 	}
 
-	fn check_store(store_path: &str, key_values: HashMap<&'static str, &'static str>) {
-		let db = sled::open(store_path).unwrap();
-		assert_eq!(db.len(), key_values.len());
-		for (key, value) in key_values {
-			assert_eq!(db.get(key).unwrap(), Some(IVec::from(value.as_bytes())));
-		}
-	}
-
 	#[tokio::test]
-	async fn test_transaction_sled_put() {
+	async fn test_transaction_sled_put_exi_get() {
 		let store_path = new_store_path();
 		{
-			let mut transaction = get_transaction(&store_path).await;
-			transaction.put("flip", "flop").await.unwrap();
-			transaction.commit().await.unwrap();
+			let mut tx = get_transaction(&store_path).await;
+			// The key should not exist
+			assert_eq!(tx.exi("flip").await.unwrap(), false);
+			assert_eq!(tx.get("flip").await.unwrap(), None);
+			tx.put("flip", "flop").await.unwrap();
+			// Check existence against memory
+			assert_eq!(tx.exi("flip").await.unwrap(), true);
+			// Read from memory
+			assert_eq!(tx.get("flip").await.unwrap(), Some("flop".as_bytes().to_vec()));
+			// Commit in storage
+			tx.commit().await.unwrap();
 		}
-		check_store(&store_path, HashMap::from([("flip", "flop")]));
+		{
+			// New transaction with the commited data
+			let mut tx = get_transaction(&store_path).await;
+			// The key exists
+			assert_eq!(tx.exi("flip").await.unwrap(), true);
+			// And the value can be retrieved
+			assert_eq!(tx.get("flip").await.unwrap(), Some("flop".as_bytes().to_vec()));
+		}
 	}
 
 	#[tokio::test]
 	async fn test_transaction_sled_putc_err() {
 		let store_path = new_store_path();
 		{
-			let mut transaction = get_transaction(&store_path).await;
-			transaction.put("flip", "flop").await.unwrap();
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("flip", "flop").await.unwrap();
 			assert_eq!(
-				transaction.putc("flip", "flap", Some("nada")).await.err().unwrap().to_string(),
+				tx.putc("flip", "flap", Some("nada")).await.err().unwrap().to_string(),
 				"Value being checked was not correct"
 			);
+			// Checked the value did not change in memory
+			assert_eq!(tx.get("flip").await.unwrap(), Some("flop".as_bytes().to_vec()));
+			// Commit in storage
+			tx.commit().await.unwrap();
 		}
-		check_store(&store_path, HashMap::from([]));
+		{
+			// New transaction with the committed data
+			let mut tx = get_transaction(&store_path).await;
+			// Check the value did not change on storage
+			assert_eq!(tx.get("flip").await.unwrap(), Some("flop".as_bytes().to_vec()));
+		}
 	}
 
 	#[tokio::test]
 	async fn test_transaction_sled_putc_ok() {
 		let store_path = new_store_path();
 		{
-			let mut transaction = get_transaction(&store_path).await;
-			transaction.put("flip", "flop").await.unwrap();
-			transaction.putc("flip", "flap", Some("flop")).await.unwrap();
-			transaction.commit().await.unwrap();
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("flip", "flop").await.unwrap();
+			tx.putc("flip", "flap", Some("flop")).await.unwrap();
+			tx.commit().await.unwrap();
 		}
-		check_store(&store_path, HashMap::from([("flip", "flap")]));
+		{
+			// New transaction with the committed data
+			let mut tx = get_transaction(&store_path).await;
+			assert_eq!(tx.get("flip").await.unwrap(), Some("flap".as_bytes().to_vec()));
+		}
+	}
+
+	fn check_scan_result(result: Vec<(Key, Val)>, expected: Vec<(&'static str, &'static str)>) {
+		assert_eq!(result.len(), expected.len());
+		let mut i = 0;
+		for (key, val) in result {
+			let (expected_key, expected_value) = expected.get(i).unwrap();
+			assert_eq!(&String::from_utf8(key).unwrap(), *expected_key);
+			assert_eq!(&String::from_utf8(val).unwrap(), *expected_value);
+			i += 1;
+		}
+	}
+
+	async fn scan_suite_checks(tx: &mut Transaction) {
+		// I can retrieve the key/values with using scan with several ranges
+		check_scan_result(
+			tx.scan("k1".."k9", 100).await.unwrap(),
+			vec![("k1", "v1"), ("k2", "v2"), ("k3", "v3"), ("k4", "v4"), ("k5", "v5")],
+		);
+		check_scan_result(tx.scan("k1".."k2", 100).await.unwrap(), vec![("k1", "v1")]);
+		check_scan_result(
+			tx.scan("k1".."k3", 100).await.unwrap(),
+			vec![("k1", "v1"), ("k2", "v2")],
+		);
+		check_scan_result(tx.scan("k2".."k3", 100).await.unwrap(), vec![("k2", "v2")]);
+		check_scan_result(tx.scan("k3".."k4", 100).await.unwrap(), vec![("k3", "v3")]);
+		check_scan_result(tx.scan("k4".."k5", 100).await.unwrap(), vec![("k4", "v4")]);
+		check_scan_result(
+			tx.scan("k4".."k6", 100).await.unwrap(),
+			vec![("k4", "v4"), ("k5", "v5")],
+		);
+		check_scan_result(tx.scan("k5".."k7", 100).await.unwrap(), vec![("k5", "v5")]);
+		check_scan_result(tx.scan("k2".."k1", 100).await.unwrap(), vec![]);
+
+		// I can retrieve the key/values using scan with several limits
+		check_scan_result(tx.scan("k1".."k9", 0).await.unwrap(), vec![]);
+		check_scan_result(tx.scan("k1".."k9", 1).await.unwrap(), vec![("k1", "v1")]);
+		check_scan_result(tx.scan("k2".."k9", 1).await.unwrap(), vec![("k2", "v2")]);
+		check_scan_result(tx.scan("k5".."k9", 1).await.unwrap(), vec![("k5", "v5")]);
+		check_scan_result(tx.scan("k6".."k9", 1).await.unwrap(), vec![]);
+		check_scan_result(tx.scan("k1".."k4", 2).await.unwrap(), vec![("k1", "v1"), ("k2", "v2")]);
+		check_scan_result(tx.scan("k2".."k4", 2).await.unwrap(), vec![("k2", "v2"), ("k3", "v3")]);
+		check_scan_result(tx.scan("k3".."k4", 2).await.unwrap(), vec![("k3", "v3")]);
+	}
+
+	#[tokio::test]
+	async fn test_transaction_sled_scan_in_transaction() {
+		let store_path = new_store_path();
+		{
+			// Given a set of key/values added in a transaction
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("k3", "v3").await.unwrap();
+			tx.put("k2", "v2").await.unwrap();
+			tx.put("k1", "v1").await.unwrap();
+			tx.put("k5", "v5").await.unwrap();
+			tx.put("k4", "v4").await.unwrap();
+
+			// Then, I can successfully use the range method on in memory key/values
+			scan_suite_checks(&mut tx).await;
+		}
+	}
+
+	#[tokio::test]
+	async fn test_transaction_sled_scan_in_storage() {
+		let store_path = new_store_path();
+		{
+			// Given three key/values added in the transaction...
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("k3", "v3").await.unwrap();
+			tx.put("k2", "v2").await.unwrap();
+			tx.put("k1", "v1").await.unwrap();
+			tx.put("k5", "v5").await.unwrap();
+			tx.put("k4", "v4").await.unwrap();
+			// ... and stored
+			tx.commit().await.unwrap();
+		}
+		{
+			// Then, I can successfully use the range method on stored key/values
+			let mut tx = get_transaction(&store_path).await;
+			scan_suite_checks(&mut tx).await;
+		}
+	}
+
+	#[tokio::test]
+	async fn test_transaction_sled_scan_mixed() {
+		let store_path = new_store_path();
+		{
+			// Given three key/values added in the transaction and stored
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("k3", "v3").await.unwrap();
+			tx.put("k2", "v2").await.unwrap();
+			tx.put("k5", "v5").await.unwrap();
+			tx.commit().await.unwrap();
+		}
+		{
+			// then, given two key/values added in the transaction
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("k1", "v1").await.unwrap();
+			tx.put("k4", "v4").await.unwrap();
+
+			// Then, I can successfully use the range method on mixed key/values
+			scan_suite_checks(&mut tx).await;
+		}
+	}
+
+	#[tokio::test]
+	async fn test_transaction_sled_del_in_transaction() {
+		let store_path = new_store_path();
+		{
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("k1", "v1").await.unwrap();
+			tx.put("k2", "v2").await.unwrap();
+			tx.del("k1").await.unwrap();
+			assert_eq!(tx.exi("k1").await.unwrap(), false);
+			assert_eq!(tx.get("k1").await.unwrap(), None);
+			tx.commit().await.unwrap();
+		}
+		{
+			let mut tx = get_transaction(&store_path).await;
+			assert_eq!(tx.exi("k1").await.unwrap(), false);
+			assert_eq!(tx.get("k2").await.unwrap(), Some("v2".as_bytes().to_vec()));
+		}
+	}
+
+	#[tokio::test]
+	async fn test_transaction_sled_del_in_storage() {
+		let store_path = new_store_path();
+		{
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("k1", "v1").await.unwrap();
+			tx.put("k2", "v2").await.unwrap();
+			tx.commit().await.unwrap();
+		}
+		{
+			let mut tx = get_transaction(&store_path).await;
+			tx.del("k1").await.unwrap();
+			assert_eq!(tx.exi("k1").await.unwrap(), false);
+			assert_eq!(tx.get("k1").await.unwrap(), None);
+			tx.commit().await.unwrap();
+		}
+		{
+			let mut tx = get_transaction(&store_path).await;
+			assert_eq!(tx.exi("k1").await.unwrap(), false);
+			assert_eq!(tx.get("k2").await.unwrap(), Some("v2".as_bytes().to_vec()));
+		}
+	}
+
+	#[tokio::test]
+	async fn test_transaction_sled_cancel_put_and_del() {
+		let store_path = new_store_path();
+		// Given a store with two keys
+		{
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("k1", "v1").await.unwrap();
+			tx.put("k2", "v2").await.unwrap();
+			tx.commit().await.unwrap();
+		}
+		{
+			// When cancelling a transaction adding k3 and deleting k1
+			let mut tx = get_transaction(&store_path).await;
+			tx.put("k3", "v3").await.unwrap();
+			tx.del("k1").await.unwrap();
+			assert_eq!(tx.exi("k1").await.unwrap(), false);
+			assert_eq!(tx.get("k3").await.unwrap(), Some("v3".as_bytes().to_vec()));
+
+			tx.cancel().await.unwrap();
+		}
+		{
+			// Then k3 has not been added, and k1 as not been deleted
+			let mut tx = get_transaction(&store_path).await;
+			assert_eq!(tx.exi("k3").await.unwrap(), false);
+			assert_eq!(tx.get("k1").await.unwrap(), Some("v1".as_bytes().to_vec()));
+			assert_eq!(tx.get("k2").await.unwrap(), Some("v2".as_bytes().to_vec()));
+		}
 	}
 }
