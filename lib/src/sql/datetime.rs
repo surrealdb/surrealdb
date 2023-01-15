92c67cc5245adcac74f2e861536e6baccbaed3ff
Tobie Morgan Hitchcock <tobie@abcum.com>
2023-01-15 08:43:39 +0000

    Allow 24 hours in datetime

--- lib/src/sql/datetime.rs
+++ lib/src/sql/datetime.rs
@@ -200,7 +200,7 @@ fn day(i: &str) -> IResult<&str, u32> {
 }
 
 fn hour(i: &str) -> IResult<&str, u32> {
-	take_digits_range(i, 2, 0..=23)
+	take_digits_range(i, 2, 0..=24)
 }
 
 fn minute(i: &str) -> IResult<&str, u32> {
@@ -208,7 +208,6 @@ fn minute(i: &str) -> IResult<&str, u32> {
 }
 
 fn second(i: &str) -> IResult<&str, u32> {
-	// 60 is for leap seconds
 	take_digits_range(i, 2, 0..=60)
 }
 
