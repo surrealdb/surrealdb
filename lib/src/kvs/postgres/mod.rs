d502c8931dae526b6ad00ba4336225f8983f8e7a
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2023-03-18 20:39:42 +0800

    kvs/postgres: handle table and index creation

--- lib/src/kvs/postgres/mod.rs
+++ lib/src/kvs/postgres/mod.rs
@@ -7,7 +7,10 @@ pub(crate) struct Datastore;
 impl Datastore {
 	/// Open a new database
 	pub async fn new(path: &str) -> Result<super::seaorm::Datastore, Error> {
-		super::seaorm::Datastore::new(path)
+		let db = super::seaorm::Datastore::new(path).await?;
+		db.ensure_table_exists().await?;
+		db.ensure_indices_exists().await?;
+		Ok(db)
 	}
 }
 
