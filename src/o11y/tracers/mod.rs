8c0e4ae9d20d4ec9a91ecda8fc50c927e7ae64aa
Salvador Girones <salvadorgirones@gmail.com>
2023-03-24 18:24:33 +0100

    try without tests

--- src/o11y/tracers/mod.rs
+++ src/o11y/tracers/mod.rs
@@ -28,61 +28,61 @@ where
 	}
 }
 
-#[cfg(test)]
-pub mod tests {
-	use futures::StreamExt;
-	use opentelemetry_proto::tonic::collector::trace::v1::{
-		trace_service_server::{TraceService, TraceServiceServer},
-		ExportTraceServiceRequest, ExportTraceServiceResponse,
-	};
-	use std::{net::SocketAddr, sync::Mutex};
-	use tokio::sync::mpsc;
-	use tokio_stream::wrappers::TcpListenerStream;
+// #[cfg(test)]
+// pub mod tests {
+// 	use futures::StreamExt;
+// 	use opentelemetry_proto::tonic::collector::trace::v1::{
+// 		trace_service_server::{TraceService, TraceServiceServer},
+// 		ExportTraceServiceRequest, ExportTraceServiceResponse,
+// 	};
+// 	use std::{net::SocketAddr, sync::Mutex};
+// 	use tokio::sync::mpsc;
+// 	use tokio_stream::wrappers::TcpListenerStream;
 
-	/// Server that mocks a TraceService and receives traces
-	struct MockServer {
-		tx: Mutex<mpsc::Sender<ExportTraceServiceRequest>>,
-	}
+// 	/// Server that mocks a TraceService and receives traces
+// 	struct MockServer {
+// 		tx: Mutex<mpsc::Sender<ExportTraceServiceRequest>>,
+// 	}
 
-	impl MockServer {
-		pub fn new(tx: mpsc::Sender<ExportTraceServiceRequest>) -> Self {
-			Self {
-				tx: Mutex::new(tx),
-			}
-		}
-	}
+// 	impl MockServer {
+// 		pub fn new(tx: mpsc::Sender<ExportTraceServiceRequest>) -> Self {
+// 			Self {
+// 				tx: Mutex::new(tx),
+// 			}
+// 		}
+// 	}
 
-	#[tonic::async_trait]
-	impl TraceService for MockServer {
-		async fn export(
-			&self,
-			request: tonic::Request<ExportTraceServiceRequest>,
-		) -> Result<tonic::Response<ExportTraceServiceResponse>, tonic::Status> {
-			self.tx.lock().unwrap().try_send(request.into_inner()).expect("Channel full");
-			Ok(tonic::Response::new(ExportTraceServiceResponse {}))
-		}
-	}
+// 	#[tonic::async_trait]
+// 	impl TraceService for MockServer {
+// 		async fn export(
+// 			&self,
+// 			request: tonic::Request<ExportTraceServiceRequest>,
+// 		) -> Result<tonic::Response<ExportTraceServiceResponse>, tonic::Status> {
+// 			self.tx.lock().unwrap().try_send(request.into_inner()).expect("Channel full");
+// 			Ok(tonic::Response::new(ExportTraceServiceResponse {}))
+// 		}
+// 	}
 
-	pub async fn mock_otlp_server() -> (SocketAddr, mpsc::Receiver<ExportTraceServiceRequest>) {
-		let addr: SocketAddr = "[::1]:0".parse().unwrap();
-		let listener = tokio::net::TcpListener::bind(addr).await.expect("failed to bind");
-		let addr = listener.local_addr().unwrap();
-		let stream = TcpListenerStream::new(listener).map(|s| {
-			if let Ok(ref s) = s {
-				println!("Got new conn at {}", s.peer_addr().unwrap());
-			}
-			s
-		});
+// 	pub async fn mock_otlp_server() -> (SocketAddr, mpsc::Receiver<ExportTraceServiceRequest>) {
+// 		let addr: SocketAddr = "[::1]:0".parse().unwrap();
+// 		let listener = tokio::net::TcpListener::bind(addr).await.expect("failed to bind");
+// 		let addr = listener.local_addr().unwrap();
+// 		let stream = TcpListenerStream::new(listener).map(|s| {
+// 			if let Ok(ref s) = s {
+// 				println!("Got new conn at {}", s.peer_addr().unwrap());
+// 			}
+// 			s
+// 		});
 
-		let (req_tx, req_rx) = mpsc::channel(10);
-		let service = TraceServiceServer::new(MockServer::new(req_tx));
-		tokio::task::spawn(async move {
-			tonic::transport::Server::builder()
-				.add_service(service)
-				.serve_with_incoming(stream)
-				.await
-				.expect("Server failed");
-		});
-		(addr, req_rx)
-	}
-}
+// 		let (req_tx, req_rx) = mpsc::channel(10);
+// 		let service = TraceServiceServer::new(MockServer::new(req_tx));
+// 		tokio::task::spawn(async move {
+// 			tonic::transport::Server::builder()
+// 				.add_service(service)
+// 				.serve_with_incoming(stream)
+// 				.await
+// 				.expect("Server failed");
+// 		});
+// 		(addr, req_rx)
+// 	}
+// }
