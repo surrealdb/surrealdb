1137b60e6ff2980cb4e13680e31de19f70084858
Finn Bear <finnbearlabs@gmail.com>
2023-04-30 22:10:11 -0700

    Fix typo.

--- lib/benches/executor.rs
+++ lib/benches/executor.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 use surrealdb::{dbs::Session, kvs::Datastore};
 
 macro_rules! query {
