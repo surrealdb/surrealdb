1dcba5594a2e03441bb25e9e530bf9a734c935c5
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2023-06-10 20:44:21 +0100

    Add tests for key-value range scans

--- lib/src/kvs/tests/raw.rs
+++ lib/src/kvs/tests/raw.rs
@@ -174,3 +174,51 @@ async fn delc() {
 	assert!(matches!(val.as_deref(), None));
 	tx.cancel().await.unwrap();
 }
+
+#[tokio::test]
+#[serial]
+async fn scan() {
+	// Create a new datastore
+	let ds = new_ds().await;
+	// Create a writeable transaction
+	let mut tx = ds.transaction(true, false).await.unwrap();
+	assert!(tx.put("test1", "1").await.is_ok());
+	assert!(tx.put("test2", "2").await.is_ok());
+	assert!(tx.put("test3", "3").await.is_ok());
+	assert!(tx.put("test4", "4").await.is_ok());
+	assert!(tx.put("test5", "5").await.is_ok());
+	tx.commit().await.unwrap();
+	// Create a readonly transaction
+	let mut tx = ds.transaction(false, false).await.unwrap();
+	let val = tx.scan("test1".."test9", u32::MAX).await.unwrap();
+	assert_eq!(val.len(), 5);
+	assert_eq!(val[0].0, b"test1");
+	assert_eq!(val[0].1, b"1");
+	assert_eq!(val[1].0, b"test2");
+	assert_eq!(val[1].1, b"2");
+	assert_eq!(val[2].0, b"test3");
+	assert_eq!(val[2].1, b"3");
+	assert_eq!(val[3].0, b"test4");
+	assert_eq!(val[3].1, b"4");
+	assert_eq!(val[4].0, b"test5");
+	assert_eq!(val[4].1, b"5");
+	tx.cancel().await.unwrap();
+	// Create a readonly transaction
+	let mut tx = ds.transaction(false, false).await.unwrap();
+	let val = tx.scan("test2".."test4", u32::MAX).await.unwrap();
+	assert_eq!(val.len(), 2);
+	assert_eq!(val[0].0, b"test2");
+	assert_eq!(val[0].1, b"2");
+	assert_eq!(val[1].0, b"test3");
+	assert_eq!(val[1].1, b"3");
+	tx.cancel().await.unwrap();
+	// Create a readonly transaction
+	let mut tx = ds.transaction(false, false).await.unwrap();
+	let val = tx.scan("test1".."test9", 2).await.unwrap();
+	assert_eq!(val.len(), 2);
+	assert_eq!(val[0].0, b"test1");
+	assert_eq!(val[0].1, b"1");
+	assert_eq!(val[1].0, b"test2");
+	assert_eq!(val[1].1, b"2");
+	tx.cancel().await.unwrap();
+}
