432e60e8158fc70fbbf53e56933c8f82f2f464a4
Hugh Kaznowski <hugh@surrealdb.com>
2023-06-13 17:07:42 +0100

    Add display to notification and action

--- lib/src/dbs/notification.rs
+++ lib/src/dbs/notification.rs
@@ -1,5 +1,6 @@
 use crate::sql::{Uuid, Value};
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Notification {
@@ -14,4 +15,25 @@ pub enum Action {
 	Create,
 	Update,
 	Delete,
+}
+
+impl fmt::Display for Notification {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(
+			f,
+			"Notification - ID: {}, Action: {}, Result: {}",
+			self.id, self.action, self.result
+		)
+	}
+}
+
+impl fmt::Display for Action {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		let action_str = match self {
+			Action::Create => "Create",
+			Action::Update => "Update",
+			Action::Delete => "Delete",
+		};
+		write!(f, "{}", action_str)
+	}
 }
