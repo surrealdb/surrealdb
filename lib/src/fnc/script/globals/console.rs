bd49b58d173a5b3b841c64148d948b6bfc01517c
Mees Delzenne <mees.delzenne@gmail.com>
2023-07-17 10:38:58 +0200

    Revert "Revert "Update js functions to new rquickjs version" (#2262)"
    This reverts commit 5c08be973d571f6e1380154ba23e523c1e2229cb.

--- lib/src/fnc/script/globals/console.rs
+++ lib/src/fnc/script/globals/console.rs
@@ -1,32 +1,44 @@
-#[js::bind(object, public)]
-#[quickjs(rename = "console")]
-#[allow(clippy::module_inception)]
-pub mod console {
-	// Specify the imports
-	use crate::sql::value::Value;
-	use js::prelude::Rest;
-	/// Log the input values as INFO
-	pub fn log(args: Rest<Value>) {
-		info!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
-	}
-	/// Log the input values as INFO
-	pub fn info(args: Rest<Value>) {
-		info!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
-	}
-	/// Log the input values as WARN
-	pub fn warn(args: Rest<Value>) {
-		warn!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
-	}
-	/// Log the input values as ERROR
-	pub fn error(args: Rest<Value>) {
-		error!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
-	}
-	/// Log the input values as DEBUG
-	pub fn debug(args: Rest<Value>) {
-		debug!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
-	}
-	/// Log the input values as TRACE
-	pub fn trace(args: Rest<Value>) {
-		trace!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
-	}
+// Specify the imports
+use crate::sql::value::Value;
+use js::{prelude::Rest, Ctx, Object, Result};
+/// Log the input values as INFO
+#[js::function]
+pub fn log(args: Rest<Value>) {
+	info!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
+}
+/// Log the input values as INFO
+#[js::function]
+pub fn info(args: Rest<Value>) {
+	info!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
+}
+/// Log the input values as WARN
+#[js::function]
+pub fn warn(args: Rest<Value>) {
+	warn!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
+}
+/// Log the input values as ERROR
+#[js::function]
+pub fn error(args: Rest<Value>) {
+	error!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
+}
+/// Log the input values as DEBUG
+#[js::function]
+pub fn debug(args: Rest<Value>) {
+	debug!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
+}
+/// Log the input values as TRACE
+#[js::function]
+pub fn trace(args: Rest<Value>) {
+	trace!("{}", args.iter().map(|v| v.to_raw_string()).collect::<Vec<String>>().join(" "));
+}
+
+pub fn console<'js>(ctx: &Ctx<'js>) -> Result<Object<'js>> {
+	let console = Object::new(ctx.clone())?;
+	console.set("log", js_log)?;
+	console.set("info", js_info)?;
+	console.set("warn", js_warn)?;
+	console.set("error", js_error)?;
+	console.set("debug", js_debug)?;
+	console.set("trace", js_trace)?;
+	Ok(console)
 }
