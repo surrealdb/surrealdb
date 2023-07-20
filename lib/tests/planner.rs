e9ff637949218e889c537a77b5eeca4c1df1c928
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2023-07-20 12:49:54 +0100

    Includes full-text index in test

--- lib/tests/planner.rs
+++ lib/tests/planner.rs
@@ -6,7 +6,7 @@ use surrealdb::err::Error;
 use surrealdb::kvs::Datastore;
 use surrealdb::sql::Value;
 
-async fn test_select_where_iterate_indexes(parallel: bool) -> Result<(), Error> {
+async fn test_select_where_iterate_multi_index(parallel: bool) -> Result<(), Error> {
 	let parallel = if parallel {
 		"PARALLEL"
 	} else {
@@ -14,20 +14,23 @@ async fn test_select_where_iterate_indexes(parallel: bool) -> Result<(), Error> 
 	};
 	let sql = format!(
 		"
-		CREATE person:tobie SET name = 'Tobie', genre='m';
-		CREATE person:jaime SET name = 'Jaime', genre='m';
-		CREATE person:lizzie SET name = 'Lizzie', genre='f';
-		DEFINE INDEX ft_name ON TABLE person COLUMNS name UNIQUE;
+		CREATE person:tobie SET name = 'Tobie', genre='m', company='SurrealDB';
+		CREATE person:jaime SET name = 'Jaime', genre='m', company='SurrealDB';
+		CREATE person:lizzie SET name = 'Lizzie', genre='f', company='SurrealDB';
+		CREATE person:neytiry SET name = 'Neytiri', genre='f', company='Metkayina';
+		DEFINE ANALYZER simple TOKENIZERS blank,class FILTERS lowercase;
+		DEFINE INDEX ft_company ON person FIELDS company SEARCH ANALYZER simple BM25;
+		DEFINE INDEX uniq_name ON TABLE person COLUMNS name UNIQUE;
 		DEFINE INDEX idx_genre ON TABLE person COLUMNS genre;
-		SELECT name FROM person WHERE name = 'Jaime' OR genre = 'm' {parallel};
-	    SELECT name FROM person WHERE name = 'Jaime' OR genre = 'm' {parallel} EXPLAIN FULL;"
+		SELECT name FROM person WHERE name = 'Jaime' OR genre = 'm' OR company @@ 'surrealdb' {parallel};
+	    SELECT name FROM person WHERE name = 'Jaime' OR genre = 'm' OR company @@ 'surrealdb' {parallel} EXPLAIN FULL;"
 	);
 	let dbs = Datastore::new("memory").await?;
 	let ses = Session::for_kv().with_ns("test").with_db("test");
 	let res = &mut dbs.execute(&sql, &ses, None).await?;
-	assert_eq!(res.len(), 7);
+	assert_eq!(res.len(), 10);
 	//
-	for _ in 0..5 {
+	for _ in 0..8 {
 		let _ = res.remove(0).result?;
 	}
 	//
@@ -39,6 +42,9 @@ async fn test_select_where_iterate_indexes(parallel: bool) -> Result<(), Error> 
 			},
             {
 				name: 'Tobie'
+			},
+			{
+				name: 'Lizzie'
 			}
 		]",
 	);
@@ -50,7 +56,7 @@ async fn test_select_where_iterate_indexes(parallel: bool) -> Result<(), Error> 
 				{
 					detail: {
 						plan: {
-							index: 'ft_name',
+							index: 'uniq_name',
 							operator: '=',
 							value: 'Jaime'
 						},
@@ -68,25 +74,36 @@ async fn test_select_where_iterate_indexes(parallel: bool) -> Result<(), Error> 
 						table: 'person',
 					},
 					operation: 'Iterate Index'
-					},
-					{
-						detail: {
-							count: 2
+				},
+				{
+					detail: {
+						plan: {
+							index: 'ft_company',
+							operator: '@@',
+							value: 'surrealdb'
 						},
-						operation: 'Fetch'
-					}
-				]",
+						table: 'person',
+					},
+					operation: 'Iterate Index'
+				},
+				{
+					detail: {
+						count: 3
+					},
+					operation: 'Fetch'
+				}
+			]",
 	);
 	assert_eq!(format!("{:#}", tmp), format!("{:#}", val));
 	Ok(())
 }
 
 #[tokio::test]
-async fn select_where_iterate_indexes() -> Result<(), Error> {
-	test_select_where_iterate_indexes(false).await
+async fn select_where_iterate_multi_index() -> Result<(), Error> {
+	test_select_where_iterate_multi_index(false).await
 }
 
 #[tokio::test]
-async fn select_where_iterate_indexes_parallel() -> Result<(), Error> {
-	test_select_where_iterate_indexes(true).await
+async fn select_where_iterate_multi_index_parallel() -> Result<(), Error> {
+	test_select_where_iterate_multi_index(true).await
 }
