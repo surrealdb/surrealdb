bfaa4301038d2b4b7373e4480b5a761459a22931
Przemyslaw Hugh Kaznowski <hugh@surrealdb.com>
2023-07-21 22:30:59 +0100

    Fix Node LQ to reflect correct path (#2301)

--- lib/src/sql/statements/kill.rs
+++ lib/src/sql/statements/kill.rs
@@ -35,13 +35,13 @@ impl KillStatement {
 		// Claim transaction
 		let mut run = txn.lock().await;
 		// Fetch the live query key
-		let key = crate::key::node::lq::new(opt.id()?, opt.ns(), opt.db(), self.id.0);
+		let key = crate::key::node::lq::new(opt.id()?, self.id.0, opt.ns(), opt.db());
 		// Fetch the live query key if it exists
 		match run.get(key).await? {
 			Some(val) => match std::str::from_utf8(&val) {
 				Ok(tb) => {
 					// Delete the node live query
-					let key = crate::key::node::lq::new(opt.id()?, opt.ns(), opt.db(), self.id.0);
+					let key = crate::key::node::lq::new(opt.id()?, self.id.0, opt.ns(), opt.db());
 					run.del(key).await?;
 					// Delete the table live query
 					let key = crate::key::table::lq::new(opt.ns(), opt.db(), tb, self.id.0);
