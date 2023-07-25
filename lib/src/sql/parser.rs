3df26c8c96817c3842aa253b184c6af64164e946
Mees Delzenne <mees.delzenne@gmail.com>
2023-07-25 15:48:24 +0200

    Redo imports

--- lib/src/sql/parser.rs
+++ lib/src/sql/parser.rs
@@ -2,6 +2,7 @@ use crate::err::Error;
 use crate::sql::error::Error::{Field, Group, Order, Parser, Split};
 use crate::sql::error::IResult;
 use crate::sql::query::{query, Query};
+use crate::sql::subquery::{subquery, Subquery};
 use crate::sql::thing::Thing;
 use crate::sql::value::Value;
 use nom::Err;
@@ -14,10 +15,10 @@ pub fn parse(input: &str) -> Result<Query, Error> {
 	parse_impl(input, query)
 }
 
-/// Parses a SurrealQL [`Query`]
+/// Parses a SurrealQL Subquery [`Subquery`]
 #[instrument(name = "parser", skip_all, fields(length = input.len()))]
-pub fn sub_query(input: &str) -> Result<super::subquery::Subquery, Error> {
-	parse_impl(input, super::subquery::subquery)
+pub fn sub_query(input: &str) -> Result<Subquery, Error> {
+	parse_impl(input, subquery)
 }
 
 /// Parses a SurrealQL [`Thing`]
