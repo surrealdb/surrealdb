cd72897ba0a3d9606cc013b851ac03ddf34673ed
Raphael Darley <raphael@raphaeldarley.com>
2023-07-26 10:19:41 +0100

    add builder for config

--- lib/src/api/opt/config.rs
+++ lib/src/api/opt/config.rs
@@ -17,6 +17,42 @@ pub struct Config {
 	pub(crate) transaction_timeout: Option<Duration>,
 }
 
+impl Config {
+	pub fn new() -> Self {
+		Default::default()
+	}
+
+	pub fn set_strict(mut self, strict: bool) -> Self {
+		self.strict = strict;
+		self
+	}
+
+	pub fn strict(mut self) -> Self {
+		self.strict = true;
+		self
+	}
+
+	pub fn set_notifications(mut self, notifications: bool) -> Self {
+		self.notifications = notifications;
+		self
+	}
+
+	pub fn notifications(mut self) -> Self {
+		self.notifications = true;
+		self
+	}
+
+	pub fn query_timeout(mut self, timeout: impl Into<Option<Duration>>) -> Self {
+		self.query_timeout = timeout.into();
+		self
+	}
+
+	pub fn transaction_timeout(mut self, timeout: impl Into<Option<Duration>>) -> Self {
+		self.transaction_timeout = timeout.into();
+		self
+	}
+}
+
 impl Default for Config {
 	fn default() -> Self {
 		Self {
