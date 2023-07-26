a20907778a21df15ee99c528f1cbd7ff2befed1f
Raphael Darley <raphael@raphaeldarley.com>
2023-07-26 11:08:23 +0100

    add doc comments

--- lib/src/api/opt/config.rs
+++ lib/src/api/opt/config.rs
@@ -9,57 +9,71 @@ use std::time::Duration;
 	feature = "kv-fdb",
 	feature = "kv-indxdb",
 ))]
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Config {
 	pub(crate) strict: bool,
 	pub(crate) notifications: bool,
 	pub(crate) query_timeout: Option<Duration>,
 	pub(crate) transaction_timeout: Option<Duration>,
 }
+#[cfg(not(any(
+	feature = "kv-mem",
+	feature = "kv-tikv",
+	feature = "kv-rocksdb",
+	feature = "kv-speedb",
+	feature = "kv-fdb",
+	feature = "kv-indxdb",
+)))]
+#[derive(Debug, Default)]
+pub struct Config();
 
+#[cfg(any(
+	feature = "kv-mem",
+	feature = "kv-tikv",
+	feature = "kv-rocksdb",
+	feature = "kv-speedb",
+	feature = "kv-fdb",
+	feature = "kv-indxdb",
+))]
 impl Config {
+	///Create a default config that can be modified to configure a connection
 	pub fn new() -> Self {
 		Default::default()
 	}
 
+	///Set the strict value of the config to the supplied value
 	pub fn set_strict(mut self, strict: bool) -> Self {
 		self.strict = strict;
 		self
 	}
 
+	///Set the config to use strict mode
 	pub fn strict(mut self) -> Self {
 		self.strict = true;
 		self
 	}
 
+	///Set the notifications value of the config to the supplied value
 	pub fn set_notifications(mut self, notifications: bool) -> Self {
 		self.notifications = notifications;
 		self
 	}
 
+	///Set the config to use notifications
 	pub fn notifications(mut self) -> Self {
 		self.notifications = true;
 		self
 	}
 
+	///Set the query timeout of the config
 	pub fn query_timeout(mut self, timeout: impl Into<Option<Duration>>) -> Self {
 		self.query_timeout = timeout.into();
 		self
 	}
 
+	///Set the transaction timeout of the config
 	pub fn transaction_timeout(mut self, timeout: impl Into<Option<Duration>>) -> Self {
 		self.transaction_timeout = timeout.into();
 		self
-	}
-}
-
-impl Default for Config {
-	fn default() -> Self {
-		Self {
-			strict: false,
-			notifications: false,
-			query_timeout: None,
-			transaction_timeout: None,
-		}
 	}
 }
