98e3dd6036a6e1253fd4d2d19c655bc8f98c90b9
Raphael Darley <raphael@raphaeldarley.com>
2023-08-02 15:22:16 +0100

    add support for Sender

--- lib/src/api/conn.rs
+++ lib/src/api/conn.rs
@@ -8,6 +8,7 @@ use crate::api::Surreal;
 use crate::opt::from_value;
 use crate::sql::Query;
 use crate::sql::Value;
+use channel;
 use flume::Receiver;
 use flume::Sender;
 use serde::de::DeserializeOwned;
@@ -119,14 +120,16 @@ pub struct Param {
 	pub(crate) query: Option<(Query, BTreeMap<String, Value>)>,
 	pub(crate) other: Vec<Value>,
 	pub(crate) file: Option<PathBuf>,
+	pub(crate) send: Option<channel::Sender<Vec<u8>>>,
 }
 
 impl Param {
 	pub(crate) fn new(other: Vec<Value>) -> Self {
 		Self {
-			other,
 			query: None,
+			other,
 			file: None,
+			send: None,
 		}
 	}
 
@@ -135,6 +138,7 @@ impl Param {
 			query: Some((query, bindings)),
 			other: Vec::new(),
 			file: None,
+			send: None,
 		}
 	}
 
@@ -143,6 +147,16 @@ impl Param {
 			query: None,
 			other: Vec::new(),
 			file: Some(file),
+			send: None,
+		}
+	}
+
+	pub(crate) fn send(send: channel::Sender<Vec<u8>>) -> Self {
+		Self {
+			query: None,
+			other: Vec::new(),
+			file: None,
+			send: Some(send),
 		}
 	}
 }
--- lib/src/api/method/export.rs
+++ lib/src/api/method/export.rs
@@ -5,8 +5,10 @@ use crate::api::Connection;
 use crate::api::Error;
 use crate::api::ExtraFeatures;
 use crate::api::Result;
+use channel::Sender;
 use std::future::Future;
 use std::future::IntoFuture;
+use std::path::Path;
 use std::path::PathBuf;
 use std::pin::Pin;
 
@@ -15,7 +17,7 @@ use std::pin::Pin;
 #[must_use = "futures do nothing unless you `.await` or poll them"]
 pub struct Export<'r, C: Connection> {
 	pub(super) router: Result<&'r Router<C>>,
-	pub(super) file: PathBuf,
+	pub(super) target: Exportable,
 }
 
 impl<'r, Client> IntoFuture for Export<'r, Client>
@@ -32,7 +34,56 @@ where
 				return Err(Error::BackupsNotSupported.into());
 			}
 			let mut conn = Client::new(Method::Export);
-			conn.execute_unit(router, Param::file(self.file)).await
+			match self.target {
+				Exportable::File(f) => conn.execute_unit(router, Param::file(f)).await,
+				Exportable::Send(s) => conn.execute_unit(router, Param::send(s)).await,
+			}
 		})
+	}
+}
+
+#[derive(Debug)]
+pub enum Exportable {
+	File(PathBuf),
+	Send(Sender<Vec<u8>>),
+}
+
+pub trait IntoExportable {
+	fn into_exportable(self) -> Exportable;
+}
+
+impl IntoExportable for &str {
+	fn into_exportable(self) -> Exportable {
+		Exportable::File(<str as AsRef<Path>>::as_ref(self).to_owned())
+	}
+}
+
+impl IntoExportable for String {
+	fn into_exportable(self) -> Exportable {
+		Exportable::File(<str as AsRef<Path>>::as_ref(&self).to_owned())
+	}
+}
+
+impl IntoExportable for &String {
+	fn into_exportable(self) -> Exportable {
+		Exportable::File(<str as AsRef<Path>>::as_ref(self).to_owned())
+	}
+}
+
+impl IntoExportable for &Path {
+	fn into_exportable(self) -> Exportable {
+		Exportable::File(self.to_owned())
+	}
+}
+
+impl IntoExportable for PathBuf {
+	fn into_exportable(self) -> Exportable {
+		Exportable::File(self)
+	}
+}
+
+impl IntoExportable for Sender<Vec<u8>> {
+	fn into_exportable(self) -> Exportable {
+		Exportable::Send(self)
 	}
 }
