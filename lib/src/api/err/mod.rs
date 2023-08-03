e86eead3844261c303ecfeb4b3ac0290c4593e9d
Salvador Girones <salvadorgirones@gmail.com>
2023-08-03 15:07:26 +0200

    [import] Provide descriptive error messages

--- lib/src/api/err/mod.rs
+++ lib/src/api/err/mod.rs
@@ -109,8 +109,8 @@ pub enum Error {
 		error: String,
 	},
 
-	/// Failed to serialize `sql::Value` to JSON string
-	#[error("Failed to serialize `{string}` to JSON string: {error}")]
+	/// Failed to deserialize from JSON string to `sql::Value`
+	#[error("Failed to deserialize `{string}` to sql::Value: {error}")]
 	FromJsonString {
 		string: String,
 		error: String,
