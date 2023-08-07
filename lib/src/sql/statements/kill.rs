97b3c80b68e33670b556a7b6c5802cc1a65217ef
Hugh Kaznowski <hugh@surrealdb.com>
2023-08-07 12:42:55 +0100

    Fixup tests

--- lib/src/sql/statements/kill.rs
+++ lib/src/sql/statements/kill.rs
@@ -14,8 +14,6 @@ use nom::bytes::complete::tag_no_case;
 use nom::combinator::map;
 use serde::{Deserialize, Serialize};
 use std::fmt;
-use std::ops::Deref;
-use std::pin::Pin;
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize, Store, Hash)]
 pub struct KillStatement {
@@ -109,14 +107,38 @@ pub fn kill(i: &str) -> IResult<&str, KillStatement> {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use crate::sql::{Ident, Param, Uuid};
 
 	#[test]
 	fn kill_uuid() {
-		let sql = "kill c005b8da-63a4-48bc-a371-07e95b39d58e";
+		// let uuid_str = Uuid::new_v4().to_string();
+		let uuid_str = "c005b8da-63a4-48bc-a371-07e95b39d58e";
+		let uuid_str_wrapped = format!("'{}'", uuid_str);
+		let sql = format!("kill {}", uuid_str_wrapped);
+		let res = kill(&sql);
+		assert!(res.is_ok(), "{:?}", res);
+		let out = res.unwrap().1;
+		assert_eq!(
+			out,
+			KillStatement {
+				id: Value::Uuid(Uuid::from(uuid::Uuid::parse_str(uuid_str).unwrap()))
+			}
+		);
+		assert_eq!("KILL 'c005b8da-63a4-48bc-a371-07e95b39d58e'", format!("{}", out));
+	}
+
+	#[test]
+	fn kill_param() {
+		let sql = "kill $id";
 		let res = kill(sql);
 		assert!(res.is_ok());
 		let out = res.unwrap().1;
-		assert_eq!(out, KillStatement::Root);
-		assert_eq!("KILL c005b8da-63a4-48bc-a371-07e95b39d58e", format!("{}", out));
+		assert_eq!(
+			out,
+			KillStatement {
+				id: Value::Param(Param(Ident("id".to_string()))),
+			}
+		);
+		assert_eq!("KILL $id", format!("{}", out));
 	}
 }
