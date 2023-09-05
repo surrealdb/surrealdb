5eb505638b158bab5340e7848d5e26bbdfa53228
Hugh Kaznowski <hugh@surrealdb.com>
2023-09-05 16:10:32 +0100

    Test fails correctly

--- lib/src/kvs/tests/cluster_init.rs
+++ lib/src/kvs/tests/cluster_init.rs
@@ -248,7 +248,22 @@ async fn bootstrap_does_not_error_on_missing_live_queries() {
 	let new_node_id = Uuid::parse_str("53f7355d-5be1-4a94-9803-5192b59c5244").unwrap();
 	// There should not be an error
 	let second_node = test.db.with_node_id(crate::sql::uuid::Uuid::from(new_node_id));
-	second_node.bootstrap().await.unwrap();
+	match second_node.bootstrap().await {
+		Ok(_) => {
+			panic!("Expected an error because of missing live query")
+		}
+		Err(Error::Tx(e)) => match e {
+			_ if e.contains("LvNotFound") => {
+				// This is what we want... an LvNotFound error, but it gets wrapped into a string so that Tx doesnt carry vecs
+			}
+			_ => {
+				panic!("Expected an LvNotFound error but got: {:?}", e);
+			}
+		},
+		Err(e) => {
+			panic!("Missing live query error: {:?}", e)
+		}
+	}
 
 	// Verify node live query was deleted
 	let mut tx = second_node.transaction(true, false).await.unwrap();
