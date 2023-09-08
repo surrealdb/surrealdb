d886b19e45e8e2a25cb79b75f1fc5881d2d3e01c
Salvador Girones <salvadorgirones@gmail.com>
2023-09-08 16:23:21 +0200

    [logging] Move HTTP/WS logs to debug

--- src/telemetry/traces/rpc.rs
+++ src/telemetry/traces/rpc.rs
@@ -2,7 +2,7 @@ use tracing::{field, Span};
 use uuid::Uuid;
 
 pub fn span_for_request(ws_id: &Uuid) -> Span {
-	let span = tracing::info_span!(
+	let span = tracing::debug_span!(
 		// Dynamic span names need to be 'recorded', can't be used on the macro. Use a static name here and overwrite later on
 		"rpc/call",
 		otel.name = field::Empty,
