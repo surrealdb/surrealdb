e4c6d817c475080283a227ca0f3e84252d827081
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2023-09-12 16:43:32 +0100

    Feature: Support of range queries in the query planner (#2656)

--- lib/src/idx/planner/executor.rs
+++ lib/src/idx/planner/executor.rs
@@ -6,36 +6,60 @@ use crate::idx::ft::termdocs::TermsDocs;
 use crate::idx::ft::terms::TermId;
 use crate::idx::ft::{FtIndex, MatchRef};
 use crate::idx::planner::iterators::{
-	MatchesThingIterator, NonUniqueEqualThingIterator, ThingIterator, UniqueEqualThingIterator,
+	IndexEqualThingIterator, IndexRangeThingIterator, MatchesThingIterator, ThingIterator,
+	UniqueEqualThingIterator, UniqueRangeThingIterator,
 };
-use crate::idx::planner::plan::IndexOption;
-use crate::idx::planner::tree::IndexMap;
+use crate::idx::planner::plan::IndexOperator::Matches;
+use crate::idx::planner::plan::{IndexOperator, IndexOption, RangeValue};
+use crate::idx::planner::tree::{IndexMap, IndexRef};
 use crate::idx::trees::store::TreeStoreType;
 use crate::idx::IndexKeyBase;
 use crate::kvs;
 use crate::kvs::Key;
 use crate::sql::index::Index;
-use crate::sql::{Expression, Operator, Table, Thing, Value};
-use std::collections::HashMap;
+use crate::sql::statements::DefineIndexStatement;
+use crate::sql::{Expression, Object, Table, Thing, Value};
+use std::collections::{HashMap, HashSet};
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
-pub(crate) type IteratorRef = u16;
-
 pub(crate) struct QueryExecutor {
 	table: String,
-	ft_map: HashMap<String, FtIndex>,
+	ft_map: HashMap<IndexRef, FtIndex>,
 	mr_entries: HashMap<MatchRef, FtEntry>,
-	exp_entries: HashMap<Expression, FtEntry>,
-	iterators: Vec<Expression>,
+	exp_entries: HashMap<Arc<Expression>, FtEntry>,
+	it_entries: Vec<IteratorEntry>,
+	index_definitions: HashMap<IndexRef, DefineIndexStatement>,
 }
 
+pub(crate) type IteratorRef = u16;
+
+pub(super) enum IteratorEntry {
+	Single(Arc<Expression>, IndexOption),
+	Range(HashSet<Arc<Expression>>, IndexRef, RangeValue, RangeValue),
+}
+
+impl IteratorEntry {
+	pub(super) fn explain(&self, e: &mut HashMap<&str, Value>) -> IndexRef {
+		match self {
+			Self::Single(_, io) => {
+				io.explain(e);
+				io.ir()
+			}
+			Self::Range(_, ir, from, to) => {
+				e.insert("from", Value::from(from));
+				e.insert("to", Value::from(to));
+				*ir
+			}
+		}
+	}
+}
 impl QueryExecutor {
 	pub(super) async fn new(
 		opt: &Options,
 		txn: &Transaction,
 		table: &Table,
-		index_map: IndexMap,
+		im: IndexMap,
 	) -> Result<Self, Error> {
 		let mut run = txn.lock().await;
 
@@ -45,28 +69,29 @@ impl QueryExecutor {
 
 		// Create all the instances of FtIndex
 		// Build the FtEntries and map them to Expressions and MatchRef
-		for (exp, io) in index_map.consume() {
+		for (exp, io) in im.options {
 			let mut entry = None;
-			if let Index::Search(p) = &io.ix().index {
-				let ixn = &io.ix().name.0;
-				if let Some(ft) = ft_map.get(ixn) {
-					if entry.is_none() {
-						entry = FtEntry::new(&mut run, ft, io).await?;
+			let ir = io.ir();
+			if let Some(idx_def) = im.definitions.get(&ir) {
+				if let Index::Search(p) = &idx_def.index {
+					if let Some(ft) = ft_map.get(&ir) {
+						if entry.is_none() {
+							entry = FtEntry::new(&mut run, ft, io).await?;
+						}
+					} else {
+						let ikb = IndexKeyBase::new(opt, idx_def);
+						let az = run.get_db_analyzer(opt.ns(), opt.db(), p.az.as_str()).await?;
+						let ft = FtIndex::new(&mut run, az, ikb, p, TreeStoreType::Read).await?;
+						if entry.is_none() {
+							entry = FtEntry::new(&mut run, &ft, io).await?;
+						}
+						ft_map.insert(ir, ft);
 					}
-				} else {
-					let ikb = IndexKeyBase::new(opt, io.ix());
-					let az = run.get_db_analyzer(opt.ns(), opt.db(), p.az.as_str()).await?;
-					let ft = FtIndex::new(&mut run, az, ikb, p, TreeStoreType::Read).await?;
-					let ixn = ixn.to_owned();
-					if entry.is_none() {
-						entry = FtEntry::new(&mut run, &ft, io).await?;
-					}
-					ft_map.insert(ixn, ft);
 				}
 			}
 
 			if let Some(e) = entry {
-				if let Some(mr) = e.0.index_option.match_ref() {
+				if let Matches(_, Some(mr)) = e.0.index_option.op() {
 					if mr_entries.insert(*mr, e.clone()).is_some() {
 						return Err(Error::DuplicatedMatchRef {
 							mr: *mr,
@@ -82,22 +107,41 @@ impl QueryExecutor {
 			ft_map,
 			mr_entries,
 			exp_entries,
-			iterators: Vec::new(),
+			it_entries: Vec::new(),
+			index_definitions: im.definitions,
 		})
 	}
 
-	pub(super) fn add_iterator(&mut self, exp: Expression) -> IteratorRef {
-		let ir = self.iterators.len();
-		self.iterators.push(exp);
+	pub(super) fn add_iterator(&mut self, it_entry: IteratorEntry) -> IteratorRef {
+		let ir = self.it_entries.len();
+		self.it_entries.push(it_entry);
 		ir as IteratorRef
 	}
 
 	pub(crate) fn is_distinct(&self, ir: IteratorRef) -> bool {
-		(ir as usize) < self.iterators.len()
+		(ir as usize) < self.it_entries.len()
 	}
 
-	pub(crate) fn get_iterator_expression(&self, ir: IteratorRef) -> Option<&Expression> {
-		self.iterators.get(ir as usize)
+	pub(crate) fn is_iterator_expression(&self, ir: IteratorRef, exp: &Expression) -> bool {
+		match self.it_entries.get(ir as usize) {
+			Some(IteratorEntry::Single(e, ..)) => exp.eq(e.as_ref()),
+			Some(IteratorEntry::Range(es, ..)) => es.contains(exp),
+			_ => false,
+		}
+	}
+
+	pub(crate) fn explain(&self, itr: IteratorRef) -> Value {
+		match self.it_entries.get(itr as usize) {
+			Some(ie) => {
+				let mut e = HashMap::default();
+				let ir = ie.explain(&mut e);
+				if let Some(ix) = self.index_definitions.get(&ir) {
+					e.insert("index", Value::from(ix.name.0.to_owned()));
+				}
+				Value::from(Object::from(e))
+			}
+			None => Value::None,
+		}
 	}
 
 	fn get_match_ref(match_ref: &Value) -> Option<MatchRef> {
@@ -113,43 +157,87 @@ impl QueryExecutor {
 		&self,
 		opt: &Options,
 		ir: IteratorRef,
-		io: IndexOption,
 	) -> Result<Option<ThingIterator>, Error> {
-		match &io.ix().index {
-			Index::Idx => Self::new_index_iterator(opt, io),
-			Index::Uniq => Self::new_unique_index_iterator(opt, io),
-			Index::Search {
-				..
-			} => self.new_search_index_iterator(ir, io).await,
-			_ => Err(Error::FeatureNotYetImplemented {
-				feature: "VectorSearch iterator".to_string(),
-			}),
+		if let Some(it_entry) = self.it_entries.get(ir as usize) {
+			match it_entry {
+				IteratorEntry::Single(_, io) => {
+					if let Some(ix) = self.index_definitions.get(&io.ir()) {
+						match ix.index {
+							Index::Idx => Self::new_index_iterator(opt, ix, io.clone()),
+							Index::Uniq => Self::new_unique_index_iterator(opt, ix, io.clone()),
+							Index::Search {
+								..
+							} => self.new_search_index_iterator(ir, io.clone()).await,
+							Index::MTree(_) => Err(Error::FeatureNotYetImplemented {
+								feature: "VectorSearch iterator".to_string(),
+							}),
+						}
+					} else {
+						Ok(None)
+					}
+				}
+				IteratorEntry::Range(_, ir, from, to) => {
+					Ok(self.new_range_iterator(opt, *ir, from, to)?)
+				}
+			}
+		} else {
+			Ok(None)
 		}
 	}
 
-	fn new_index_iterator(opt: &Options, io: IndexOption) -> Result<Option<ThingIterator>, Error> {
-		if io.op() == &Operator::Equal {
-			return Ok(Some(ThingIterator::NonUniqueEqual(NonUniqueEqualThingIterator::new(
-				opt,
-				io.ix(),
-				io.array(),
-			)?)));
+	fn new_index_iterator(
+		opt: &Options,
+		ix: &DefineIndexStatement,
+		io: IndexOption,
+	) -> Result<Option<ThingIterator>, Error> {
+		match io.op() {
+			IndexOperator::Equality(array) => {
+				Ok(Some(ThingIterator::IndexEqual(IndexEqualThingIterator::new(opt, ix, array)?)))
+			}
+			IndexOperator::RangePart(_, _) => Ok(None), // TODO
+			_ => Ok(None),
+		}
+	}
+
+	fn new_range_iterator(
+		&self,
+		opt: &Options,
+		ir: IndexRef,
+		from: &RangeValue,
+		to: &RangeValue,
+	) -> Result<Option<ThingIterator>, Error> {
+		if let Some(ix) = self.index_definitions.get(&ir) {
+			match ix.index {
+				Index::Idx => {
+					return Ok(Some(ThingIterator::IndexRange(IndexRangeThingIterator::new(
+						opt, ix, from, to,
+					))))
+				}
+				Index::Uniq => {
+					return Ok(Some(ThingIterator::UniqueRange(UniqueRangeThingIterator::new(
+						opt, ix, from, to,
+					))))
+				}
+				_ => {}
+			}
 		}
 		Ok(None)
 	}
 
 	fn new_unique_index_iterator(
 		opt: &Options,
+		ix: &DefineIndexStatement,
 		io: IndexOption,
 	) -> Result<Option<ThingIterator>, Error> {
-		if io.op() == &Operator::Equal {
-			return Ok(Some(ThingIterator::UniqueEqual(UniqueEqualThingIterator::new(
-				opt,
-				io.ix(),
-				io.array(),
-			)?)));
+		match io.op() {
+			IndexOperator::Equality(array) => {
+				Ok(Some(ThingIterator::UniqueEqual(UniqueEqualThingIterator::new(opt, ix, array)?)))
+			}
+			IndexOperator::RangePart(_, _) => {
+				todo!()
+			}
+			_ => Ok(None),
 		}
-		Ok(None)
 	}
 
 	async fn new_search_index_iterator(
@@ -157,11 +245,10 @@ impl QueryExecutor {
 		ir: IteratorRef,
 		io: IndexOption,
 	) -> Result<Option<ThingIterator>, Error> {
-		if let Some(exp) = self.iterators.get(ir as usize) {
-			if let Operator::Matches(_) = io.op() {
-				let ixn = &io.ix().name.0;
-				if let Some(fti) = self.ft_map.get(ixn) {
-					if let Some(fte) = self.exp_entries.get(exp) {
+		if let Some(IteratorEntry::Single(exp, ..)) = self.it_entries.get(ir as usize) {
+			if let Matches(_, _) = io.op() {
+				if let Some(fti) = self.ft_map.get(&io.ir()) {
+					if let Some(fte) = self.exp_entries.get(exp.as_ref()) {
 						let it = MatchesThingIterator::new(fti, fte.0.terms_docs.clone()).await?;
 						return Ok(Some(ThingIterator::Matches(it)));
 					}
@@ -223,7 +310,7 @@ impl QueryExecutor {
 
 	fn get_ft_entry_and_index(&self, match_ref: &Value) -> Option<(&FtEntry, &FtIndex)> {
 		if let Some(e) = self.get_ft_entry(match_ref) {
-			if let Some(ft) = self.ft_map.get(&e.0.index_option.ix().name.0) {
+			if let Some(ft) = self.ft_map.get(&e.0.index_option.ir()) {
 				return Some((e, ft));
 			}
 		}
@@ -304,7 +391,7 @@ impl FtEntry {
 		ft: &FtIndex,
 		io: IndexOption,
 	) -> Result<Option<Self>, Error> {
-		if let Some(qs) = io.qs() {
+		if let Matches(qs, _) = io.op() {
 			let terms = ft.extract_terms(tx, qs.to_owned()).await?;
 			let terms_docs = Arc::new(ft.get_terms_docs(tx, &terms).await?);
 			Ok(Some(Self(Arc::new(Inner {
