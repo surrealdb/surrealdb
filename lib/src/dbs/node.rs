853956be0db67b3cdcf905ec5fd4e5cdc0d66e39
Hugh Kaznowski <hugh@surrealdb.com>
2023-10-05 17:29:31 +0100

    Revert "Remove Default"
    This reverts commit 6cbd67f3fa356c4e8cb91eb2fe74de6ad5f2763e.

--- lib/src/dbs/node.rs
+++ lib/src/dbs/node.rs
@@ -19,7 +19,7 @@ pub struct ClusterMembership {
 // This struct is meant to represent a timestamp that can be used to partially order
 // events in a cluster. It should be derived from a timestamp oracle, such as the
 // one available in TiKV via the client `TimestampExt` implementation.
-#[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize, PartialOrd, Hash, Store)]
+#[derive(Clone, Debug, Eq, PartialEq, Deserialize, Serialize, PartialOrd, Hash, Store, Default)]
 #[revisioned(revision = 1)]
 pub struct Timestamp {
 	pub value: u64,
