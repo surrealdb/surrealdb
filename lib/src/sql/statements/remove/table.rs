96fbec82bd4ad4ffe564523f21b66faabcb18e96
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-10-06 18:07:00 -0400

    feat: remove multiple tables

--- lib/src/sql/statements/remove/table.rs
+++ lib/src/sql/statements/remove/table.rs
@@ -10,7 +10,10 @@ use crate::sql::ident::{ident, Ident};
 use crate::sql::value::Value;
 use derive::Store;
 use nom::bytes::complete::tag_no_case;
+use nom::bytes::streaming::tag;
 use nom::combinator::cut;
+use nom::multi::separated_list1;
+use nom::sequence::pair;
 use revision::revisioned;
 use serde::{Deserialize, Serialize};
 use std::fmt::{self, Display, Formatter};
@@ -18,7 +21,7 @@ use std::fmt::{self, Display, Formatter};
 #[derive(Clone, Debug, Default, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Store, Hash)]
 #[revisioned(revision = 1)]
 pub struct RemoveTableStatement {
-	pub name: Ident,
+	pub names: Vec<Ident>,
 }
 
 impl RemoveTableStatement {
@@ -35,21 +38,23 @@ impl RemoveTableStatement {
 		let mut run = txn.lock().await;
 		// Clear the cache
 		run.clear_cache();
-		// Get the defined table
-		let tb = run.get_tb(opt.ns(), opt.db(), &self.name).await?;
-		// Delete the definition
-		let key = crate::key::database::tb::new(opt.ns(), opt.db(), &self.name);
-		run.del(key).await?;
-		// Remove the resource data
-		let key = crate::key::table::all::new(opt.ns(), opt.db(), &self.name);
-		run.delp(key, u32::MAX).await?;
-		// Check if this is a foreign table
-		if let Some(view) = &tb.view {
-			// Process each foreign table
-			for v in view.what.0.iter() {
-				// Save the view config
-				let key = crate::key::table::ft::new(opt.ns(), opt.db(), v, &self.name);
-				run.del(key).await?;
+		for name in &self.names {
+			// Get the defined table
+			let tb = run.get_tb(opt.ns(), opt.db(), &name).await?;
+			// Delete the definition
+			let key = crate::key::database::tb::new(opt.ns(), opt.db(), &name);
+			run.del(key).await?;
+			// Remove the resource data
+			let key = crate::key::table::all::new(opt.ns(), opt.db(), &name);
+			run.delp(key, u32::MAX).await?;
+			// Check if this is a foreign table
+			if let Some(view) = &tb.view {
+				// Process each foreign table
+				for v in view.what.0.iter() {
+					// Save the view config
+					let key = crate::key::table::ft::new(opt.ns(), opt.db(), v, &name);
+					run.del(key).await?;
+				}
 			}
 		}
 		// Ok all good
@@ -59,18 +64,41 @@ impl RemoveTableStatement {
 
 impl Display for RemoveTableStatement {
 	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-		write!(f, "REMOVE TABLE {}", self.name)
+		write!(
+			f,
+			"REMOVE TABLE {}",
+			self.names.iter().map(|v| v.clone().0).collect::<Vec<String>>().join(", ")
+		)
 	}
 }
 
 pub fn table(i: &str) -> IResult<&str, RemoveTableStatement> {
 	let (i, _) = tag_no_case("TABLE")(i)?;
 	let (i, _) = shouldbespace(i)?;
-	let (i, name) = cut(ident)(i)?;
+	let (i, names) = cut(separated_list1(pair(tag(","), shouldbespace), ident))(i)?;
 	Ok((
 		i,
 		RemoveTableStatement {
-			name,
+			names,
 		},
 	))
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_table() {
+		let (rem, res) = table("TABLE foo").unwrap();
+		assert_eq!(rem, "");
+		assert_eq!(res.names, vec![Ident("foo".to_string())]);
+	}
+
+	#[test]
+	fn test_table_multi() {
+		let (rem, res) = table("TABLE foo, bar").unwrap();
+		assert_eq!(rem, "");
+		assert_eq!(res.names, vec![Ident("foo".to_string()), Ident("bar".to_string())]);
+	}
 }
