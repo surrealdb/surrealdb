a1d0523fbe3a1cce997b756a685ad60678c28c2b
Micha de Vries <micha@devrie.sh>
2023-10-14 00:15:54 +0200

    Throw `Error::ConvertTo` for invalid passed values

--- lib/src/fnc/type.rs
+++ lib/src/fnc/type.rs
@@ -114,13 +114,16 @@ pub fn thing((arg1, arg2): (Value, Option<Value>)) -> Result<Value, Error> {
 		})
 	} else {
 		match arg1 {
-			Value::Thing(v) => v.into(),
-			Value::Strand(v) => match Thing::try_from(v) {
-				Ok(v) => v.into(),
-				_ => Value::None,
-			},
-			_ => Value::None,
-		}
+			Value::Thing(v) => Ok(v),
+			Value::Strand(v) => Thing::try_from(v.as_str()).map_err(move |_| Error::ConvertTo {
+				from: Value::Strand(v),
+				into: "record".into(),
+			}),
+			v => Err(Error::ConvertTo {
+				from: v,
+				into: "record".into(),
+			})
+		}?.into()
 	})
 }
 
