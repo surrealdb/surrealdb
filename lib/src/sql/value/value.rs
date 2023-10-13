5fdbf818742cf791eeb0e8bd485f2aa61239771f
Micha de Vries <micha@devrie.sh>
2023-10-13 22:00:58 +0200

    Apply suggestion from review

--- lib/src/sql/value/value.rs
+++ lib/src/sql/value/value.rs
@@ -2134,16 +2134,13 @@ impl Value {
 
 	/// Try to convert this value to a Record or `Thing`
 	pub(crate) fn convert_to_record(self) -> Result<Thing, Error> {
-		match self.clone() {
+		match self {
 			// Records are allowed
 			Value::Thing(v) => Ok(v),
-			Value::Strand(v) => match Thing::try_from(v) {
-				Ok(v) => Ok(v),
-				_ => Err(Error::ConvertTo {
-					from: self,
-					into: "record".into(),
-				}),
-			},
+			Value::Strand(v) => Thing::try_from(v.as_str()).map_err(move |_| Error::ConvertTo {
+				from: Value::Strand(v),
+				into: "record".into(),
+			}),
 			// Anything else raises an error
 			_ => Err(Error::ConvertTo {
 				from: self,
