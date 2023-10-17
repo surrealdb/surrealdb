e0c87f704e5c70c48420b49c917b027d6f733b0a
Hugh Kaznowski <hugh@surrealdb.com>
2023-10-17 10:09:57 +0100

    Fixup string format

--- lib/src/kvs/tests/cluster_init.rs
+++ lib/src/kvs/tests/cluster_init.rs
@@ -51,7 +51,7 @@ async fn expired_nodes_are_garbage_collected() {
 	let scanned = tx.scan_nd(100).await.unwrap();
 	assert_eq!(scanned.len(), 1);
 	for cl in scanned.iter() {
-		assert_eq!(&cl.name, &new_node.to_string());
+		assert_eq!(cl.name, new_node.0.to_string());
 	}
 
 	tx.commit().await.unwrap();
