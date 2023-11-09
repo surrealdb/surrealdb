357faa4b82d2be4b5081bfbc32d61a0ed84743f5
Rushmore Mushambi <rushmore@surrealdb.com>
2023-11-09 16:57:53 +0200

    Improve isolation in live query API tests

--- lib/tests/api/live.rs
+++ lib/tests/api/live.rs
@@ -10,16 +10,17 @@ use surrealdb::Notification;
 async fn live_select_table() {
 	let db = new_db().await;
 	db.use_ns(NS).use_db(Ulid::new().to_string()).await.unwrap();
-	let table = "user";
 
 	{
+		let table = Ulid::new().to_string();
+
 		// Start listening
-		let mut users = db.select(table).live().await.unwrap();
+		let mut users = db.select(&table).live().await.unwrap();
 
 		// Create a record
 		let created: Vec<RecordId> = db.create(table).await.unwrap();
 		// Pull the notification
-		let notification: Notification<RecordId> = users.try_next().await.unwrap().unwrap();
+		let notification: Notification<RecordId> = users.next().await.unwrap().unwrap();
 		// The returned record should match the created record
 		assert_eq!(created, vec![notification.data.clone()]);
 		// It should be newly created
@@ -29,24 +30,26 @@ async fn live_select_table() {
 		let _: Option<RecordId> =
 			db.update(&notification.data.id).content(json!({"foo": "bar"})).await.unwrap();
 		// Pull the notification
-		let notification: Notification<RecordId> = users.try_next().await.unwrap().unwrap();
+		let notification: Notification<RecordId> = users.next().await.unwrap().unwrap();
 		// It should be updated
 		assert_eq!(notification.action, Action::Update);
 
 		// Delete the record
 		let _: Option<RecordId> = db.delete(&notification.data.id).await.unwrap();
 		// Pull the notification
-		let notification: Notification<RecordId> = users.try_next().await.unwrap().unwrap();
+		let notification: Notification<RecordId> = users.next().await.unwrap().unwrap();
 		// It should be deleted
 		assert_eq!(notification.action, Action::Delete);
 	}
 
 	{
+		let table = Ulid::new().to_string();
+
 		// Start listening
-		let mut users = db.select(Resource::from(table)).live().await.unwrap();
+		let mut users = db.select(Resource::from(&table)).live().await.unwrap();
 
 		// Create a record
-		db.create(Resource::from(table)).await.unwrap();
+		db.create(Resource::from(&table)).await.unwrap();
 		// Pull the notification
 		let notification = users.next().await.unwrap();
 		// The returned record should be an object
@@ -60,11 +63,12 @@ async fn live_select_table() {
 async fn live_select_record_id() {
 	let db = new_db().await;
 	db.use_ns(NS).use_db(Ulid::new().to_string()).await.unwrap();
-	let record_id = ("user", "john");
 
 	{
+		let record_id = Thing::from((Ulid::new().to_string(), "john".to_owned()));
+
 		// Start listening
-		let mut users = db.select(record_id).live().await.unwrap();
+		let mut users = db.select(&record_id).live().await.unwrap();
 
 		// Create a record
 		let created: Option<RecordId> = db.create(record_id).await.unwrap();
@@ -92,8 +96,10 @@ async fn live_select_record_id() {
 	}
 
 	{
+		let record_id = Thing::from((Ulid::new().to_string(), "john".to_owned()));
+
 		// Start listening
-		let mut users = db.select(Resource::from(record_id)).live().await.unwrap();
+		let mut users = db.select(Resource::from(&record_id)).live().await.unwrap();
 
 		// Create a record
 		db.create(Resource::from(record_id)).await.unwrap();
@@ -110,11 +116,12 @@ async fn live_select_record_id() {
 async fn live_select_record_ranges() {
 	let db = new_db().await;
 	db.use_ns(NS).use_db(Ulid::new().to_string()).await.unwrap();
-	let table = "user";
 
 	{
+		let table = Ulid::new().to_string();
+
 		// Start listening
-		let mut users = db.select(table).range("jane".."john").live().await.unwrap();
+		let mut users = db.select(&table).range("jane".."john").live().await.unwrap();
 
 		// Create a record
 		let created: Option<RecordId> = db.create((table, "jane")).await.unwrap();
@@ -142,9 +149,11 @@ async fn live_select_record_ranges() {
 	}
 
 	{
+		let table = Ulid::new().to_string();
+
 		// Start listening
 		let mut users =
-			db.select(Resource::from(table)).range("jane".."john").live().await.unwrap();
+			db.select(Resource::from(&table)).range("jane".."john").live().await.unwrap();
 
 		// Create a record
 		db.create(Resource::from((table, "job"))).await.unwrap();
