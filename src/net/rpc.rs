3f43e741c2744a40948ca4109edbe30c2145ede6
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2023-11-20 19:08:07 +0000

    Improve WebSocket performance (#2999)
    Co-authored-by: Salvador Girones Gil <salvadorgirones@gmail.com>

--- src/net/rpc.rs
+++ src/net/rpc.rs
@@ -1,3 +1,4 @@
+use crate::cnf;
 use crate::rpc::connection::Connection;
 use axum::routing::get;
 use axum::Extension;
@@ -25,19 +26,25 @@ async fn handler(
 	Extension(sess): Extension<Session>,
 	Extension(req_id): Extension<RequestId>,
 ) -> impl IntoResponse {
-	// finalize the upgrade process by returning upgrade callback.
-	// we can customize the callback by sending additional info such as address.
-	ws.on_upgrade(move |socket| handle_socket(socket, sess, req_id))
+	ws
+		// Set the maximum frame size
+		.max_frame_size(*cnf::WEBSOCKET_MAX_FRAME_SIZE)
+		// Set the maximum message size
+		.max_message_size(*cnf::WEBSOCKET_MAX_MESSAGE_SIZE)
+		// Set the potential WebSocket protocol formats
+		.protocols(["surrealql-binary", "json", "cbor", "messagepack"])
+		// Handle the WebSocket upgrade and process messages
+		.on_upgrade(move |socket| handle_socket(socket, sess, req_id))
 }
 
 async fn handle_socket(ws: WebSocket, sess: Session, req_id: RequestId) {
+	// Create a new connection instance
 	let rpc = Connection::new(sess);
-
 	// Update the WebSocket ID with the Request ID
 	if let Ok(Ok(req_id)) = req_id.header_value().to_str().map(Uuid::parse_str) {
 		// If the ID couldn't be updated, ignore the error and keep the default ID
 		let _ = rpc.write().await.update_ws_id(req_id).await;
 	}
-
+	// Serve the socket connection requests
 	Connection::serve(rpc, ws).await;
 }
