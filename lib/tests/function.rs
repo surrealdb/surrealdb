37aa344de53cbb4ff230f8cad7428e149629e77a
Micha de Vries <micha@devrie.sh>
2023-11-27 18:25:36 +0000

    fix statement count in test

--- lib/tests/function.rs
+++ lib/tests/function.rs
@@ -6298,7 +6298,7 @@ async fn function_custom_optional_args() -> Result<(), Error> {
 	let dbs = new_ds().await?;
 	let ses = Session::owner().with_ns("test").with_db("test");
 	let res = &mut dbs.execute(sql, &ses, None).await?;
-	assert_eq!(res.len(), 11);
+	assert_eq!(res.len(), 14);
 	//
 	let tmp = res.remove(0).result?;
 	let val = Value::None;
