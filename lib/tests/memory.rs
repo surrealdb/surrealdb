9b052917ce0f3d50371da58d8e179f7c030fc8f2
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2023-12-01 19:31:31 +0000

    Add features [re-mem] to enable the tests using re_memory

--- lib/tests/memory.rs
+++ lib/tests/memory.rs
@@ -1,6 +1,7 @@
 mod helpers;
 
 // We use a separated package so that only these package is using PeakAlloc
+#[cfg(not(feature = "re-mem"))]
 mod memory_tests {
 	use crate::helpers::new_ds;
 	extern crate peak_alloc;
@@ -82,33 +83,35 @@ mod memory_tests {
 	}
 }
 
-// mod re_memory_tests {
-// 	use crate::helpers::new_ds;
-// 	use re_memory::accounting_allocator::{set_tracking_callstacks, tracking_stats};
-// 	use re_memory::AccountingAllocator;
-// 	use surrealdb::dbs::Session;
-// 	use surrealdb::err::Error;
-//
-// 	#[global_allocator]
-// 	static GLOBAL: AccountingAllocator<std::alloc::System> =
-// 		AccountingAllocator::new(std::alloc::System);
-//
-// 	#[tokio::test]
-// 	async fn re_memory_test_dbs() -> Result<(), Error> {
-// 		set_tracking_callstacks(true);
-// 		let dbs = new_ds().await?;
-// 		let ses = Session::owner().with_ns("test").with_db("test");
-//
-// 		for _ in 0..10000 {
-// 			let res = &mut dbs.execute("SELECT * FROM nothing", &ses, None).await?;
-// 			assert_eq!(res.len(), 1);
-// 			let _ = res.remove(0).result?;
-// 		}
-// 		if let Some(ts) = tracking_stats() {
-// 			for cs in ts.top_callstacks {
-// 				println!("{}", cs.readable_backtrace);
-// 			}
-// 		}
-// 		Ok(())
-// 	}
-// }
+#[cfg(feature = "re-mem")]
+mod re_memory_tests {
+	use crate::helpers::new_ds;
+	use re_memory::accounting_allocator::{set_tracking_callstacks, tracking_stats};
+	use re_memory::AccountingAllocator;
+	use surrealdb::dbs::Session;
+	use surrealdb::err::Error;
+
+	#[global_allocator]
+	static GLOBAL: AccountingAllocator<std::alloc::System> =
+		AccountingAllocator::new(std::alloc::System);
+
+	#[tokio::test]
+	async fn re_memory_test_dbs() -> Result<(), Error> {
+		set_tracking_callstacks(true);
+		let dbs = new_ds().await?;
+		let ses = Session::owner().with_ns("test").with_db("test");
+
+		for _ in 0..10000 {
+			let res = &mut dbs.execute("SELECT * FROM nothing", &ses, None).await?;
+			assert_eq!(res.len(), 1);
+			let _ = res.remove(0).result?;
+		}
+		if let Some(ts) = tracking_stats() {
+			for cs in &ts.top_callstacks {
+				println!("{}", cs.readable_backtrace);
+			}
+			assert_eq!(ts.top_callstacks.len(), 0);
+		}
+		Ok(())
+	}
+}
