01a07b7c91e42e194d04b7ebbf166523ad066f93
Mees Delzenne <DelSkayn@users.noreply.github.com>
2023-12-09 21:25:50 +0100

    Fix clippy warnings in tests (#3101)

--- lib/src/idx/trees/mtree.rs
+++ lib/src/idx/trees/mtree.rs
@@ -2041,7 +2041,7 @@ mod tests {
 		let mut c = 0;
 		for (doc_id, obj) in collection.as_ref() {
 			{
-				let (s, mut tx) = new_operation(&ds, TreeStoreType::Write).await;
+				let (s, mut tx) = new_operation(ds, TreeStoreType::Write).await;
 				let mut s = s.lock().await;
 				t.insert(&mut tx, &mut s, obj.as_ref().clone(), *doc_id).await?;
 				finish_operation(tx, s, true).await?;
@@ -2049,9 +2049,9 @@ mod tests {
 			}
 			c += 1;
 			{
-				let (s, mut tx) = new_operation(&ds, TreeStoreType::Traversal).await;
+				let (s, mut tx) = new_operation(ds, TreeStoreType::Traversal).await;
 				let mut s = s.lock().await;
-				let p = check_tree_properties(&mut tx, &mut s, &t).await?;
+				let p = check_tree_properties(&mut tx, &mut s, t).await?;
 				assert_eq!(p.doc_count, c);
 			}
 		}
@@ -2065,7 +2065,7 @@ mod tests {
 	) -> Result<HashMap<DocId, SharedVector>, Error> {
 		let mut map = HashMap::with_capacity(collection.as_ref().len());
 		{
-			let (s, mut tx) = new_operation(&ds, TreeStoreType::Write).await;
+			let (s, mut tx) = new_operation(ds, TreeStoreType::Write).await;
 			let mut s = s.lock().await;
 			for (doc_id, obj) in collection.as_ref() {
 				t.insert(&mut tx, &mut s, obj.as_ref().clone(), *doc_id).await?;
@@ -2074,9 +2074,9 @@ mod tests {
 			finish_operation(tx, s, true).await?;
 		}
 		{
-			let (s, mut tx) = new_operation(&ds, TreeStoreType::Traversal).await;
+			let (s, mut tx) = new_operation(ds, TreeStoreType::Traversal).await;
 			let mut s = s.lock().await;
-			check_tree_properties(&mut tx, &mut s, &t).await?;
+			check_tree_properties(&mut tx, &mut s, t).await?;
 		}
 		Ok(map)
 	}
@@ -2108,13 +2108,13 @@ mod tests {
 			{
 				let (s, mut tx) = new_operation(ds, TreeStoreType::Traversal).await;
 				let mut s = s.lock().await;
-				check_tree_properties(&mut tx, &mut s, &t).await?;
+				check_tree_properties(&mut tx, &mut s, t).await?;
 			}
 		}
 
-		let (s, mut tx) = new_operation(&ds, TreeStoreType::Traversal).await;
+		let (s, mut tx) = new_operation(ds, TreeStoreType::Traversal).await;
 		let mut s = s.lock().await;
-		check_tree_properties(&mut tx, &mut s, &t).await?.check(0, 0, None, None, 0, 0);
+		check_tree_properties(&mut tx, &mut s, t).await?.check(0, 0, None, None, 0, 0);
 		Ok(())
 	}
 
@@ -2123,7 +2123,7 @@ mod tests {
 		t: &mut MTree,
 		collection: &TestCollection,
 	) -> Result<(), Error> {
-		let (s, mut tx) = new_operation(&ds, TreeStoreType::Read).await;
+		let (s, mut tx) = new_operation(ds, TreeStoreType::Read).await;
 		let mut s = s.lock().await;
 		let max_knn = 20.max(collection.as_ref().len());
 		for (doc_id, obj) in collection.as_ref() {
@@ -2162,9 +2162,9 @@ mod tests {
 		t: &mut MTree,
 		map: &HashMap<DocId, SharedVector>,
 	) -> Result<(), Error> {
-		let (s, mut tx) = new_operation(&ds, TreeStoreType::Read).await;
+		let (s, mut tx) = new_operation(ds, TreeStoreType::Read).await;
 		let mut s = s.lock().await;
-		for (_, obj) in map {
+		for obj in map.values() {
 			let res = t.knn_search(&mut tx, &mut s, obj, map.len()).await?;
 			assert_eq!(
 				map.len(),
@@ -2234,7 +2234,7 @@ mod tests {
 	impl AsRef<Vec<(DocId, SharedVector)>> for TestCollection {
 		fn as_ref(&self) -> &Vec<(DocId, SharedVector)> {
 			match self {
-				TestCollection::Unique(c) | TestCollection::NonUnique(c) => &c,
+				TestCollection::Unique(c) | TestCollection::NonUnique(c) => c,
 			}
 		}
 	}
