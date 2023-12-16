03433f2bd0526f262e6380f3d10a4a3fa95a1f55
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2023-12-16 08:49:03 +0000

    Fix formatting

--- tests/http_integration.rs
+++ tests/http_integration.rs
@@ -1341,8 +1341,12 @@ mod http_integration {
 
 		// Check that the content is gzip encoded
 		{
-			let res =
-				client.post(url).basic_auth(USER, Some(PASS)).body("CREATE |foo:100|").send().await?;
+			let res = client
+				.post(url)
+				.basic_auth(USER, Some(PASS))
+				.body("CREATE |foo:100|")
+				.send()
+				.await?;
 			assert_eq!(res.status(), 200);
 			assert_eq!(res.headers()["content-encoding"], "gzip");
 		}
