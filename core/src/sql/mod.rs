86a0ca48f589a884621ea9762d292f96ae889657
Rushmore Mushambi <rushmore@surrealdb.com>
2024-01-30 10:54:04 +0200

    Hide re-exports to hidden types

--- core/src/sql/mod.rs
+++ core/src/sql/mod.rs
@@ -135,6 +135,7 @@ pub use self::timeout::Timeout;
 pub use self::tokenizer::Tokenizer;
 pub use self::uuid::Uuid;
 pub use self::value::serde::to_value;
+#[doc(hidden)]
 pub use self::value::serde::{from_value, FromValueError};
 pub use self::value::Value;
 pub use self::value::Values;
