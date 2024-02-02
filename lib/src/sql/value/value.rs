7b5f146fe065e4f88235be589a004890bcf7351b
Mees Delzenne <mees.delzenne@gmail.com>
2024-02-02 13:23:36 +0100

    Change value size test to allow smaller sizes

--- lib/src/sql/value/value.rs
+++ lib/src/sql/value/value.rs
@@ -2846,7 +2846,7 @@ mod tests {
 
 	#[test]
 	fn check_size() {
-		assert_eq!(56, std::mem::size_of::<Value>());
+		assert!(64 >= std::mem::size_of::<Value>());
 		assert_eq!(104, std::mem::size_of::<Error>());
 		assert_eq!(104, std::mem::size_of::<Result<Value, Error>>());
 		assert_eq!(24, std::mem::size_of::<crate::sql::number::Number>());
