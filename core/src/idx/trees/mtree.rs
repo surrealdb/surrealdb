243f0e1d8c576855131be89b8242ebb7a2efbbff
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2024-02-09 10:31:35 +0000

    Clippy fixes

--- core/src/idx/trees/mtree.rs
+++ core/src/idx/trees/mtree.rs
@@ -1,5 +1,7 @@
 use std::cmp::Ordering;
 use std::collections::btree_map::Entry;
+#[cfg(debug_assertions)]
+use std::collections::HashMap;
 use std::collections::{BTreeMap, BTreeSet, BinaryHeap, VecDeque};
 use std::fmt::{Debug, Display, Formatter};
 use std::io::Cursor;
