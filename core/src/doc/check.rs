58929d32af871f3f0209b4e9e5c18a2e4b10366d
Rushmore Mushambi <rushmore@surrealdb.com>
2024-02-11 09:32:02 +0200

    Revert "Bugfix: Compute futures in query conditions (#3118)"
    This reverts commit 953e496e7d8393f49ec665896f335936e92d994f.

--- core/src/doc/check.rs
+++ core/src/doc/check.rs
@@ -15,11 +15,7 @@ impl<'a> Document<'a> {
 		// Check where condition
 		if let Some(cond) = stm.conds() {
 			// Check if the expression is truthy
-			if !cond
-				.compute(ctx, &opt.new_with_futures(true), txn, Some(&self.current))
-				.await?
-				.is_truthy()
-			{
+			if !cond.compute(ctx, opt, txn, Some(&self.current)).await?.is_truthy() {
 				// Ignore this document
 				return Err(Error::Ignore);
 			}
