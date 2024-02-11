da94438e06d3b60b72509690880ff9a4012ca68e
Rushmore Mushambi <rushmore@surrealdb.com>
2024-02-11 18:48:22 +0200

    Revert "Bugfix: Compute futures in query conditions" (#3476)

--- core/src/doc/lives.rs
+++ core/src/doc/lives.rs
@@ -167,8 +167,7 @@ impl<'a> Document<'a> {
 		// Check where condition
 		if let Some(cond) = stm.conds() {
 			// Check if the expression is truthy
-
-			if !cond.compute(ctx, &opt.new_with_futures(true), txn, Some(doc)).await?.is_truthy() {
+			if !cond.compute(ctx, opt, txn, Some(doc)).await?.is_truthy() {
 				// Ignore this document
 				return Err(Error::Ignore);
 			}
