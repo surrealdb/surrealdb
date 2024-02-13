b3f0048fcad96b73fa60cae4e67d1f1e171ed8e6
Rushmore Mushambi <rushmore@surrealdb.com>
2024-02-13 12:14:43 +0200

    Run cargo fmt

--- lib/src/api/engine/any/mod.rs
+++ lib/src/api/engine/any/mod.rs
@@ -1,5 +1,5 @@
 //! Dynamic support for any engine
-//! 
+//!
 //! SurrealDB supports various ways of storing and accessing your data. For storing data we support a number of
 //! key value stores. These are RocksDB, SpeeDB, TiKV, FoundationDB and an in-memory store. We call these
 //! local engines. RocksDB and SpeeDB are file-based, single node key value stores. TiKV and FoundationDB are
@@ -7,7 +7,7 @@
 //! your data, it only stores it in memory. All these can be embedded in your application, so you don't need to
 //! spin up a SurrealDB server first in order to use them. We also support spinning up a server externally and then
 //! access your database via WebSockets or HTTP. We call these remote engines.
-//! 
+//!
 //! The Rust SDK abstracts away the implementation details of the engines to make them work in a unified way.
 //! All these engines, whether they are local or remote, work exactly the same way using the same API. The only
 //! difference in the API is the endpoint you use to access the engine. Normally you provide the scheme of the engine
@@ -15,7 +15,7 @@
 //! you are trying to use is enabled. If not, your code won't compile. This is awesome but it strongly couples your
 //! application to the engine you are using. In order to change an engine you would need to update your code to
 //! the new scheme and endpoint you need to use and recompile it. This is where the any engine comes in.
-//! 
+//!
 //! The any engine allows you to use any engine as long as it was enabled when compiling. Unlike with the typed scheme,
 //! the choice of the engine is made at runtime depending on the endpoint that you provide as a string. If you use an
 //! environment variable to provide this endpoint string, you won't need to change your code in any way in order to
@@ -23,7 +23,7 @@
 //! want to use when compiling your code. On the other hand, this totally decouples your application from the engine
 //! you are using and makes it possible to use virtually any engine SurrealDB supports by simply changing the Cargo
 //! features you enable when compiling. This enables some cool workflows.
-//! 
+//!
 //! One of the common use-cases we see is using SurrealDB as an embedded database using RocksDB as the local engine.
 //! This is a nice way to boost the performance of your application when all you need is a single node. The downside
 //! of this approach is that RocksDB is not written in Rust so you will need to install some external dependencies
@@ -33,24 +33,24 @@
 //! develop using an in-memory engine but deploy using RocksDB. If you develop on Windows but deploy to Linux then
 //! you completely avoid having to build RocksDB on Windows at all.
 //!
-//! # Getting Started 
+//! # Getting Started
 //!
 //! You can start by declaring your `surrealdb` dependency like this in Cargo.toml
 //!
 //! ```toml
 //! surrealdb = {
 //!     version = "1",
-//! 
+//!
 //!     # Disables the default features, which are `protocol-ws` and `rustls`.
 //!     # Not necessary but can reduce your compile times if you don't need those features.
 //!     default-features = false,
-//! 
+//!
 //!     # Unconditionally enables the in-memory store.
 //!     # Also not necessary but this will make `cargo run` just work.
 //!     # Without it, you would need `cargo run --features surrealdb/kv-mem` during development. If you use a build
 //!     # tool like `make` or `cargo make`, however, you can put that in your build step and avoid typing it manually.
 //!     features = ["kv-mem"],
-//! 
+//!
 //!     # Also not necessary but this makes it easy to switch between `stable`, `beta` and `nightly` crates, if need be.
 //!     # See https://surrealdb.com/blog/introducing-nightly-and-beta-rust-crates for more information on those crates.
 //!     package = "surrealdb"
@@ -58,7 +58,7 @@
 //! ```
 //!
 //! You then simply need to instantiate `Surreal<Any>` instead of `Surreal<Db>` or `Surreal<Client>`.
-//! 
+//!
 //! # Examples
 //!
 //! ```rust
@@ -109,21 +109,21 @@
 //! export SURREALDB_ENDPOINT="rocksdb:/path/to/database/folder"
 //! /path/to/binary
 //! ```
-//! 
+//!
 //! The example above shows how you can avoid compiling RocksDB on your development machine, thereby avoiding dependency hell
 //! and paying the compilation cost during development. This is not the only benefit you can derive from using the any engine
 //! though. It's still useful even when your engine isn't expensive to compile. For example, the remote engines use pure Rust
 //! dependencies but you can still benefit from using the any engine by using the in-memory engine for development and deploy
 //! using a remote engine like the WebSocket engine. This way you avoid having to spin up a SurrealDB server first when
 //! developing and testing your application.
-//! 
+//!
 //! For some applications where you allow users to determine the engine they want to use, you can enable multiple engines for
 //! them when building, or even enable them all. To do this you simply need to comma separate the Cargo features.
-//! 
+//!
 //! ```bash
 //! cargo build --features surrealdb/protocol-ws,surrealdb/kv-rocksdb,surrealdb/kv-tikv --release
 //! ```
-//! 
+//!
 //! In this case, the binary you build will have support for accessing an external server via WebSockets, embedding the database
 //! using RocksDB or using a distributed TiKV cluster.
 
@@ -270,10 +270,10 @@ impl Surreal<Any> {
 ///
 /// // Instantiate an file-backed instance (currently uses RocksDB)
 /// let db = connect("file://path/to/database-folder").await?;
-/// 
+///
 /// /// // Instantiate an RocksDB-backed instance
 /// let db = connect("rocksdb://path/to/database-folder").await?;
-/// 
+///
 /// // Instantiate an SpeeDB-backed instance
 /// let db = connect("speedb://path/to/database-folder").await?;
 ///
