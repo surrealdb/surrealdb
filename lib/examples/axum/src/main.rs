4e8c56ce8499f0f32b89b13122f3d0e887d0f223
timpratim <pratim.bhosale@gmail.com>
2024-02-18 14:56:21 +0530

    Create separate router function

--- lib/examples/axum/src/main.rs
+++ lib/examples/axum/src/main.rs
@@ -8,6 +8,7 @@ use surrealdb::opt::auth::Root;
 use surrealdb::Surreal;
 use tokio::net::TcpListener;
 
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let db = Surreal::new::<Ws>("localhost:8000").await?;
@@ -20,17 +21,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	db.use_ns("namespace").use_db("database").await?;
 
-	let router = Router::new()
-		.route("/person/:id", post(person::create))
-		.route("/person/:id", get(person::read))
-		.route("/person/:id", put(person::update))
-		.route("/person/:id", delete(person::delete))
-		.route("/people", get(person::list))
-		.with_state(db);
-
 	let listener = TcpListener::bind("localhost:8080").await?;
+	let router = create_router(db);
 
 	axum::serve(listener, router).await?;
 
 	Ok(())
+}
+
+fn create_router(db: Surreal<surrealdb::engine::remote::ws::Client>) -> Router {
+	Router::new()
+		//curl -X POST -H "Content-Type: application/json" -d '{"name":"John Doe"}' http://localhost:8080/person/1
+		.route("/person/:id", post(person::create))
+		//curl -X GET http://localhost:8080/person/1
+		.route("/person/:id", get(person::read))
+		//curl -X PUT -H "Content-Type: application/json" -d '{"name":"Jane Doe"}' http://localhost:8080/person/1
+		.route("/person/:id", put(person::update))
+		//curl -X DELETE http://localhost:8080/person/1
+		.route("/person/:id", delete(person::delete))
+		//curl -X GET http://localhost:8080/people
+		.route("/people", get(person::list))
+		.with_state(db)
 }
