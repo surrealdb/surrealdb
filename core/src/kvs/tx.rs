962fba0ea8342f13423c99a2b57669e8c0a722a5
Hugh Kaznowski <hugh@surrealdb.com>
2024-02-19 14:30:36 +0000

    Test tx interaction

--- core/src/kvs/tx.rs
+++ core/src/kvs/tx.rs
@@ -3123,3 +3123,47 @@ mod tests {
 		}
 	}
 }
+
+#[cfg(all(test, feature = "kv-mem"))]
+mod tx_test {
+	use crate::kvs::lq_structs::LqEntry;
+	use crate::kvs::Datastore;
+	use crate::kvs::LockType::Optimistic;
+	use crate::kvs::TransactionType::Write;
+	use crate::sql;
+	use crate::sql::statements::LiveStatement;
+	use crate::sql::Value;
+
+	#[tokio::test]
+	pub async fn lqs_can_be_submitted_and_read() {
+		let ds = Datastore::new("memory").await.unwrap();
+		let mut tx = ds.transaction(Write, Optimistic).await.unwrap();
+
+		// Create live query data
+		let node_id = uuid::uuid!("d2715187-9d1a-49a5-9b0a-b496035b6c21");
+		let lq_entry = LqEntry {
+			live_id: sql::Uuid::new_v4(),
+			ns: "namespace".to_string(),
+			db: "database".to_string(),
+			stm: LiveStatement {
+				id: sql::Uuid::new_v4(),
+				node: sql::uuid::Uuid(node_id),
+				expr: Default::default(),
+				what: Default::default(),
+				cond: None,
+				fetch: None,
+				archived: None,
+				session: Some(Value::None),
+				auth: None,
+			},
+		};
+		tx.prepare_lq(lq_entry.clone()).unwrap();
+
+		tx.commit().await.unwrap();
+
+		// Verify data
+		let live_queries = tx.consume_pending_live_queries();
+		assert_eq!(live_queries.len(), 1);
+		assert_eq!(live_queries[0], lq_entry);
+	}
+}
