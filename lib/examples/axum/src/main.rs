d1a28d5b60eba0f1f61a4c3ac348ff6105cf6628
timpratim <pratim.bhosale@gmail.com>
2024-02-19 19:32:15 +0530

    Fixed review comments

--- lib/examples/axum/src/main.rs
+++ lib/examples/axum/src/main.rs
@@ -1,19 +1,14 @@
 mod error;
 mod person;
-
-use axum::routing::{delete, get, post, put};
-use axum::Router;
-use dotenv::dotenv;
+use axum_example::create_router;
+use std::env;
 use surrealdb::engine::any;
 use surrealdb::opt::auth::Root;
-use surrealdb::Surreal;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-	// export
-	dotenv().ok();
-	let endpoint = dotenv::var("SURREALDB_ENDPOINT").unwrap_or_else(|_| "memory".to_owned());
+	let endpoint = env::var("SURREALDB_ENDPOINT").unwrap_or_else(|_| "memory".to_owned());
 	let db = any::connect(endpoint).await?;
 
 	db.signin(Root {
@@ -30,19 +25,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	axum::serve(listener, router).await?;
 
 	Ok(())
-}
-
-fn create_router(db: Surreal<surrealdb::engine::any::Any>) -> Router {
-	Router::new()
-		//curl -X POST -H "Content-Type: application/json" -d '{"name":"John Doe"}' http://localhost:8080/person/1
-		.route("/person/:id", post(person::create))
-		//curl -X GET http://localhost:8080/person/1
-		.route("/person/:id", get(person::read))
-		//curl -X PUT -H "Content-Type: application/json" -d '{"name":"Jane Doe"}' http://localhost:8080/person/1
-		.route("/person/:id", put(person::update))
-		//curl -X DELETE http://localhost:8080/person/1
-		.route("/person/:id", delete(person::delete))
-		//curl -X GET http://localhost:8080/people
-		.route("/people", get(person::list))
-		.with_state(db)
 }
