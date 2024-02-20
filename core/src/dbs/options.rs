20f820a9c08b62a4815914e48d51558d165bb28f
Hugh Kaznowski <hugh@surrealdb.com>
2024-02-20 11:22:02 +0000

    Revert "Nearly working test"
    This reverts commit 49dd3ef43cdd2ee61a8b07ee965a487032648bdc.

--- core/src/dbs/options.rs
+++ core/src/dbs/options.rs
@@ -49,7 +49,6 @@ pub struct Options {
 	/// Should we process variable field projections?
 	pub projections: bool,
 	/// The channel over which we send notifications
-	/// This is set by datastore
 	pub sender: Option<Sender<Notification>>,
 	/// Datastore capabilities
 	pub capabilities: Arc<Capabilities>,
@@ -428,9 +427,6 @@ impl Options {
 	/// Check whether this request supports realtime queries
 	pub fn realtime(&self) -> Result<(), Error> {
 		if !self.live {
-			if true {
-				panic!("wow, bakktrace")
-			};
 			return Err(Error::RealtimeDisabled);
 		}
 		Ok(())
