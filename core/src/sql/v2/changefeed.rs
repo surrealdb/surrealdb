8c1e9c041a65c07400c6bc12b0cb97ee08d3579d
Hugh Kaznowski <hugh@surrealdb.com>
2024-02-23 16:10:51 +0000

    CF stores original optionally

--- core/src/sql/v2/changefeed.rs
+++ core/src/sql/v2/changefeed.rs
@@ -5,29 +5,29 @@ use std::fmt::{self, Display, Formatter};
 use std::str;
 use std::time;
 
-#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
 #[revisioned(revision = 2)]
 pub struct ChangeFeed {
-	pub expiry: time::Duration,
-	#[revision(start = 2)]
-	pub store_original: bool,
+    pub expiry: time::Duration,
+    #[revision(start = 2)]
+    pub store_original: bool,
 }
 
 impl Display for ChangeFeed {
-	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-		write!(f, "CHANGEFEED {}", Duration(self.expiry))?;
-		if self.store_original {
-			write!(f, " INCLUDE ORIGINAL")?;
-		};
-		Ok(())
-	}
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "CHANGEFEED {}", Duration(self.expiry))?;
+        if self.store_original {
+            write!(f, " INCLUDE ORIGINAL")?;
+        };
+        Ok(())
+    }
 }
 
 impl Default for ChangeFeed {
-	fn default() -> Self {
-		Self {
-			expiry: time::Duration::from_secs(0),
-			store_original: false,
-		}
-	}
+    fn default() -> Self {
+        Self {
+            expiry: time::Duration::from_secs(0),
+            store_original: false,
+        }
+    }
 }
