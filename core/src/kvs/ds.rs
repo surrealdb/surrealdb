6baf2346d04612ab191400b1c68fdc2a4cd51d74
Steve Fan <29133953+stevefan1999-personal@users.noreply.github.com>
2024-02-26 20:36:20 +0800

    Revert "kvs: add exhaustive unreachable since rust is too dumb"
    This reverts commit 99019f73d860c96ac170cb28969e758bd18d900a.

--- core/src/kvs/ds.rs
+++ core/src/kvs/ds.rs
@@ -130,7 +130,6 @@ impl fmt::Display for Datastore {
 			Inner::SurrealKV(_) => write!(f, "surrealkv"),
 			#[cfg(feature = "kv-postgres")]
 			Inner::Postgres(_) => write!(f, "postgres"),
-			_ => unreachable!(),
 		}
 	}
 }
@@ -1084,7 +1083,6 @@ impl Datastore {
 				let tx = v.transaction(write, lock).await?;
 				super::tx::Inner::Postgres(tx)
 			}
-			_ => unreachable!(),
 		};
 
 		let (send, recv): (Sender<LqEntry>, Receiver<LqEntry>) = channel::bounded(LQ_CHANNEL_SIZE);
