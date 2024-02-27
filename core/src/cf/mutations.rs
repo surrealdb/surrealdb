db5c46ab26904e249ef9dfd427b58ffd94d86b3d
Hugh Kaznowski <hugh@surrealdb.com>
2024-02-27 15:26:56 +0000

    Rustfmt

--- core/src/cf/mutations.rs
+++ core/src/cf/mutations.rs
@@ -15,26 +15,26 @@ use std::fmt::{self, Display, Formatter};
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Store, Hash)]
 #[revisioned(revision = 2)]
 pub enum TableMutation {
-    // Although the Value is supposed to contain a field "id" of Thing,
-    // we do include it in the first field for convenience.
-    Set(Thing, Value),
-    Del(Thing),
-    Def(DefineTableStatement),
-    #[revision(start = 2)]
-    // Includes the previous value that may be None
-    SetWithDiff(Thing, Value, Vec<Operation>),
+	// Although the Value is supposed to contain a field "id" of Thing,
+	// we do include it in the first field for convenience.
+	Set(Thing, Value),
+	Del(Thing),
+	Def(DefineTableStatement),
+	#[revision(start = 2)]
+	// Includes the previous value that may be None
+	SetWithDiff(Thing, Value, Vec<Operation>),
 }
 
 impl From<DefineTableStatement> for Value {
-    #[inline]
-    fn from(v: DefineTableStatement) -> Self {
-        let mut h = HashMap::<&str, Value>::new();
-        if let Some(id) = v.id {
-            h.insert("id", id.into());
-        }
-        h.insert("name", v.name.0.into());
-        Value::Object(Object::from(h))
-    }
+	#[inline]
+	fn from(v: DefineTableStatement) -> Self {
+		let mut h = HashMap::<&str, Value>::new();
+		if let Some(id) = v.id {
+			h.insert("id", id.into());
+		}
+		h.insert("name", v.name.0.into());
+		Value::Object(Object::from(h))
+	}
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Store, Hash)]
@@ -42,9 +42,9 @@ impl From<DefineTableStatement> for Value {
 pub struct TableMutations(pub String, pub Vec<TableMutation>);
 
 impl TableMutations {
-    pub fn new(tb: String) -> Self {
-        Self(tb, Vec::new())
-    }
+	pub fn new(tb: String) -> Self {
+		Self(tb, Vec::new())
+	}
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Store, Hash)]
@@ -52,15 +52,15 @@ impl TableMutations {
 pub struct DatabaseMutation(pub Vec<TableMutations>);
 
 impl DatabaseMutation {
-    pub fn new() -> Self {
-        Self(Vec::new())
-    }
+	pub fn new() -> Self {
+		Self(Vec::new())
+	}
 }
 
 impl Default for DatabaseMutation {
-    fn default() -> Self {
-        Self::new()
-    }
+	fn default() -> Self {
+		Self::new()
+	}
 }
 
 // Change is a set of mutations made to a table at the specific timestamp.
@@ -69,105 +69,105 @@ impl Default for DatabaseMutation {
 pub struct ChangeSet(pub [u8; 10], pub DatabaseMutation);
 
 impl TableMutation {
-    pub fn into_value(self) -> Value {
-        let mut h = BTreeMap::<String, Value>::new();
-        let h = match self {
-            TableMutation::Set(_thing, v) => {
-                // if true { panic!("The previous value is {:?},     {:?}", _thing, v); }
-                h.insert("update".to_string(), v);
-                h
-            }
-            TableMutation::SetWithDiff(_thing, Value::None, v) => {
-                h.insert("original".to_string(), Value::None);
-                h.insert("create".to_string(), Value::Array(Array(Vec::new())));
-                h
-            }
-            TableMutation::SetWithDiff(_thing, previous, v) => {
-                h.insert("original".to_string(), previous);
-                h.insert(
-                    "update".to_string(),
-                    Value::Array(Array(
-                        v.into_iter().map(|x| Value::Object(Object::from(x))).collect(),
-                    )),
-                );
-                h
-            }
-            TableMutation::Del(t) => {
-                // if true { panic!("The del value is {:?}", t); }
-                // TODO(phughk): Future PR for lq on cf feature, store update in delete for diff and notification
-                let mut other = BTreeMap::<String, Value>::new();
-                other.insert("id".to_string(), Value::Thing(t));
-                let o = Object::from(other);
-                h.insert("delete".to_string(), Value::Object(o));
-                h
-            }
-            TableMutation::Def(t) => {
-                h.insert("define_table".to_string(), Value::from(t));
-                h
-            }
-        };
-        let o = crate::sql::object::Object::from(h);
-        Value::Object(o)
-    }
+	pub fn into_value(self) -> Value {
+		let mut h = BTreeMap::<String, Value>::new();
+		let h = match self {
+			TableMutation::Set(_thing, v) => {
+				// if true { panic!("The previous value is {:?},     {:?}", _thing, v); }
+				h.insert("update".to_string(), v);
+				h
+			}
+			TableMutation::SetWithDiff(_thing, Value::None, v) => {
+				h.insert("original".to_string(), Value::None);
+				h.insert("create".to_string(), Value::Array(Array(Vec::new())));
+				h
+			}
+			TableMutation::SetWithDiff(_thing, previous, v) => {
+				h.insert("original".to_string(), previous);
+				h.insert(
+					"update".to_string(),
+					Value::Array(Array(
+						v.into_iter().map(|x| Value::Object(Object::from(x))).collect(),
+					)),
+				);
+				h
+			}
+			TableMutation::Del(t) => {
+				// if true { panic!("The del value is {:?}", t); }
+				// TODO(phughk): Future PR for lq on cf feature, store update in delete for diff and notification
+				let mut other = BTreeMap::<String, Value>::new();
+				other.insert("id".to_string(), Value::Thing(t));
+				let o = Object::from(other);
+				h.insert("delete".to_string(), Value::Object(o));
+				h
+			}
+			TableMutation::Def(t) => {
+				h.insert("define_table".to_string(), Value::from(t));
+				h
+			}
+		};
+		let o = crate::sql::object::Object::from(h);
+		Value::Object(o)
+	}
 }
 
 impl DatabaseMutation {
-    pub fn into_value(self) -> Value {
-        let mut changes = Vec::<Value>::new();
-        for tbs in self.0 {
-            for tb in tbs.1 {
-                changes.push(tb.into_value());
-            }
-        }
-        Value::Array(Array::from(changes))
-    }
+	pub fn into_value(self) -> Value {
+		let mut changes = Vec::<Value>::new();
+		for tbs in self.0 {
+			for tb in tbs.1 {
+				changes.push(tb.into_value());
+			}
+		}
+		Value::Array(Array::from(changes))
+	}
 }
 
 impl ChangeSet {
-    pub fn into_value(self) -> Value {
-        let mut m = BTreeMap::<String, Value>::new();
-        let vs = to_u128_be(self.0);
-        m.insert("versionstamp".to_string(), Value::from(vs));
-        m.insert("changes".to_string(), self.1.into_value());
-        let so: Object = m.into();
-        Value::Object(so)
-    }
+	pub fn into_value(self) -> Value {
+		let mut m = BTreeMap::<String, Value>::new();
+		let vs = to_u128_be(self.0);
+		m.insert("versionstamp".to_string(), Value::from(vs));
+		m.insert("changes".to_string(), self.1.into_value());
+		let so: Object = m.into();
+		Value::Object(so)
+	}
 }
 
 impl Display for TableMutation {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        match self {
-            TableMutation::Set(id, v) => write!(f, "SET {} {}", id, v),
-            TableMutation::SetWithDiff(id, _previous, v) => write!(f, "SET {} {:?}", id, v),
-            TableMutation::Del(id) => write!(f, "DEL {}", id),
-            TableMutation::Def(t) => write!(f, "{}", t),
-        }
-    }
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		match self {
+			TableMutation::Set(id, v) => write!(f, "SET {} {}", id, v),
+			TableMutation::SetWithDiff(id, _previous, v) => write!(f, "SET {} {:?}", id, v),
+			TableMutation::Del(id) => write!(f, "DEL {}", id),
+			TableMutation::Def(t) => write!(f, "{}", t),
+		}
+	}
 }
 
 impl Display for TableMutations {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let tb = &self.0;
-        let muts = &self.1;
-        write!(f, "{}", tb)?;
-        muts.iter().try_for_each(|v| write!(f, "{}", v))
-    }
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		let tb = &self.0;
+		let muts = &self.1;
+		write!(f, "{}", tb)?;
+		muts.iter().try_for_each(|v| write!(f, "{}", v))
+	}
 }
 
 impl Display for DatabaseMutation {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let x = &self.0;
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		let x = &self.0;
 
-        x.iter().try_for_each(|v| write!(f, "{}", v))
-    }
+		x.iter().try_for_each(|v| write!(f, "{}", v))
+	}
 }
 
 impl Display for ChangeSet {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let x = &self.1;
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		let x = &self.1;
 
-        write!(f, "{}", x)
-    }
+		write!(f, "{}", x)
+	}
 }
 
 // WriteMutationSet is a set of mutations to be to a table at the specific timestamp.
@@ -176,103 +176,103 @@ impl Display for ChangeSet {
 pub struct WriteMutationSet(pub Vec<TableMutations>);
 
 impl WriteMutationSet {
-    pub fn new() -> Self {
-        Self(Vec::new())
-    }
+	pub fn new() -> Self {
+		Self(Vec::new())
+	}
 }
 
 impl Default for WriteMutationSet {
-    fn default() -> Self {
-        Self::new()
-    }
+	fn default() -> Self {
+		Self::new()
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::sql::Strand;
+	use crate::sql::Strand;
 
-    #[test]
-    fn serialization() {
-        use super::*;
-        use std::collections::HashMap;
-        let cs = ChangeSet(
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            DatabaseMutation(vec![TableMutations(
-                "mytb".to_string(),
-                vec![
-                    TableMutation::Set(
-                        Thing::from(("mytb".to_string(), "tobie".to_string())),
-                        Value::Object(Object::from(HashMap::from([
-                            (
-                                "id",
-                                Value::from(Thing::from(("mytb".to_string(), "tobie".to_string()))),
-                            ),
-                            ("note", Value::from("surreal")),
-                        ]))),
-                    ),
-                    TableMutation::Del(Thing::from(("mytb".to_string(), "tobie".to_string()))),
-                    TableMutation::Def(DefineTableStatement {
-                        name: "mytb".into(),
-                        ..DefineTableStatement::default()
-                    }),
-                ],
-            )]),
-        );
-        let v = cs.into_value().into_json();
-        let s = serde_json::to_string(&v).unwrap();
-        assert_eq!(
-            s,
-            r#"{"changes":[{"update":{"id":"mytb:tobie","note":"surreal"}},{"delete":{"id":"mytb:tobie"}},{"define_table":{"name":"mytb"}}],"versionstamp":1}"#
-        );
-    }
+	#[test]
+	fn serialization() {
+		use super::*;
+		use std::collections::HashMap;
+		let cs = ChangeSet(
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			DatabaseMutation(vec![TableMutations(
+				"mytb".to_string(),
+				vec![
+					TableMutation::Set(
+						Thing::from(("mytb".to_string(), "tobie".to_string())),
+						Value::Object(Object::from(HashMap::from([
+							(
+								"id",
+								Value::from(Thing::from(("mytb".to_string(), "tobie".to_string()))),
+							),
+							("note", Value::from("surreal")),
+						]))),
+					),
+					TableMutation::Del(Thing::from(("mytb".to_string(), "tobie".to_string()))),
+					TableMutation::Def(DefineTableStatement {
+						name: "mytb".into(),
+						..DefineTableStatement::default()
+					}),
+				],
+			)]),
+		);
+		let v = cs.into_value().into_json();
+		let s = serde_json::to_string(&v).unwrap();
+		assert_eq!(
+			s,
+			r#"{"changes":[{"update":{"id":"mytb:tobie","note":"surreal"}},{"delete":{"id":"mytb:tobie"}},{"define_table":{"name":"mytb"}}],"versionstamp":1}"#
+		);
+	}
 
-    #[test]
-    fn serialization_rev2() {
-        use super::*;
-        use std::collections::HashMap;
-        let cs = ChangeSet(
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            DatabaseMutation(vec![TableMutations(
-                "mytb".to_string(),
-                vec![
-                    TableMutation::SetWithDiff(
-                        Thing::from(("mytb".to_string(), "tobie".to_string())),
-                        Value::None,
-                        Value::Object(Object::from(HashMap::from([
-                            (
-                                "id",
-                                Value::from(Thing::from(("mytb".to_string(), "tobie".to_string()))),
-                            ),
-                            ("note", Value::from("surreal")),
-                        ]))),
-                    ),
-                    TableMutation::SetWithDiff(
-                        Thing::from(("mytb".to_string(), "tobie".to_string())),
-                        Value::Strand(Strand::from("this would normally be an object")),
-                        Value::Object(Object::from(HashMap::from([
-                            (
-                                "id",
-                                Value::from(Thing::from((
-                                    "mytb".to_string(),
-                                    "tobie2".to_string(),
-                                ))),
-                            ),
-                            ("note", Value::from("surreal")),
-                        ]))),
-                    ),
-                    TableMutation::Del(Thing::from(("mytb".to_string(), "tobie".to_string()))),
-                    TableMutation::Def(DefineTableStatement {
-                        name: "mytb".into(),
-                        ..DefineTableStatement::default()
-                    }),
-                ],
-            )]),
-        );
-        let v = cs.into_value().into_json();
-        let s = serde_json::to_string(&v).unwrap();
-        assert_eq!(
-            s,
-            r#"{"changes":[{"create":{"id":"mytb:tobie","note":"surreal"}},{"update":{"id":"mytb:tobie2","note":"surreal"}},{"delete":{"id":"mytb:tobie"}},{"define_table":{"name":"mytb"}}],"versionstamp":1}"#
-        );
-    }
+	#[test]
+	fn serialization_rev2() {
+		use super::*;
+		use std::collections::HashMap;
+		let cs = ChangeSet(
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			DatabaseMutation(vec![TableMutations(
+				"mytb".to_string(),
+				vec![
+					TableMutation::SetWithDiff(
+						Thing::from(("mytb".to_string(), "tobie".to_string())),
+						Value::None,
+						Value::Object(Object::from(HashMap::from([
+							(
+								"id",
+								Value::from(Thing::from(("mytb".to_string(), "tobie".to_string()))),
+							),
+							("note", Value::from("surreal")),
+						]))),
+					),
+					TableMutation::SetWithDiff(
+						Thing::from(("mytb".to_string(), "tobie".to_string())),
+						Value::Strand(Strand::from("this would normally be an object")),
+						Value::Object(Object::from(HashMap::from([
+							(
+								"id",
+								Value::from(Thing::from((
+									"mytb".to_string(),
+									"tobie2".to_string(),
+								))),
+							),
+							("note", Value::from("surreal")),
+						]))),
+					),
+					TableMutation::Del(Thing::from(("mytb".to_string(), "tobie".to_string()))),
+					TableMutation::Def(DefineTableStatement {
+						name: "mytb".into(),
+						..DefineTableStatement::default()
+					}),
+				],
+			)]),
+		);
+		let v = cs.into_value().into_json();
+		let s = serde_json::to_string(&v).unwrap();
+		assert_eq!(
+			s,
+			r#"{"changes":[{"create":{"id":"mytb:tobie","note":"surreal"}},{"update":{"id":"mytb:tobie2","note":"surreal"}},{"delete":{"id":"mytb:tobie"}},{"define_table":{"name":"mytb"}}],"versionstamp":1}"#
+		);
+	}
 }
