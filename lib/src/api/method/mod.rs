186b3aba20341b41a067b0b4ef7b3c245e0277f1
Dhghomon <56599343+Dhghomon@users.noreply.github.com>
2024-03-01 10:38:20 +0900

    Add example of .take() for error handling to query method

--- lib/src/api/method/mod.rs
+++ lib/src/api/method/mod.rs
@@ -635,7 +635,26 @@ where
 	/// let created: Option<Person> = result.take(0)?;
 	///
 	/// // Get all of the results from the second query
-	/// let people: Vec<Person> = result.take(1)?;
+	/// let people: Vec<Person> = result.take(1)?; 
+	///
+	/// #[derive(serde::Deserialize)]
+	/// struct Country {
+	/// 	name: String
+	/// }
+	///
+	/// // The .take() method can be used for error handling
+	///
+	/// // If the table has no defined schema, this query will 
+	/// // create a `country` on the SurrealDB side, but...
+	/// let mut result = db
+	/// 	.query("CREATE country")
+	/// 	.await?;
+	///
+	/// // It won't deserialize into a Country struct
+	/// if let Err(e) = result.take::<Option<Country>>(0) {
+	/// 	println!("Failed to make a country: {e:#?}");
+	/// 	assert!(e.to_string().contains("missing field `name`"));
+	/// }
 	/// #
 	/// # Ok(())
 	/// # }
