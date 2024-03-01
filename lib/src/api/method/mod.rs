de8e6d1a7dc8980e0b8a28a275683ccabff81f94
Dhghomon <56599343+Dhghomon@users.noreply.github.com>
2024-03-01 11:04:26 +0900

    satisfy clippy

--- lib/src/api/method/mod.rs
+++ lib/src/api/method/mod.rs
@@ -639,7 +639,7 @@ where
 	///
 	/// #[derive(serde::Deserialize)]
 	/// struct Country {
-	/// 	name: String
+	///     name: String
 	/// }
 	///
 	/// // The .take() method can be used for error handling
@@ -647,13 +647,13 @@ where
 	/// // If the table has no defined schema, this query will
 	/// // create a `country` on the SurrealDB side, but...
 	/// let mut result = db
-	/// 	.query("CREATE country")
-	/// 	.await?;
+	///     .query("CREATE country")
+	///     .await?;
 	///
 	/// // It won't deserialize into a Country struct
 	/// if let Err(e) = result.take::<Option<Country>>(0) {
-	/// 	println!("Failed to make a country: {e:#?}");
-	/// 	assert!(e.to_string().contains("missing field `name`"));
+	///     println!("Failed to make a country: {e:#?}");
+	///     assert!(e.to_string().contains("missing field `name`"));
 	/// }
 	/// #
 	/// # Ok(())
