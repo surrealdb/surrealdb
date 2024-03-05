ecbbe002564d282a649e02b5b4f3e2cff03416e8
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-03-05 18:40:45 +0000

    Update core/src/sql/v2/value/serde/ser/statement/remove/index.rs
    Co-authored-by: Rushmore Mushambi <rushmore@surrealdb.com>

--- core/src/sql/v2/value/serde/ser/statement/remove/index.rs
+++ core/src/sql/v2/value/serde/ser/statement/remove/index.rs
@@ -56,7 +56,7 @@ impl serde::ser::SerializeStruct for SerializeRemoveIndexStatement {
 				self.what = Ident(value.serialize(ser::string::Serializer.wrap())?);
 			}
 			"if_exists" => {
-				self.if_exists = value.serialize(ser::primitive::bool::Serializer.wrap())?
+				self.if_exists = value.serialize(ser::primitive::bool::Serializer.wrap())?;
 			}
 			key => {
 				return Err(Error::custom(format!(
