2de7f960ac698c49e43a7a82aeb043ae57223d6c
Raphael Darley <raphael@raphaeldarley.com>
2024-03-18 11:03:20 +0000

    Update core/src/sql/table_type.rs
    Co-authored-by: Tobie Morgan Hitchcock <tobie@surrealdb.com>

--- core/src/sql/table_type.rs
+++ core/src/sql/table_type.rs
@@ -9,11 +9,10 @@ use super::Kind;
 #[derive(Debug, Default, Serialize, Deserialize, Hash, Clone, Eq, PartialEq, PartialOrd)]
 #[revisioned(revision = 1)]
 pub enum TableType {
-	Relation(Relation),
-	Normal,
-	// Should not be changed in version 2.0.0, this is required for revision compatibility
 	#[default]
 	Any,
+	Normal,
+	Relation(Relation),
 }
 
 impl Display for TableType {
