7b37e27be37b718f82cb95508578fb7774f565c3
timpratim <pratim.bhosale@gmail.com>
2024-03-18 18:13:37 +0530

    fallback to memory mode when endpoint is not mentioned

--- lib/examples/rocket/src/lib.rs
+++ lib/examples/rocket/src/lib.rs
@@ -5,6 +5,7 @@ use rocket::serde::{Deserialize, Serialize};
 use std::env;
 use surrealdb::engine::any;
 use surrealdb::opt::auth::Root;
+use surrealdb::opt::Config;
 use surrealdb::Surreal;
 
 #[derive(Serialize, Deserialize)]
@@ -17,14 +18,13 @@ pub type Db = Surreal<any::Any>;
 
 pub async fn create_db_connection() -> Result<Db, Box<dyn std::error::Error>> {
 	let endpoint = env::var("SURREALDB_ENDPOINT").unwrap_or_else(|_| "memory".to_owned());
-	let db = any::connect(&endpoint).await?;
-
-	db.signin(Root {
+	let root = Root {
 		username: "root",
 		password: "root",
-	})
-	.await?;
-
+	};
+	let rootconfig = Config::new().user(root);
+	let db = any::connect((endpoint, rootconfig)).await?;
+	db.signin(root).await?;
 	db.use_ns("namespace").use_db("database").await?;
 
 	Ok(db)
