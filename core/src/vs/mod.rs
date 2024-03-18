a77f81e371963a50a0652baadc5b4163dc345a15
Hugh Kaznowski <hugh@surrealdb.com>
2024-03-18 15:58:50 +0000

    WIP, versionstamp generator for tests

--- core/src/vs/mod.rs
+++ core/src/vs/mod.rs
@@ -21,3 +21,53 @@ pub(crate) mod oracle;
 
 pub use self::conv::*;
 pub use self::oracle::*;
+use futures::StreamExt;
+use std::collections::binary_heap::Iter;
+
+/// Generate S-tuples of valid, sequenced versionstamps within range.
+/// The limit is used, because these are combinatorics - without an upper bound, combinations aren't possible.
+#[cfg(test)]
+#[doc(hidden)]
+pub fn generate_versionstamp_sequences<const S: usize>(limit: usize) -> Iter<[u64; S]> {}
+
+#[cfg(test)]
+#[doc(hidden)]
+pub struct VersionstampSequence<const S: usize> {
+	next_state: Option<[u64; S]>,
+	limit: u64,
+}
+
+impl<const S: usize> Iterator for VersionstampSequence<S> {
+	type Item = [u64; S];
+
+	fn next(&mut self) -> Option<Self::Item> {
+		if self.next_state.is_none() {
+			return None;
+		}
+		let current_next = self.next_state.unwrap();
+		// Now calculate next
+		let next_val = self
+			.next_state
+			// Transform the array into arrays of ranges, without combinatorics
+			.map(|arr| arr.iter().flat_map(|i| (*i..self.limit).iter()))
+			// Filter everything that isn't in range
+			.filter(|arr| arr.iter().all(|i| *i < self.limit))
+			// Find the first valid option
+			.next();
+		self.next_state = next_val;
+		Some(current_next)
+	}
+}
+
+trait Nextable {
+	type Item;
+	fn next(&self) -> Option<Self::Item>;
+}
+
+impl Nextable for &[u64] {
+	type Item = Into<[u64]>;
+
+	fn next(&self) -> Option<Self::Item> {
+		todo!()
+	}
+}
