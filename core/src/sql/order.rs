c82bbc0820b00e572114f8ef6e2ef0f74b548d1f
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2024-03-28 16:29:55 +0000

    Feature: On disk temporary table (#3749)
    Co-authored-by: Gerard Guillemas Martos <gerard.guillemas@surrealdb.com>

--- core/src/sql/order.rs
+++ core/src/sql/order.rs
@@ -1,7 +1,9 @@
 use crate::sql::fmt::Fmt;
 use crate::sql::idiom::Idiom;
+use crate::sql::Value;
 use revision::revisioned;
 use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
 use std::fmt;
 use std::ops::Deref;
 
@@ -9,6 +11,33 @@ use std::ops::Deref;
 #[revisioned(revision = 1)]
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 pub struct Orders(pub Vec<Order>);
+
+impl Orders {
+	pub(crate) fn compare(&self, a: &Value, b: &Value) -> Ordering {
+		for order in &self.0 {
+			// Reverse the ordering if DESC
+			let o = match order.random {
+				true => {
+					let a = rand::random::<f64>();
+					let b = rand::random::<f64>();
+					a.partial_cmp(&b)
+				}
+				false => match order.direction {
+					true => a.compare(b, order, order.collate, order.numeric),
+					false => b.compare(a, order, order.collate, order.numeric),
+				},
+			};
+			//
+			match o {
+				Some(Ordering::Greater) => return Ordering::Greater,
+				Some(Ordering::Equal) => continue,
+				Some(Ordering::Less) => return Ordering::Less,
+				None => continue,
+			}
+		}
+		Ordering::Equal
+	}
+}
 
 impl Deref for Orders {
 	type Target = Vec<Order>;
