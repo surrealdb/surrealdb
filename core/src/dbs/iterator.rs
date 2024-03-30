829fb0baf9a1a63b8e612ce2ed7829a54859bb20
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2024-03-30 23:11:54 +0000

    Bug fix: Improve support of range queries with complex queries (#3786)

--- core/src/dbs/iterator.rs
+++ core/src/dbs/iterator.rs
@@ -19,18 +19,19 @@ use crate::sql::thing::Thing;
 use crate::sql::value::Value;
 use async_recursion::async_recursion;
 use std::mem;
+use std::sync::Arc;
 
 #[derive(Clone)]
 pub(crate) enum Iterable {
 	Value(Value),
-	Table(Table),
+	Table(Arc<Table>),
 	Thing(Thing),
 	Range(Range),
 	Edges(Edges),
 	Defer(Thing),
 	Mergeable(Thing, Value),
 	Relatable(Thing, Thing, Thing),
-	Index(Table, IteratorRef),
+	Index(Arc<Table>, IteratorRef),
 }
 
 pub(crate) struct Processed {
@@ -117,7 +118,7 @@ impl Iterator {
 					}
 					_ => {
 						// Ingest the table for scanning
-						self.ingest(Iterable::Table(v))
+						self.ingest(Iterable::Table(Arc::new(v)))
 					}
 				},
 				// There is no data clause so create a record id
@@ -128,7 +129,7 @@ impl Iterator {
 					}
 					_ => {
 						// Ingest the table for scanning
-						self.ingest(Iterable::Table(v))
+						self.ingest(Iterable::Table(Arc::new(v)))
 					}
 				},
 			},
