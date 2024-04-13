0e03a669751b844d5907ab90c5a6a1679da51f49
Micha de Vries <micha@devrie.sh>
2024-04-13 21:57:02 +0200

    Document should also match type of event

--- core/src/doc/event.rs
+++ core/src/doc/event.rs
@@ -34,21 +34,23 @@ impl<'a> Document<'a> {
 			} else {
 				Value::from("UPDATE")
 			};
+			// Depending on type of event, how do we populate the document
+			let doc = match stm.is_delete() {
+				true => &self.initial,
+				false => &self.current,
+			};
 			// Configure the context
 			let mut ctx = Context::new(ctx);
 			ctx.add_value("event", met);
-			ctx.add_value("value", match stm.is_delete() {
-				true => self.initial.doc.deref(),
-				false => self.current.doc.deref(),
-			});
+			ctx.add_value("value", doc.doc.deref());
 			ctx.add_value("after", self.current.doc.deref());
 			ctx.add_value("before", self.initial.doc.deref());
 			// Process conditional clause
-			let val = ev.when.compute(&ctx, opt, txn, Some(&self.current)).await?;
+			let val = ev.when.compute(&ctx, opt, txn, Some(&doc)).await?;
 			// Execute event if value is truthy
 			if val.is_truthy() {
 				for v in ev.then.iter() {
-					v.compute(&ctx, opt, txn, Some(&self.current)).await?;
+					v.compute(&ctx, opt, txn, Some(&doc)).await?;
 				}
 			}
 		}
