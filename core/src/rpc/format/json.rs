02ed9b15df6116362f410dc9136880ab0bc3fcb8
Micha de Vries <micha@devrie.sh>
2024-04-17 15:51:09 +0200

    json should use `syn::value_legacy_strand`

--- core/src/rpc/format/json.rs
+++ core/src/rpc/format/json.rs
@@ -1,12 +1,12 @@
 use crate::rpc::request::Request;
 use crate::rpc::RpcError;
-use crate::sql;
 use crate::sql::Value;
+use crate::syn;
 
 use super::ResTrait;
 
 pub fn req(val: &[u8]) -> Result<Request, RpcError> {
-	sql::value(std::str::from_utf8(val).or(Err(RpcError::ParseError))?)
+	syn::value_legacy_strand(std::str::from_utf8(val).or(Err(RpcError::ParseError))?)
 		.or(Err(RpcError::ParseError))?
 		.try_into()
 }
