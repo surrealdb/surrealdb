81ae29015dc278c27067623f611242130c5046ee
Hugh Kaznowski <hugh@surrealdb.com>
2024-04-30 13:05:54 +0100

    DelWithOriginal

--- core/src/kvs/lq_v2_doc.rs
+++ core/src/kvs/lq_v2_doc.rs
@@ -60,6 +60,17 @@ pub(in crate::kvs) fn construct_document(
 			// TODO(SUR-328): reverse diff and apply to doc to retrieve original version of doc
 			Ok(Some(doc))
 		}
+		TableMutation::DelWithOriginal(id, val) => {
+			let doc = Document::new_artificial(
+				None,
+				Some(id),
+				None,
+				Cow::Owned(Value::None),
+				Cow::Borrowed(val),
+				Workable::Normal,
+			);
+			Ok(Some(doc))
+		}
 	}
 }
 
@@ -134,6 +145,31 @@ mod test {
 	fn test_construct_document_delete() {
 		let thing = Thing::from(("table", "id"));
 		let tb_mutation = TableMutation::Del(thing.clone());
+		let doc = construct_document(&tb_mutation).unwrap();
+		let doc = doc.unwrap();
+		// The previous and current doc values are "None", so technically this is a new doc as per
+		// current == None
+		assert!(!doc.is_new(), "{:?}", doc);
+		assert!(doc.is_delete(), "{:?}", doc);
+		assert!(doc.current_doc().is_none());
+		assert!(doc.initial_doc().is_some());
+		match doc.initial_doc() {
+			Value::Object(o) => {
+				assert!(o.contains_key("id"));
+				assert_eq!(o.get("id").unwrap(), &Value::Thing(thing));
+			}
+			_ => panic!("Initial doc should be an object"),
+		}
+	}
+
+	#[test]
+	fn test_construct_document_delete_with_original() {
+		let thing = Thing::from(("table", "id"));
+		let original = Value::Object(Object(map! {
+			"id".to_string() => Value::Thing(thing.clone()),
+			"some_key".to_string() => Value::Strand(Strand::from("some_value")),
+		}));
+		let tb_mutation = TableMutation::DelWithOriginal(thing.clone(), original);
 		let doc = construct_document(&tb_mutation).unwrap();
 		let doc = doc.unwrap();
 		// The previous and current doc values are "None", so technically this is a new doc as per
