87ea8c76e07c36ef4b92b0b26279ff8a6c96887d
Hugh Kaznowski <hugh@surrealdb.com>
2024-05-08 09:45:47 +0100

    Shrug

--- core/src/kvs/tests/timestamp_to_versionstamp.rs
+++ core/src/kvs/tests/timestamp_to_versionstamp.rs
@@ -1,3 +1,5 @@
+use crate::key;
+
 // Timestamp to versionstamp tests
 // This translation mechanism is currently used by the garbage collector to determine which change feed entries to delete.
 //
@@ -42,4 +44,41 @@ async fn timestamp_to_versionstamp() {
 	tx.commit().await.unwrap();
 	assert!(vs1 < vs2);
 	assert!(vs2 < vs3);
+}
+
+#[tokio::test]
+#[serial]
+async fn only_the_latest_ts_is_retained() {
+	// Create a new datastore
+	let node_id = Uuid::parse_str("A905CA25-56ED-49FB-B759-696AEA87C342").unwrap();
+	let clock = Arc::new(SizedClock::Fake(FakeClock::new(Timestamp::default())));
+	let (ds, _) = new_ds(node_id, clock).await;
+
+	// Give the current versionstamp a timestamp of 0
+	let mut tx = ds.transaction(Write, Optimistic).await.unwrap();
+	tx.set_timestamp_for_versionstamp(0, "myns", "mydb", true).await.unwrap();
+	tx.commit().await.unwrap();
+
+	// Get the versionstamp for timestamp 0
+	let mut tx = ds.transaction(Write, Optimistic).await.unwrap();
+	let vs1 = tx.get_versionstamp_from_timestamp(0, "myns", "mydb", true).await.unwrap().unwrap();
+	tx.commit().await.unwrap();
+
+	// Give the current versionstamp a timestamp of 1
+	let mut tx = ds.transaction(Write, Optimistic).await.unwrap();
+	tx.set_timestamp_for_versionstamp(1, "myns", "mydb", true).await.unwrap();
+	tx.commit().await.unwrap();
+
+	// Get the versionstamp for timestamp 1
+	let mut tx = ds.transaction(Write, Optimistic).await.unwrap();
+	let vs2 = tx.get_versionstamp_from_timestamp(1, "myns", "mydb", true).await.unwrap().unwrap();
+	tx.commit().await.unwrap();
+
+	// Scan range
+	let start = key::database::ts::new("myns", "mydb", 0);
+	let end = key::database::ts::new("myns", "mydb", u64::MAX);
+	let mut tx = ds.transaction(Write, Optimistic).await.unwrap();
+	let scanned = tx.scan(start..end, u32::MAX).await.unwrap();
+	tx.commit().await.unwrap();
+	assert_eq!(scanned.len(), 1)
 }
