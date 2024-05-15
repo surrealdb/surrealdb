ca2e0388c1542a41d13fad3fd1afe0f56c1b7aab
Micha de Vries <micha@devrie.sh>
2024-05-15 13:21:37 +0200

    Add suggestion from review

--- lib/tests/option.rs
+++ lib/tests/option.rs
@@ -13,13 +13,14 @@ async fn option_import_indexes_should_be_populated() -> Result<(), Error> {
 		DEFINE INDEX field_num ON test FIELDS num;
 		CREATE ONLY test:1 SET num = 123;
 		SELECT * FROM test WHERE num = 123;
+		SELECT * FROM test WHERE num = 123 EXPLAIN;
 	";
 	let dbs = new_ds().await?;
 	let ses = Session::owner().with_ns("test").with_db("test");
 	let res = &mut dbs.execute(sql, &ses, None).await?;
 	//
 	// OPTION IMPORT does not count as a result
-	assert_eq!(res.len(), 3);
+	assert_eq!(res.len(), 4);
 	//
 	let tmp = res.remove(0).result;
 	assert!(tmp.is_ok(), "{:?}", tmp.err());
@@ -39,6 +40,30 @@ async fn option_import_indexes_should_be_populated() -> Result<(), Error> {
 			{
 				id: test:1,
 				num: 123
+			}
+		]",
+	);
+	assert_eq!(tmp, val);
+	//
+	let tmp = res.remove(0).result?;
+	let val = Value::parse(
+		"[
+			{
+				detail: {
+					plan: {
+						index: 'field_num',
+						operator: '=',
+						value: 123
+					},
+					table: 'test'
+				},
+				operation: 'Iterate Index'
+			},
+			{
+				detail: {
+					type: 'Memory'
+				},
+				operation: 'Collector'
 			}
 		]",
 	);
