d2488fd4ff189ee474c7651110358b79416b7a4a
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2024-05-21 10:36:53 +0100

    Use TiKv for sql1, SurrealKV for sql2

--- tests/sdk/sql2/main.rs
+++ tests/sdk/sql2/main.rs
@@ -3,7 +3,7 @@ use surrealdb::Surreal;
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
-	let db = Surreal::new::<SurrealKV>("/tmp/sdk_test_sql1").await?;
+	let db = Surreal::new::<SurrealKV>("/tmp/sdk_test_sql2").await?;
 	let _ = db.query("INFO FOR ROOT").await.unwrap().check().is_ok();
 	Ok(())
 }
