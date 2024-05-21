2ea8153ce9462f2d9ea9b372817d76ebaed710c1
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2024-05-21 10:22:41 +0100

    Use SurrealKV for tests (fast compilation)

--- tests/sdk/sql1/main.rs
+++ tests/sdk/sql1/main.rs
@@ -1,9 +1,9 @@
-use surrealdb::engine::local::RocksDb;
+use surrealdb::engine::local::SurrealKV;
 use surrealdb::Surreal;
 
 #[tokio::main]
 async fn main() -> surrealdb::Result<()> {
-	let db = Surreal::new::<RocksDb>("/tmp/sdk_test_sql1").await?;
+	let db = Surreal::new::<SurrealKV>("/tmp/sdk_test_sql1").await?;
 	let _ = db.query("INFO FOR ROOT").await.unwrap().check().is_ok();
 	Ok(())
 }
