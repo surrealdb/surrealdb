e37a6fb18bb0beaca37ea259ee1e0c36066b7e01
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2024-05-22 11:05:32 +0100

    Make temporary table active only if the temporary directory is set (#4079)

--- lib/src/api/engine/local/native.rs
+++ lib/src/api/engine/local/native.rs
@@ -145,6 +145,7 @@ pub(crate) fn router(
 			.with_query_timeout(address.config.query_timeout)
 			.with_transaction_timeout(address.config.transaction_timeout)
 			.with_capabilities(address.config.capabilities);
+
 		#[cfg(any(
 			feature = "kv-surrealkv",
 			feature = "kv-file",
@@ -153,7 +154,10 @@ pub(crate) fn router(
 			feature = "kv-tikv",
 			feature = "kv-speedb"
 		))]
-		let kvs = kvs.with_temporary_directory(address.config.temporary_directory);
+		let kvs = match address.config.temporary_directory {
+			Some(tmp_dir) => kvs.with_temporary_directory(tmp_dir),
+			_ => kvs,
+		};
 
 		let kvs = Arc::new(kvs);
 		let mut vars = BTreeMap::new();
