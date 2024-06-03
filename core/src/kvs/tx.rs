8f9f53620a627282278f09cf78a1fff10543315f
Micha de Vries <micha@devrie.sh>
2024-06-03 10:22:43 +0200

    Optimize

--- core/src/kvs/tx.rs
+++ core/src/kvs/tx.rs
@@ -2584,17 +2584,15 @@ impl Transaction {
 
 						// Add batches of INSERT statements
 						for records in normal.chunks(1000).into_iter() {
-							let tb_name = &tb.name;
 							let values = records.join(", ");
-							let sql = format!("INSERT INTO {tb_name} [ {values} ]");
+							let sql = format!("INSERT [ {values} ]");
 							chn.send(bytes!(sql)).await?;
 						}
 
 						// Add batches of INSERT RELATION statements
 						for records in relation.chunks(1000).into_iter() {
-							let tb_name = &tb.name;
 							let values = records.join(", ");
-							let sql = format!("INSERT RELATION INTO {tb_name} [ {values} ]");
+							let sql = format!("INSERT RELATION [ {values} ]");
 							chn.send(bytes!(sql)).await?;
 						}
 						continue;
