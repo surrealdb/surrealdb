b72b88607a56bbe8ad2428fa722e88a62135e105
Odonno <bottiau.david@laposte.net>
2024-06-10 20:35:45 +0200

    Use ammonia for `html::encode` fn

--- core/src/fnc/html.rs
+++ core/src/fnc/html.rs
@@ -2,7 +2,7 @@ use crate::err::Error;
 use crate::sql::value::Value;
 
 pub fn encode((arg,): (String,)) -> Result<Value, Error> {
-	Ok(html_escape::encode_text(&arg).to_string().into())
+	Ok(ammonia::clean_text(&arg).to_string().into())
 }
 
 pub fn sanitize((arg,): (String,)) -> Result<Value, Error> {
@@ -16,7 +16,7 @@ mod tests {
 	#[test]
 	fn encode_html() {
 		let value = super::encode((String::from("<div>Hello world!</div>"),)).unwrap();
-		assert_eq!(value, Value::Strand("&lt;div&gt;Hello world!&lt;/div&gt;".into()));
+		assert_eq!(value, Value::Strand("&lt;div&gt;Hello&#32;world!&lt;&#47;div&gt;".into()));
 
 		let value = super::encode((String::from("SurrealDB"),)).unwrap();
 		assert_eq!(value, Value::Strand("SurrealDB".into()));
