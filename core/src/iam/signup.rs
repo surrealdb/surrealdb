52fa8533c2459f48553ec0d63df667267efff893
Gerard Guillemas Martos <gerard.guillemas@surrealdb.com>
2024-06-26 16:58:06 +0200

    Rename variable to prevent shadowing

--- core/src/iam/signup.rs
+++ core/src/iam/signup.rs
@@ -96,7 +96,7 @@ pub async fn db_access(
 												..Claims::default()
 											};
 											// AUTHENTICATE clause
-											if let Some(ac) = at.authenticate {
+											if let Some(au) = at.authenticate {
 												// Setup the system session for finding the signin record
 												let mut sess =
 													Session::editor().with_ns(&ns).with_db(&db);
@@ -105,7 +105,7 @@ pub async fn db_access(
 												sess.ip.clone_from(&session.ip);
 												sess.or.clone_from(&session.or);
 												// Compute the value with the params
-												match kvs.evaluate(ac, &sess, None).await {
+												match kvs.evaluate(au, &sess, None).await {
 													Ok(val) => match val.record() {
 														Some(id) => {
 															// Update rid with result from AUTHENTICATE clause
