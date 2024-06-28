5bb11a9de42fdc67e605db6c6f5bda8353fc7f60
Micha de Vries <micha@devrie.sh>
2024-06-28 11:47:22 +0200

    fmt

--- core/src/sql/statements/ifelse.rs
+++ core/src/sql/statements/ifelse.rs
@@ -153,9 +153,6 @@ mod tests {
 	fn format_pretty() {
 		let query = parse("IF 1 { 1 } ELSE IF 2 { 2 }").unwrap();
 		assert_eq!(format!("{}", query), "IF 1 { 1 } ELSE IF 2 { 2 };");
-		assert_eq!(
-			format!("{:#}", query),
-			"IF 1\n\t{ 1 }\nELSE IF 2\n\t{ 2 }\n;"
-		);
+		assert_eq!(format!("{:#}", query), "IF 1\n\t{ 1 }\nELSE IF 2\n\t{ 2 }\n;");
 	}
 }
