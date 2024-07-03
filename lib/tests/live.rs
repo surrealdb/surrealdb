24d568885388b078ca1946e219c01a40b7a87a22
Hugh Kaznowski <hugh@surrealdb.com>
2024-07-03 10:16:39 +0100

    Test demonstrating it doesn't work

--- lib/tests/live.rs
+++ lib/tests/live.rs
@@ -97,3 +97,73 @@ async fn live_query_sends_registered_lq_details() -> Result<(), Error> {
 
 	Ok(())
 }
+
+#[tokio::test]
+async fn live_query_sends_notification_function_update() -> Result<(), Error> {
+	let table = "lq_fn_update_test_123";
+	let table_def = match FFLAGS.change_feed_live_queries.enabled() {
+		true => format!("DEFINE TABLE {table} CHANGEFEED 10m INCLUDE ORIGINAL;"),
+		false => format!("DEFINE TABLE {table};"),
+	};
+	let sql = format!(
+		"
+		{table_def}
+		CREATE other_table:123;
+		CREATE {table};
+		DEFINE FUNCTION fn::update_lq_test() {{
+			LET $record = SELECT VALUE id FROM {table} LIMIT 1;
+			UPDATE $record SET link = other_table:123;
+			RETURN $record;
+		}};
+		LIVE SELECT * FROM {table};
+	"
+	);
+	let mut stack = reblessive::tree::TreeStack::new();
+	let dbs = new_ds().await?;
+	let ses = Session::owner().with_ns("test").with_db("test").with_rt(true);
+	println!("SQL: {}", sql);
+	let res = &mut dbs.execute(&sql, &ses, None).await.unwrap();
+	assert_eq!(res.len(), 5);
+
+	// Define table didnt fail
+	let tmp = res.remove(0).result;
+	assert!(tmp.is_ok());
+
+	// Create other table didn't fail
+	let tmp = res.remove(0).result;
+	assert!(tmp.is_ok());
+
+	// Create table to update didn't fail
+	let tmp = res.remove(0).result;
+	assert!(tmp.is_ok());
+
+	// Define function didnt fail
+	let tmp = res.remove(0).result;
+	assert!(tmp.is_ok());
+
+	// Live query returned a valid uuid
+	let actual = res.remove(0).result.unwrap();
+	let live_id = match actual {
+		Value::Uuid(live_id) => live_id,
+		_ => panic!("Expected a UUID"),
+	};
+	assert!(!live_id.is_nil());
+
+	// Invoke the function to cause an update
+	let res = &mut dbs.execute("RETURN fn::update_lq_test();", &ses, None).await?;
+	assert_eq!(res.len(), 1);
+
+	let result = res.remove(0);
+	assert!(result.result.is_ok());
+
+	// Check there is an update notification
+	let def = Default::default();
+	stack.enter(|stk| dbs.process_lq_notifications(stk, &def)).finish().await?;
+
+	let notifications_chan = dbs.notifications().unwrap();
+
+	assert!(notifications_chan.try_recv().is_ok());
+	assert!(notifications_chan.try_recv().is_err());
+
+	Ok(())
+}
