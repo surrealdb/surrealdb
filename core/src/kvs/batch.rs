67f6ead0292782247cab144424c03936a20ece86
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-07-08 21:03:28 +0100

    Fix scanner implementation

--- core/src/kvs/batch.rs
+++ core/src/kvs/batch.rs
@@ -3,6 +3,7 @@ use super::Val;
 use std::ops::Range;
 
 /// A batch scan result returned from the [`Transaction::batch`] or [`Transactor::batch`] functions.
+#[derive(Debug)]
 pub struct Batch {
 	pub next: Option<Range<Key>>,
 	pub values: Vec<(Key, Val)>,
--- core/src/kvs/scanner.rs
+++ core/src/kvs/scanner.rs
@@ -4,10 +4,13 @@ use super::Val;
 use crate::err::Error;
 use futures::stream::Stream;
 use futures::Future;
+use futures::FutureExt;
 use std::collections::VecDeque;
 use std::ops::Range;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+
+type Output = Result<Vec<(Key, Val)>, Error>;
 
 pub(super) struct Scanner<'a> {
 	/// The store which started this range scan
@@ -18,6 +21,10 @@ pub(super) struct Scanner<'a> {
 	range: Range<Key>,
 	// The results from the last range scan
 	results: VecDeque<(Key, Val)>,
+	/// The currently running future to be polled
+	future: Option<Pin<Box<dyn Future<Output = Output> + 'a>>>,
+	/// Whether this stream should try to fetch more
+	exhausted: bool,
 }
 
 impl<'a> Scanner<'a> {
@@ -26,7 +33,9 @@ impl<'a> Scanner<'a> {
 			store,
 			batch,
 			range,
+			future: None,
 			results: VecDeque::new(),
+			exhausted: false,
 		}
 	}
 }
@@ -41,44 +50,57 @@ impl<'a> Stream for Scanner<'a> {
 		if let Some(v) = self.results.pop_front() {
 			return Poll::Ready(Some(Ok(v)));
 		}
+		// If we won't fetch more results then exit
+		if self.exhausted {
+			return Poll::Ready(None);
+		}
 		// Set the max number of results to fetch
 		let num = std::cmp::min(1000, self.batch);
 		// Clone the range to use when scanning
 		let range = self.range.clone();
 		// Prepare a future to scan for results
-		let mut scanner = Box::pin(self.store.scan(range, num));
+		if self.future.is_none() {
+			self.future = Some(Box::pin(self.store.scan(range, num)));
+		}
 		// Try to resolve the future
-		match scanner.as_mut().poll(cx) {
+		match self.future.as_mut().unwrap().poll_unpin(cx) {
 			// The future has now completed fully
-			Poll::Ready(result) => match result {
-				// The range was fetched successfully
-				Ok(v) => match v.is_empty() {
-					// There are no more results to stream
-					true => {
-						// Mark this stream as complete
-						Poll::Ready(None)
-					}
-					// There are results which need streaming
-					false => {
-						// Release the mutable scanner reference
-						drop(scanner);
-						// Get the last element of the results
-						let last = v.last().unwrap();
-						// Start the next scan from the last result
-						self.range.start.clone_from(&last.0);
-						// Ensure we don't see the last result again
-						self.range.start.push(0xff);
-						// Store the fetched range results
-						self.results.extend(v);
-						// Remove the first result to return
-						let item = self.results.pop_front().unwrap();
-						// Return the first result
-						Poll::Ready(Some(Ok(item)))
-					}
-				},
-				// Return the received error
-				Err(error) => Poll::Ready(Some(Err(error))),
-			},
+			Poll::Ready(result) => {
+				// Drop the completed asynchronous future
+				self.future = None;
+				// Check the result of the finished future
+				match result {
+					// The range was fetched successfully
+					Ok(v) => match v.is_empty() {
+						// There are no more results to stream
+						true => {
+							// Mark this stream as complete
+							Poll::Ready(None)
+						}
+						// There are results which need streaming
+						false => {
+							// We fetched the last elements in the range
+							if v.len() < self.batch as usize {
+								self.exhausted = true;
+							}
+							// Get the last element of the results
+							let last = v.last().unwrap();
+							// Start the next scan from the last result
+							self.range.start.clone_from(&last.0);
+							// Ensure we don't see the last result again
+							self.range.start.push(0xff);
+							// Store the fetched range results
+							self.results.extend(v);
+							// Remove the first result to return
+							let item = self.results.pop_front().unwrap();
+							// Return the first result
+							Poll::Ready(Some(Ok(item)))
+						}
+					},
+					// Return the received error
+					Err(error) => Poll::Ready(Some(Err(error))),
+				}
+			}
 			// The future has not yet completed
 			Poll::Pending => Poll::Pending,
 		}
