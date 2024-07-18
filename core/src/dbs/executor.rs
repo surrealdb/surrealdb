080b183e30bca5a8783704d337483afd5fe51ca9
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-07-18 09:58:49 +0100

    Fix code

--- core/src/dbs/executor.rs
+++ core/src/dbs/executor.rs
@@ -426,10 +426,9 @@ impl<'a> Executor<'a> {
 			let res = Response {
 				// Get the statement end time
 				time: now.elapsed(),
-				result: res.inspect_err(|&e| {
+				result: res.inspect_err(|_| {
 					// Mark the error.
 					self.err = true;
-					e
 				}),
 				query_type: match (is_stm_live, is_stm_kill) {
 					(true, _) => QueryType::Live,
