bf8ffab9ef708b94bad307fc0561a4c769f5f932
Mees Delzenne <mees.delzenne@gmail.com>
2024-08-16 13:02:50 +0200

    Add test

--- core/src/syn/parser/test/mod.rs
+++ core/src/syn/parser/test/mod.rs
@@ -1,6 +1,9 @@
 use nom::AsBytes;
 
-use crate::{sql, syn::parser::mac::test_parse};
+use crate::{
+	sql::{self, Id, Statement, Thing, Value},
+	syn::parser::mac::test_parse,
+};
 
 mod limit;
 mod stmt;
@@ -11,6 +14,37 @@ mod value;
 fn multiple_semicolons() {
 	let res = test_parse!(parse_query, r#";;"#).unwrap();
 	let expected = sql::Query(sql::Statements(vec![]));
+	assert_eq!(res, expected);
+}
+
+#[test]
+fn glued_identifiers() {
+	let res = test_parse!(parse_query, r#"T:1"#).unwrap();
+	let expected = sql::Query(sql::Statements(vec![Statement::Value(Value::Thing(Thing {
+		tb: "T".to_string(),
+		id: Id::Number(1),
+	}))]));
+	assert_eq!(res, expected);
+
+	let res = test_parse!(parse_query, r#"T9T9T9T:1"#).unwrap();
+	let expected = sql::Query(sql::Statements(vec![Statement::Value(Value::Thing(Thing {
+		tb: "T9T9T9T".to_string(),
+		id: Id::Number(1),
+	}))]));
+	assert_eq!(res, expected);
+
+	let res = test_parse!(parse_query, r#"Z:1"#).unwrap();
+	let expected = sql::Query(sql::Statements(vec![Statement::Value(Value::Thing(Thing {
+		tb: "Z".to_string(),
+		id: Id::Number(1),
+	}))]));
+	assert_eq!(res, expected);
+
+	let res = test_parse!(parse_query, r#"Z9Z9Z9Z:1"#).unwrap();
+	let expected = sql::Query(sql::Statements(vec![Statement::Value(Value::Thing(Thing {
+		tb: "Z9Z9Z9Z".to_string(),
+		id: Id::Number(1),
+	}))]));
 	assert_eq!(res, expected);
 }
 
