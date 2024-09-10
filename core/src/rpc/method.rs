11532a82ad9c08f56733863067649e896c091583
Rushmore Mushambi <rushmore@surrealdb.com>
2024-09-10 23:26:46 +0100

    Run `cargo fmt`

--- core/src/rpc/method.rs
+++ core/src/rpc/method.rs
@@ -102,13 +102,19 @@ impl Method {
 		matches!(
 			self,
 			Method::Ping
-				| Method::Info | Method::Select
-				| Method::Insert | Method::Create
-				| Method::Update | Method::Merge
-				| Method::Patch | Method::Delete
+				| Method::Info
+				| Method::Select
+				| Method::Insert
+				| Method::Create
+				| Method::Update
+				| Method::Merge
+				| Method::Patch
+				| Method::Delete
 				| Method::Version
-				| Method::Query | Method::Relate
-				| Method::Run | Method::Unknown
+				| Method::Query
+				| Method::Relate
+				| Method::Run
+				| Method::Unknown
 		)
 	}
 }
