2bd260535828d7cb65bd1b464cd3d5ef5df43b43
Raphael Darley <raphael.darley@surrealdb.com>
2024-09-17 11:43:20 +0100

    init impl

--- core/build.rs
+++ core/build.rs
@@ -1,4 +1,55 @@
+use std::{collections::BTreeMap, fs};
+use walkdir::WalkDir;
+
 fn main() {
+	let mut rev_acc = BTreeMap::new();
+	let mut files = Vec::new();
+	rev_acc.insert("foo".to_string(), 1usize);
+
+	for entry in WalkDir::new("src").into_iter().filter_map(Result::ok) {
+		let path = entry.path();
+		files.push(path.to_string_lossy().to_string());
+		let Ok(contents) = fs::read_to_string(path) else {
+			continue;
+		};
+		let mut lines = contents.lines();
+		while let Some(line) = lines.next() {
+			if line.trim_start().starts_with("#[revisioned") {
+				let Ok(rev): Result<usize, _> = line
+					.trim()
+					.trim_start_matches("#[revisioned(revision = ")
+					.trim_end_matches(")]")
+					.parse()
+				else {
+					continue;
+				};
+				while let Some(l) = lines.next() {
+					if !l.trim_start().starts_with(char::is_alphabetic) {
+						continue;
+					} else {
+						let Some(mut name) = l
+							.split_whitespace()
+							.skip_while(|w| *w == "pub" || *w == "struct" || *w == "enum")
+							.next()
+						else {
+							panic!("foo: {l}")
+						};
+						if let Some(idx) = name.find('(') {
+							name = &name[0..idx];
+						}
+						rev_acc.insert(name.to_string(), rev);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	let lock_str =
+		rev_acc.into_iter().map(|(n, r)| format!("{n}={r}")).collect::<Vec<String>>().join("\n");
+
+	fs::write("revision.lock", lock_str).ok();
+
 	if cfg!(target_arch = "wasm32") {
 		println!("cargo:rustc-cfg=wasm");
 		println!("cargo::rustc-check-cfg=cfg(wasm)");
