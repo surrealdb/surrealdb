754ab380cc14dada6708d1ca213b1fa639cc3ea4
Rushmore Mushambi <rushmore@surrealdb.com>
2024-09-17 12:29:42 +0100

    Fix the Axum example

--- sdk/examples/axum/src/main.rs
+++ sdk/examples/axum/src/main.rs
@@ -2,24 +2,36 @@ use axum_example::create_router;
 use std::env;
 use surrealdb::engine::any;
 use surrealdb::opt::auth::Root;
+use surrealdb::opt::Config;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	// Allow the endpoint to be configured via a `SURREALDB_ENDPOINT` environment variable
+	// or fallback to memory. This makes it possible to configure the endpoint at runtime.
 	let endpoint = env::var("SURREALDB_ENDPOINT").unwrap_or_else(|_| "memory".to_owned());
-	let db = any::connect(endpoint).await?;
 
-	db.signin(Root {
+	// Define the root user.
+	let root = Root {
 		username: "root",
 		password: "root",
-	})
-	.await?;
+	};
 
+	// Activate authentication on local engines by supplying the root user to be used.
+	let config = Config::new().user(root);
+
+	// Create the database connection.
+	let db = any::connect((endpoint, config)).await?;
+
+	// Sign in as root.
+	db.signin(root).await?;
+
+	// Configure the namespace amd database to use.
 	db.use_ns("namespace").use_db("database").await?;
 
+	// Configure and start the Axum server.
 	let listener = TcpListener::bind("localhost:8080").await?;
 	let router = create_router(db);
-
 	axum::serve(listener, router).await?;
 
 	Ok(())
