eddb2739c81018a7abe822630c2837c903a47eb9
Mees Delzenne <mees.delzenne@gmail.com>
2024-09-18 15:14:09 +0200

    Fix typo

--- core/src/syn/error/location.rs
+++ core/src/syn/error/location.rs
@@ -14,7 +14,7 @@ pub struct Location {
 
 /// Safety: b must be a substring of a.
 unsafe fn str_offset(a: &str, b: &str) -> usize {
-	b.as_ptr().offset_from(b.as_ptr()) as usize
+	b.as_ptr().offset_from(a.as_ptr()) as usize
 }
 
 impl Location {
