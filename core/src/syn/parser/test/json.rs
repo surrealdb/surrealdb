a7daea4f2488825b60d369710524feef7825a57e
Mees Delzenne <mees.delzenne@gmail.com>
2024-09-18 14:57:42 +0200

    Add test

--- core/src/syn/parser/test/json.rs
+++ core/src/syn/parser/test/json.rs
@@ -6,6 +6,11 @@ fn object_with_negative() {
 }
 
 #[test]
+fn object_with_trailing_whitespace() {
+	test_parse!(parse_json, r#"{"foo": -1 }\n"#).unwrap();
+}
+
+#[test]
 fn array_with_negative() {
 	test_parse!(parse_json, r#"[-1]"#).unwrap();
 }
