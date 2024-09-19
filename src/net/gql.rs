21ff5140cf991aaa214793e4ad82e4fb3b71425e
Gerard Guillemas Martos <gerard.guillemas@surrealdb.com>
2024-09-19 14:12:04 +0200

    Correctly check for GraphQL HTTP route

--- src/net/gql.rs
+++ src/net/gql.rs
@@ -3,21 +3,15 @@ use std::sync::Arc;
 
 use axum::routing::post_service;
 
-use surrealdb::dbs::capabilities::RouteTarget;
 use surrealdb::gql::cache::Pessimistic;
 use surrealdb::kvs::Datastore;
 
-use crate::err::Error;
 use crate::gql::GraphQL;
 
 pub(super) async fn router<S>(ds: Arc<Datastore>) -> Router<S>
 where
 	S: Clone + Send + Sync + 'static,
 {
-	// Check if capabilities allow querying the requested HTTP route
-	if !ds.allows_http_route(&RouteTarget::GraphQL) {
-		return Err(Error::OperationForbidden);
-	}
 	let service = GraphQL::new(Pessimistic, ds);
 	Router::new().route("/graphql", post_service(service))
 }
