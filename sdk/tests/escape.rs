b98d07da9c820d72548df107f5d2dec2c8dea57f
Mees Delzenne <mees.delzenne@gmail.com>
2024-10-02 14:40:38 +0200

    Add tests

--- sdk/tests/escape.rs
+++ sdk/tests/escape.rs
@@ -122,3 +122,18 @@ async fn complex_strings() -> Result<(), Error> {
 	//
 	Ok(())
 }
+
+#[tokio::test]
+async fn object_keys() -> Result<(), Error> {
+	let sql = r#"
+		RETURN object::from_entries([ ["3ds": 1 ] ])
+	"#;
+	let dbs = new_ds().await?;
+	let ses = Session::owner().with_ns("test").with_db("test");
+	let res = &mut dbs.execute(sql, &ses, None).await?;
+	assert_eq!(res.len(), 1);
+	//
+	let tmp = res.remove(0).result?.to_string();
+	assert_eq!(tmp, "{ \"3ds\": 1 }");
+	Ok(())
+}
