3ac0b625a9672e32e714da35ffaaec5bb48f3f47
arriqaaq <farhan.khan@surrealdb.com>
2024-10-14 14:14:26 +0530

    chore: add scan_all_version method

--- core/src/kvs/node.rs
+++ core/src/kvs/node.rs
@@ -154,7 +154,8 @@ impl Datastore {
 				let end = crate::key::node::lq::suffix(*id);
 				let mut next = Some(beg..end);
 				while let Some(rng) = next {
-					let res = catch!(txn, txn.batch(rng, *NORMAL_FETCH_SIZE, true, None).await);
+					let res =
+						catch!(txn, txn.batch(rng, *NORMAL_FETCH_SIZE, true, None, false).await);
 					next = res.next;
 					for (k, v) in res.values.iter() {
 						// Decode the data for this live query
@@ -242,7 +243,10 @@ impl Datastore {
 					let end = crate::key::table::lq::suffix(&ns.name, &db.name, &tb.name);
 					let mut next = Some(beg..end);
 					while let Some(rng) = next {
-						let res = catch!(txn, txn.batch(rng, *NORMAL_FETCH_SIZE, true, None).await);
+						let res = catch!(
+							txn,
+							txn.batch(rng, *NORMAL_FETCH_SIZE, true, None, false).await
+						);
 						next = res.next;
 						for (k, v) in res.values.iter() {
 							// Decode the LIVE query statement
