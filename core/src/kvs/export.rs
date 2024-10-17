284bc64968ad7480e33a0e8484c31cf02c327d9b
arriqaaq <farhan.khan@surrealdb.com>
2024-10-17 18:49:52 +0530

    chore: remove unused code

--- core/src/kvs/export.rs
+++ core/src/kvs/export.rs
@@ -58,264 +58,6 @@ impl TableConfig {
 	}
 }
 
-// impl Transaction {
-// 	/// Writes the full database contents as binary SQL.
-// 	pub async fn export(
-// 		&self,
-// 		ns: &str,
-// 		db: &str,
-// 		cfg: Config,
-// 		chn: Sender<Vec<u8>>,
-// 	) -> Result<(), Error> {
-// 		// Output OPTIONS
-// 		{
-// 			chn.send(bytes!("-- ------------------------------")).await?;
-// 			chn.send(bytes!("-- OPTION")).await?;
-// 			chn.send(bytes!("-- ------------------------------")).await?;
-// 			chn.send(bytes!("")).await?;
-// 			chn.send(bytes!("OPTION IMPORT;")).await?;
-// 			chn.send(bytes!("")).await?;
-// 		}
-// 		// Output USERS
-// 		if cfg.users {
-// 			let dus = self.all_db_users(ns, db).await?;
-// 			if !dus.is_empty() {
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("-- USERS")).await?;
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("")).await?;
-// 				for us in dus.iter() {
-// 					chn.send(bytes!(format!("{us};"))).await?;
-// 				}
-// 				chn.send(bytes!("")).await?;
-// 			}
-// 		}
-// 		// Output ACCESSES
-// 		if cfg.accesses {
-// 			let dts = self.all_db_accesses(ns, db).await?;
-// 			if !dts.is_empty() {
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("-- ACCESSES")).await?;
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("")).await?;
-// 				for dt in dts.iter() {
-// 					chn.send(bytes!(format!("{dt};"))).await?;
-// 				}
-// 				chn.send(bytes!("")).await?;
-// 			}
-// 		}
-// 		// Output PARAMS
-// 		if cfg.params {
-// 			let pas = self.all_db_params(ns, db).await?;
-// 			if !pas.is_empty() {
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("-- PARAMS")).await?;
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("")).await?;
-// 				for pa in pas.iter() {
-// 					chn.send(bytes!(format!("{pa};"))).await?;
-// 				}
-// 				chn.send(bytes!("")).await?;
-// 			}
-// 		}
-// 		// Output FUNCTIONS
-// 		if cfg.functions {
-// 			let fcs = self.all_db_functions(ns, db).await?;
-// 			if !fcs.is_empty() {
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("-- FUNCTIONS")).await?;
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("")).await?;
-// 				for fc in fcs.iter() {
-// 					chn.send(bytes!(format!("{fc};"))).await?;
-// 				}
-// 				chn.send(bytes!("")).await?;
-// 			}
-// 		}
-// 		// Output ANALYZERS
-// 		if cfg.analyzers {
-// 			let azs = self.all_db_analyzers(ns, db).await?;
-// 			if !azs.is_empty() {
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("-- ANALYZERS")).await?;
-// 				chn.send(bytes!("-- ------------------------------")).await?;
-// 				chn.send(bytes!("")).await?;
-// 				for az in azs.iter() {
-// 					chn.send(bytes!(format!("{az};"))).await?;
-// 				}
-// 				chn.send(bytes!("")).await?;
-// 			}
-// 		}
-// 		// Output TABLES
-// 		if cfg.tables.is_any() {
-// 			let tbs = self.all_tb(ns, db, None).await?;
-// 			if !tbs.is_empty() {
-// 				for tb in tbs.iter() {
-// 					// Check table
-// 					if !cfg.tables.includes(&tb.name) {
-// 						continue;
-// 					}
-// 					// Output TABLE
-// 					chn.send(bytes!("-- ------------------------------")).await?;
-// 					chn.send(bytes!(format!("-- TABLE: {}", tb.name))).await?;
-// 					chn.send(bytes!("-- ------------------------------")).await?;
-// 					chn.send(bytes!("")).await?;
-// 					chn.send(bytes!(format!("{tb};"))).await?;
-// 					chn.send(bytes!("")).await?;
-// 					// Output FIELDS
-// 					let fds = self.all_tb_fields(ns, db, &tb.name, None).await?;
-// 					if !fds.is_empty() {
-// 						for fd in fds.iter() {
-// 							chn.send(bytes!(format!("{fd};"))).await?;
-// 						}
-// 						chn.send(bytes!("")).await?;
-// 					}
-// 					// Output INDEXES
-// 					let ixs = self.all_tb_indexes(ns, db, &tb.name).await?;
-// 					if !ixs.is_empty() {
-// 						for ix in ixs.iter() {
-// 							chn.send(bytes!(format!("{ix};"))).await?;
-// 						}
-// 						chn.send(bytes!("")).await?;
-// 					}
-// 					// Output EVENTS
-// 					let evs = self.all_tb_events(ns, db, &tb.name).await?;
-// 					if !evs.is_empty() {
-// 						for ev in evs.iter() {
-// 							chn.send(bytes!(format!("{ev};"))).await?;
-// 						}
-// 						chn.send(bytes!("")).await?;
-// 					}
-// 				}
-// 				// Records to be exported, categorised by the type of INSERT statement
-// 				let mut records_normal: Vec<String> =
-// 					Vec::with_capacity(*EXPORT_BATCH_SIZE as usize);
-// 				let mut records_relate: Vec<String> =
-// 					Vec::with_capacity(*EXPORT_BATCH_SIZE as usize);
-
-// 				// Output TABLE data
-// 				for tb in tbs.iter() {
-// 					// Start records
-// 					chn.send(bytes!("-- ------------------------------")).await?;
-// 					chn.send(bytes!(format!("-- TABLE DATA: {}", tb.name))).await?;
-// 					chn.send(bytes!("-- ------------------------------")).await?;
-// 					chn.send(bytes!("")).await?;
-// 					// Fetch records
-// 					let beg = crate::key::thing::prefix(ns, db, &tb.name);
-// 					let end = crate::key::thing::suffix(ns, db, &tb.name);
-// 					let mut next = Some(beg..end);
-// 					while let Some(rng) = next {
-// 						// Get the next batch of records
-// 						let batch = if cfg.versions {
-// 							self.batch_versions(rng, *EXPORT_BATCH_SIZE).await?
-// 						} else {
-// 							self.batch(rng, *EXPORT_BATCH_SIZE, true, None).await?
-// 						};
-// 						// Set the next scan range
-// 						next = batch.next;
-// 						// Check there are records
-// 						let values = if cfg.versions {
-// 							BatchValues::Versioned(batch.versioned_values)
-// 						} else {
-// 							BatchValues::Regular(batch.values)
-// 						};
-// 						match values {
-// 							BatchValues::Versioned(versioned_values) => {
-// 								if versioned_values.is_empty() {
-// 									break;
-// 								}
-
-// 								chn.send(bytes!("BEGIN")).await?;
-
-// 								// Categorize the record types
-// 								for (_, v, ts, is_tombstone) in versioned_values.into_iter() {
-// 									// Parse the key and the value
-// 									let v: Value = (&v).into();
-// 									// Check if this is a graph edge
-// 									match (v.pick(&*EDGE), v.pick(&*IN), v.pick(&*OUT)) {
-// 										// This is a graph edge record
-// 										(Value::Bool(true), Value::Thing(_), Value::Thing(_)) => {
-// 											records_relate.push(v.to_string());
-// 										}
-// 										// This is a normal record
-// 										_ => {
-// 											let rec = v.to_string();
-
-// 											if is_tombstone{
-// 												let rec_id = v.pick(&*ID).to_string();
-// 												let sql = format!(
-// 													"DELETE {rec_id};",
-// 												);
-// 												println!("dsql: {:?}", sql);
-// 												chn.send(bytes!(sql)).await?;
-// 											} else {
-// 												let sql = format!(
-// 													"INSERT {rec} VERSION d'{:?}';",
-// 													Duration::from_nanos(ts)
-// 												);
-// 												println!("isql: {:?}", sql);
-// 												chn.send(bytes!(sql)).await?;
-// 											}
-// 										}
-// 									}
-// 								}
-
-// 								chn.send(bytes!("COMMIT")).await?;
-// 							}
-// 							BatchValues::Regular(regular_values) => {
-// 								if regular_values.is_empty() {
-// 									break;
-// 								}
-// 								// Categorize the record types
-// 								for (_, v) in regular_values.into_iter() {
-// 									// Parse the key and the value
-// 									let v: Value = (&v).into();
-// 									// Check if this is a graph edge
-// 									match (v.pick(&*EDGE), v.pick(&*IN), v.pick(&*OUT)) {
-// 										// This is a graph edge record
-// 										(Value::Bool(true), Value::Thing(_), Value::Thing(_)) => {
-// 											records_relate.push(v.to_string());
-// 										}
-// 										// This is a normal record
-// 										_ => {
-// 											println!("v: {:?}", v.to_string());
-// 											println!("id: {:?}", v.pick(&*ID).to_string());
-// 											records_normal.push(v.to_string());
-// 										}
-// 									}
-// 								}
-
-// 								// Add batches of INSERT statements
-// 								if !records_normal.is_empty() {
-// 									let values = records_normal.join(", ");
-// 									let sql = format!("INSERT [ {values} ];");
-// 									println!("sql: {:?}", sql);
-// 									chn.send(bytes!(sql)).await?;
-// 									records_normal.clear();
-// 								}
-// 							}
-// 						}
-
-// 						// Add batches of INSERT RELATION statements
-// 						if !records_relate.is_empty() {
-// 							let values = records_relate.join(", ");
-// 							let sql = format!("INSERT RELATION [ {values} ];");
-// 							chn.send(bytes!(sql)).await?;
-// 							records_relate.clear()
-// 						}
-// 						// Fetch more records
-// 						continue;
-// 					}
-
-// 					chn.send(bytes!("")).await?;
-// 				}
-// 			}
-// 		}
-// 		// Everything exported
-// 		Ok(())
-// 	}
-// }
-
 impl Transaction {
 	/// Writes the full database contents as binary SQL.
 	pub async fn export(
@@ -497,7 +239,13 @@ impl Transaction {
 		Ok(())
 	}
 
-	fn process_value(v: Value, records_relate: &mut Vec<String>, records_normal: &mut Vec<String>, is_tombstone: Option<bool>, ts: Option<u64>) -> String {
+	fn process_value(
+		v: Value,
+		records_relate: &mut Vec<String>,
+		records_normal: &mut Vec<String>,
+		is_tombstone: Option<bool>,
+		ts: Option<u64>,
+	) -> String {
 		match (v.pick(&*EDGE), v.pick(&*IN), v.pick(&*OUT)) {
 			(Value::Bool(true), Value::Thing(_), Value::Thing(_)) => {
 				records_relate.push(v.to_string());
@@ -526,59 +274,65 @@ impl Transaction {
 		if versioned_values.is_empty() {
 			return Ok(());
 		}
-	
+
 		let mut records_relate = Vec::with_capacity(*EXPORT_BATCH_SIZE as usize);
-	
+
 		chn.send(bytes!("BEGIN")).await?;
-		
+
 		for (_, v, ts, is_tombstone) in versioned_values {
 			let v: Value = (&v).into();
-			let sql = Self::process_value(v, &mut records_relate, &mut Vec::new(), Some(is_tombstone), Some(ts));
+			let sql = Self::process_value(
+				v,
+				&mut records_relate,
+				&mut Vec::new(),
+				Some(is_tombstone),
+				Some(ts),
+			);
 			if !sql.is_empty() {
 				chn.send(bytes!(sql)).await?;
 			}
 		}
-	
+
 		chn.send(bytes!("COMMIT")).await?;
-	
+
 		if !records_relate.is_empty() {
 			let values = records_relate.join(", ");
 			let sql = format!("INSERT RELATION [ {} ];", values);
 			chn.send(bytes!(sql)).await?;
 		}
-	
+
 		Ok(())
 	}
 
-async fn export_regular_data(
-    &self,
-    regular_values: Vec<(Vec<u8>, Vec<u8>)>,
-    chn: &Sender<Vec<u8>>,
-) -> Result<(), Error> {
-    if regular_values.is_empty() {
-        return Ok(());
-    }
+	async fn export_regular_data(
+		&self,
+		regular_values: Vec<(Vec<u8>, Vec<u8>)>,
+		chn: &Sender<Vec<u8>>,
+	) -> Result<(), Error> {
+		if regular_values.is_empty() {
+			return Ok(());
+		}
 
-    let mut records_normal = Vec::with_capacity(*EXPORT_BATCH_SIZE as usize);
-    let mut records_relate = Vec::with_capacity(*EXPORT_BATCH_SIZE as usize);
+		let mut records_normal = Vec::with_capacity(*EXPORT_BATCH_SIZE as usize);
+		let mut records_relate = Vec::with_capacity(*EXPORT_BATCH_SIZE as usize);
 
-    for (_, v) in regular_values {
-        let v: Value = (&v).into();
-        Self::process_value(v, &mut records_relate, &mut records_normal, None, None);
-    }
+		for (_, v) in regular_values {
+			let v: Value = (&v).into();
+			Self::process_value(v, &mut records_relate, &mut records_normal, None, None);
+		}
 
-    if !records_normal.is_empty() {
-        let values = records_normal.join(", ");
-        let sql = format!("INSERT [ {} ];", values);
-		chn.send(bytes!(sql)).await?;
-    }
+		if !records_normal.is_empty() {
+			let values = records_normal.join(", ");
+			let sql = format!("INSERT [ {} ];", values);
+			chn.send(bytes!(sql)).await?;
+		}
 
-    if !records_relate.is_empty() {
-        let values = records_relate.join(", ");
-        let sql = format!("INSERT RELATION [ {} ];", values);
-		chn.send(bytes!(sql)).await?;
-    }
+		if !records_relate.is_empty() {
+			let values = records_relate.join(", ");
+			let sql = format!("INSERT RELATION [ {} ];", values);
+			chn.send(bytes!(sql)).await?;
+		}
 
-    Ok(())
-}
+		Ok(())
+	}
 }
