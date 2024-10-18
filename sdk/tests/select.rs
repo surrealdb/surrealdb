7b480e93620d132ee81f5d8fc337a325ab712eed
Mees Delzenne <mees.delzenne@gmail.com>
2024-10-18 16:30:21 +0200

    Add test

--- sdk/tests/select.rs
+++ sdk/tests/select.rs
@@ -2,6 +2,7 @@ mod parse;
 use parse::Parse;
 mod helpers;
 use helpers::new_ds;
+use surrealdb::sql::Number;
 use surrealdb_core::dbs::Session;
 use surrealdb_core::err::Error;
 use surrealdb_core::iam::Role;
@@ -1382,6 +1383,49 @@ async fn select_field_value_permissions() -> Result<(), Error> {
 	let tmp = res.remove(0).result?;
 	let val = Value::parse("[NONE]");
 	assert_eq!(tmp, val);
+
+	Ok(())
+}
+
+#[tokio::test]
+async fn select_order_by_rand_large() -> Result<(), Error> {
+	let dbs = new_ds().await?;
+
+	let sql = r#"
+		let $array = <array> 0..1000;
+		SELECT * FROM $array ORDER BY RAND()
+	"#;
+	let ses = Session::owner().with_ns("test").with_db("test");
+	let res = &mut dbs.execute(sql, &ses, None).await?;
+	assert_eq!(res.len(), 2);
+	let _ = res.remove(0).result?;
+
+	let v = res.remove(0).result.unwrap();
+	let Value::Array(x) = v else {
+		panic!("not the right type");
+	};
+
+	let x: Vec<_> = x
+		.into_iter()
+		.map(|x| {
+			let Value::Number(Number::Int(x)) = x else {
+				panic!("not the right type");
+			};
+			x
+		})
+		.collect();
+
+	// It is technically possible that the array was shuffeled in such a way that it ends up
+	// with the original order but, if properly shuffeled, that chance should be so small the it
+	// will effectively never happens.
+	assert!(
+		!x.iter().enumerate().all(|(idx, v)| idx as i64 == *v),
+		"array was still in original order"
+	);
+
+	for i in 0..1000 {
+		assert!(x.contains(&i))
+	}
 
 	Ok(())
 }
