ae4abfe43f4d9d84501be14ee4b7117cbce75907
Micha de Vries <micha@devrie.sh>
2024-11-05 09:40:56 +0100

    Graph with filter should also be flattened after Record ID

--- core/src/sql/value/get.rs
+++ core/src/sql/value/get.rs
@@ -352,15 +352,14 @@ impl Value {
 											.run(|stk| v.get(stk, ctx, opt, None, path.next()))
 											.await?;
 										// We only want to flatten the results if the next part
-										// is a graph part. Reason being that if we flatten fields,
-										// the results of those fields (which could be arrays) will
-										// be merged into each other. So [1, 2, 3], [4, 5, 6] would
+										// is a graph or where part. Reason being that if we flatten 
+										// fields, the results of those fields (which could be arrays) 
+										// will be merged into each other. So [1, 2, 3], [4, 5, 6] would
 										// become [1, 2, 3, 4, 5, 6]. This slice access won't panic
 										// as we have already checked the length of the path.
-										Ok(if let Part::Graph(_) = path[1] {
-											res.flatten()
-										} else {
-											res
+										Ok(match path[1] {
+											Part::Graph(_) | Part::Where(_) => res.flatten(),
+											_ => res
 										})
 									}
 								}
