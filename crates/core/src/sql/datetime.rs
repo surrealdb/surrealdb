69bb2f049bf348ff950f3d7237f60415ec52c437
Micha de Vries <micha@devrie.sh>
2024-12-03 13:31:13 +0100

    Also accept datetimes for `rand::time` (#5202)
    Co-authored-by: Tobie Morgan Hitchcock <tobie@surrealdb.com>

--- crates/core/src/sql/datetime.rs
+++ crates/core/src/sql/datetime.rs
@@ -100,8 +100,13 @@ impl Datetime {
 	}
 
 	/// Convert to nanosecond timestamp.
-	pub fn to_u64(&self) -> u64 {
-		self.0.timestamp_nanos_opt().unwrap_or_default() as u64
+	pub fn to_u64(&self) -> Option<u64> {
+		self.0.timestamp_nanos_opt().map(|v| v as u64)
+	}
+
+	/// Convert to nanosecond timestamp.
+	pub fn to_i64(&self) -> Option<i64> {
+		self.0.timestamp_nanos_opt()
 	}
 }
 
