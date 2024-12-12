8a7cc324e53f8d39af942b8e9e425d50d8b0dc80
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-12-12 13:16:22 +0000

    Simplify field processing logic (#5244)

--- crates/core/src/doc/update.rs
+++ crates/core/src/doc/update.rs
@@ -7,7 +7,7 @@ use crate::sql::value::Value;
 use reblessive::tree::Stk;
 
 impl Document {
-	pub async fn update(
+	pub(super) async fn update(
 		&mut self,
 		stk: &mut Stk,
 		ctx: &Context,
@@ -21,7 +21,7 @@ impl Document {
 		self.check_permissions_table(stk, ctx, opt, stm).await?;
 		self.process_record_data(stk, ctx, opt, stm).await?;
 		self.process_table_fields(stk, ctx, opt, stm).await?;
-		self.cleanup_table_fields(stk, ctx, opt, stm).await?;
+		self.cleanup_table_fields(ctx, opt, stm).await?;
 		self.default_record_data(ctx, opt, stm).await?;
 		self.check_permissions_table(stk, ctx, opt, stm).await?;
 		self.store_record_data(ctx, opt, stm).await?;
