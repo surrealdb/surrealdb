a39569ddc98c3ee44b4f7bc70f9d2fe3c19fa75f
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-12-16 08:25:25 +0000

    Don’t track Rust memory allocation (#5272)

--- crates/core/src/mem/track.rs
+++ crates/core/src/mem/track.rs
@@ -9,44 +9,6 @@ use std::sync::LazyLock;
 /// process at this time.
 static CURRENT: AtomicUsize = AtomicUsize::new(0);
 
-/// An environment variable for configuring the memory
-/// threshold which can be used across the programme
-/// to check if the amount of available memory is low.
-/// The value can be specified as bytes (b, or without
-/// any suffix), kibibytes (k, kb, or kib), mebibytes
-/// (m, mb, or mib), or gibibytes (g, gb, or gib).
-static MEMORY_THRESHOLD: LazyLock<usize> = std::sync::LazyLock::new(|| {
-	std::env::var("SURREAL_MEMORY_THRESHOLD")
-		.map(|input| {
-			// Trim the input of any spaces
-			let input = input.trim();
-			// Check if this contains a suffix
-			let split = input.find(|c: char| !c.is_ascii_digit());
-			// Split the value into number and suffix
-			let parts = match split {
-				Some(index) => input.split_at(index),
-				None => (input, ""),
-			};
-			// Parse the number as a positive number
-			let number = parts.0.parse::<usize>().unwrap_or_default();
-			// Parse the supplied suffix as a multiplier
-			let suffix = match parts.1.trim().to_lowercase().as_str() {
-				"" | "b" => 1,
-				"k" | "kb" | "kib" => 1024,
-				"m" | "mb" | "mib" => 1024 * 1024,
-				"g" | "gb" | "gib" => 1024 * 1024 * 1024,
-				_ => 1,
-			};
-			// Multiply the input by the suffix
-			let bytes = number.checked_mul(suffix).unwrap_or_default();
-			// Log the parsed memory threshold
-			debug!("Memory threshold guide: {input} ({bytes} bytes)");
-			// Return the total byte threshold
-			bytes
-		})
-		.unwrap_or(0)
-});
-
 /// This structure implements a wrapper around the
 /// system allocator, or around a user-specified
 /// allocator. It tracks the current memory which
@@ -82,13 +44,6 @@ impl<A> TrackAlloc<A> {
 	/// Returns the amount of memory (in GiB) that is currently allocated
 	pub fn current_usage_as_gb(&self) -> f32 {
 		Self::gb(self.current_usage())
-	}
-	/// Checks whether the allocator is above the memory limit threshold
-	pub async fn is_beyond_threshold(&self) -> bool {
-		match *MEMORY_THRESHOLD {
-			0 => false,
-			v => self.current_usage() > v,
-		}
 	}
 	/// Performs the bytes to kibibytes conversion
 	fn kb(x: usize) -> f32 {
