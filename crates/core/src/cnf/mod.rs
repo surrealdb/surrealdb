a5ce2dd785297033ab654392590a6cb7b74ef87a
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-01-09 11:54:02 +0000

    Simplify scanner and remove upper fetch limit

--- crates/core/src/cnf/mod.rs
+++ crates/core/src/cnf/mod.rs
@@ -42,15 +42,12 @@ pub static DATASTORE_CACHE_SIZE: LazyLock<usize> =
 	lazy_env_parse!("SURREAL_DATASTORE_CACHE_SIZE", usize, 1_000);
 
 /// The maximum number of keys that should be scanned at once in general queries.
-pub static NORMAL_FETCH_SIZE: LazyLock<u32> = lazy_env_parse!("SURREAL_NORMAL_FETCH_SIZE", u32, 50);
+pub static NORMAL_FETCH_SIZE: LazyLock<u32> =
+	lazy_env_parse!("SURREAL_NORMAL_FETCH_SIZE", u32, 500);
 
 /// The maximum number of keys that should be scanned at once for export queries.
 pub static EXPORT_BATCH_SIZE: LazyLock<u32> =
 	lazy_env_parse!("SURREAL_EXPORT_BATCH_SIZE", u32, 1000);
-
-/// The maximum number of keys that should be fetched when streaming range scans in a Scanner.
-pub static MAX_STREAM_BATCH_SIZE: LazyLock<u32> =
-	lazy_env_parse!("SURREAL_MAX_STREAM_BATCH_SIZE", u32, 1000);
 
 /// The maximum number of keys that should be scanned at once per concurrent indexing batch.
 pub static INDEXING_BATCH_SIZE: LazyLock<u32> =
