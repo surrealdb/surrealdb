38ac9f5856f63d972784b434b61d4f2b888b9a21
Mees Delzenne <DelSkayn@users.noreply.github.com>
2025-01-11 21:15:29 +0100

    Fix a bug in the parser. (#5358)

--- crates/core/src/syn/parser/glue.rs
+++ crates/core/src/syn/parser/glue.rs
@@ -16,7 +16,41 @@ impl Parser<'_> {
 		let token = self.peek();
 		match token.kind {
 			TokenKind::Glued(_) => return Ok(self.peek1()),
-			t!("+") | t!("-") | TokenKind::Digits => {
+			t!("+") | t!("-") => {
+				let peek1 = self.peek_whitespace1();
+				if !matches!(peek1.kind, TokenKind::Digits) {
+					return Ok(token);
+				}
+
+				// This is a bit of an annoying special case.
+				// The problem is that `+` and `-` can be an prefix operator and a the start
+				// of a number token.
+				// To figure out which it is we need to peek the next whitespace token,
+				// This eats the digits that the lexer needs to lex the number. So we we need
+				// to backup before the digits token was consumed, clear the digits token from
+				// the token buffer so it isn't popped after parsing the number and then lex the
+				// number.
+				self.lexer.backup_before(peek1.span);
+				self.token_buffer.clear();
+				let value = self.lexer.lex_compound(token, compound::numeric_kind)?;
+				match value.value {
+					compound::NumericKind::Number(x) => {
+						self.glued_value = GluedValue::Number(x);
+						self.prepend_token(Token {
+							span: value.span,
+							kind: TokenKind::Glued(Glued::Number),
+						});
+					}
+					compound::NumericKind::Duration(x) => {
+						self.glued_value = GluedValue::Duration(Duration(x));
+						self.prepend_token(Token {
+							span: value.span,
+							kind: TokenKind::Glued(Glued::Duration),
+						});
+					}
+				}
+			}
+			TokenKind::Digits => {
 				self.pop_peek();
 				let value = self.lexer.lex_compound(token, compound::numeric_kind)?;
 				match value.value {
