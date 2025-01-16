9c2e2ac48ad0ed392e2fb95c55cc0cadd950bce5
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-01-16 13:47:15 +0000

    Ensure more than 1 worker thread is available

--- crates/core/src/kvs/threadpool.rs
+++ crates/core/src/kvs/threadpool.rs
@@ -3,9 +3,13 @@
 /// Create a new blocking threadpool
 #[cfg(not(target_family = "wasm"))]
 pub(super) fn initialise() {
+	// Get a sensible number of worker threads
+	let size = std::cmp::max(8, num_cpus::get());
+	// Create the threadpool and ignore errors
 	let _ = affinitypool::Builder::new()
 		.thread_name("surrealdb-threadpool")
-		.thread_per_core(true)
+		.thread_per_core(false)
+		.worker_threads(size)
 		.build()
 		.build_global();
 }
@@ -13,5 +17,5 @@ pub(super) fn initialise() {
 /// Create a new blocking threadpool
 #[cfg(target_family = "wasm")]
 pub(super) fn initialise() {
-	// Do nothing
+	// Do nothing in WebAssembly
 }
