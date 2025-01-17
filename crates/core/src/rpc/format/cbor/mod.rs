45c1f054628334abb5a55f775d30207d083e7fb4
Mees Delzenne <mees.delzenne@gmail.com>
2025-01-17 11:27:28 +0100

    Remove debug code

--- crates/core/src/rpc/format/cbor/mod.rs
+++ crates/core/src/rpc/format/cbor/mod.rs
@@ -24,7 +24,7 @@ pub fn req(val: Vec<u8>) -> Result<Request, RpcError> {
 pub fn res(res: impl ResTrait) -> Result<Vec<u8>, RpcError> {
 	// Convert the response into a value
 	let val: Value = res.into();
-	let val: Cbor = dbg!(val.try_into())?;
+	let val: Cbor = val.try_into()?;
 	// Create a new vector for encoding output
 	let mut res = Vec::new();
 	// Serialize the value into CBOR binary data
