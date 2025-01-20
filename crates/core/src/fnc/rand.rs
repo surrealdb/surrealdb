ce9d92dc020880c4627375a102d350474893025b
Finn Bear <finnbearlabs@gmail.com>
2025-01-19 22:19:20 -0800

    Fix #5409 - meaningful `rand::time()` retries.

--- crates/core/src/fnc/rand.rs
+++ crates/core/src/fnc/rand.rs
@@ -153,11 +153,11 @@ pub fn time((range,): (Option<(Value, Value)>,)) -> Result<Value, Error> {
 	// Set the maximum valid seconds
 	const LIMIT: i64 = 8210298412799;
 	// Check the function input arguments
-	let val = if let Some((min, max)) = range {
+	let (min, max) = if let Some((min, max)) = range {
 		match min {
 			min if (1..=LIMIT).contains(&min) => match max {
-				max if min <= max && max <= LIMIT => rand::thread_rng().gen_range(min..=max),
-				max if max >= 1 && max <= min => rand::thread_rng().gen_range(max..=min),
+				max if min <= max && max <= LIMIT => (min, max),
+				max if max >= 1 && max <= min => (max, min),
 				_ => return Err(Error::InvalidArguments {
 					name: String::from("rand::time"),
 					message: format!("To generate a time between X and Y seconds, the 2 arguments must be positive numbers and no higher than {LIMIT}."),
@@ -169,10 +169,11 @@ pub fn time((range,): (Option<(Value, Value)>,)) -> Result<Value, Error> {
 			}),
 		}
 	} else {
-		rand::thread_rng().gen_range(0..=LIMIT)
+		(0, LIMIT)
 	};
 	// Generate the random time, try up to 5 times
 	for _ in 0..5 {
+		let val = rand::thread_rng().gen_range(min..=max);
 		if let Some(v) = Utc.timestamp_opt(val, 0).earliest() {
 			return Ok(v.into());
 		}
