10c59f2d16e550323382fe04e02baf6c3bc5b0a9
Micha de Vries <micha@devrie.sh>
2025-02-11 11:28:22 +0100

    attempt fix

--- crates/core/src/api/invocation.rs
+++ crates/core/src/api/invocation.rs
@@ -28,7 +28,7 @@ pub struct ApiInvocation {
 	pub params: Object,
 	pub method: Method,
 	pub query: BTreeMap<String, String>,
-	#[cfg_attr(feature = "arbitrary", arbitrary(skip))]
+	#[cfg_attr(feature = "arbitrary", arbitrary(value = "HeaderMap::new()"))]
 	pub headers: HeaderMap,
 }
 
