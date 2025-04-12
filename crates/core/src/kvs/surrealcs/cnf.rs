e54178301016558b3b37ef3121b56441d37d84d4
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-04-12 07:48:42 +0100

    Remove `surrealcs` from core source code (#5786)

--- crates/core/src/kvs/surrealcs/cnf.rs
+++
@@ -1,4 +0,0 @@
-use std::sync::LazyLock;
-
-pub static SURREALCS_CONNECTION_POOL_SIZE: LazyLock<usize> =
-	lazy_env_parse_or_else!("SURREAL_SURREALCS_CONNECTION_POOL_SIZE", usize, |_| num_cpus::get());
