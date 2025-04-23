9447aa0139bcc81cb059e99e91714c4bd3b3de01
Micha de Vries <micha@devrie.sh>
2025-04-23 14:33:52 +0200

    Simplify get.rs

--- crates/core/src/sql/value/get.rs
+++ crates/core/src/sql/value/get.rs
@@ -252,21 +252,11 @@ impl Value {
 						let cur_doc = CursorDoc::from(self.clone());
 						let mut obj = BTreeMap::<String, Value>::new();
 						for p in p.iter() {
-							let path = p.path();
-							let v = if let Some(Part::Start(start)) = path.first() {
-								let start = start.compute(stk, ctx, opt, Some(&cur_doc)).await?;
-								stk.run(|stk| {
-									start.get(stk, ctx, opt, Some(&cur_doc), &path.as_slice()[1..])
-								})
-								.await?
-							} else {
-								stk.run(|stk| {
-									self.get(stk, ctx, opt, Some(&cur_doc), path.as_slice())
-								})
-								.await?
-							};
-
-							obj.insert(p.field().to_raw(), v);
+							let idiom = p.idiom();
+							obj.insert(
+								p.field().to_raw(),
+								stk.run(|stk| idiom.compute(stk, ctx, opt, Some(&cur_doc))).await?,
+							);
 						}
 
 						let obj = Value::from(obj);
