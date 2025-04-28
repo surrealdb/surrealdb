4a48f17316bf9284ca5b40b3976211530f2e8b7d
Stu Schwartz <stu.schwartz@surrealdb.com>
2025-04-28 06:45:44 -0700

    fmt

--- crates/core/src/sql/number/decimal.rs
+++ crates/core/src/sql/number/decimal.rs
@@ -1,6 +1,10 @@
+//! Decimal functionality and extension traits.
+
 use rust_decimal::Decimal;
 
+/// A trait to extend the Decimal type with additional functionality.
 pub trait DecimalExt {
+	/// Converts a string to a Decimal, normalizing it in the process.
 	fn from_str_normalized(s: &str) -> Result<Self, rust_decimal::Error>
 	where
 		Self: Sized;
