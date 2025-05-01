0c5c420355fa8d10c42ded8becd5850a12ecd7d7
Micha de Vries <micha@devrie.sh>
2025-05-01 09:24:31 +0200

    WIP field

---
+++ crates/core/src/sql/statements/alter/field.rs
@@ -0,0 +1,174 @@
+use crate::ctx::Context;
+use crate::dbs::Options;
+use crate::doc::CursorDoc;
+use crate::err::Error;
+use crate::iam::{Action, ResourceKind};
+use crate::sql::reference::Reference;
+use crate::sql::{Base, Ident, Permissions, Strand, Value};
+use crate::sql::{Idiom, Kind};
+
+use reblessive::tree::Stk;
+use revision::revisioned;
+use serde::{Deserialize, Serialize};
+use std::fmt::{self, Display};
+use std::ops::Deref;
+
+#[revisioned(revision = 1)]
+#[derive(Clone, Debug, Default, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
+#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+#[non_exhaustive]
+pub struct AlterFieldStatement {
+	pub name: Idiom,
+	pub what: Ident,
+	pub if_exists: bool,
+	pub flex: Option<bool>,
+	pub kind: Option<Option<Kind>>,
+	pub readonly: Option<bool>,
+	pub value: Option<Option<Value>>,
+	pub assert: Option<Option<Value>>,
+	pub default: Option<Option<Value>>,
+	pub permissions: Option<Permissions>,
+	pub comment: Option<Option<Strand>>,
+	pub reference: Option<Option<Reference>>,
+	pub default_always: Option<bool>,
+}
+
+impl AlterFieldStatement {
+	pub(crate) async fn compute(
+		&self,
+		_stk: &mut Stk,
+		ctx: &Context,
+		opt: &Options,
+		_doc: Option<&CursorDoc>,
+	) -> Result<Value, Error> {
+		// Allowed to run?
+		opt.is_allowed(Action::Edit, ResourceKind::Field, &Base::Db)?;
+		// Get the NS and DB
+		let (ns, db) = opt.ns_db()?;
+		// Fetch the transaction
+		let txn = ctx.tx();
+		// Get the table definition
+		let name = self.name.to_string();
+		let mut df = match txn.get_tb_field(ns, db, &self.what, &name).await {
+			Ok(tb) => tb.deref().clone(),
+			Err(Error::FdNotFound {
+				..
+			}) if self.if_exists => return Ok(Value::None),
+			Err(v) => return Err(v),
+		};
+		// Process the statement
+		if let Some(ref flex) = &self.flex {
+			df.flex = *flex;
+		}
+		if let Some(ref kind) = &self.kind {
+			df.kind = kind.clone();
+		}
+		if let Some(ref readonly) = &self.readonly {
+			df.readonly = *readonly;
+		}
+		if let Some(ref value) = &self.value {
+			df.value.clone_from(value);
+		}
+		if let Some(ref assert) = &self.assert {
+			df.assert.clone_from(assert);
+		}
+		if let Some(ref default) = &self.default {
+			df.default.clone_from(default);
+		}
+		if let Some(ref permissions) = &self.permissions {
+			df.permissions = permissions.clone();
+		}
+		if let Some(ref comment) = &self.comment {
+			df.comment.clone_from(comment);
+		}
+		if let Some(ref reference) = &self.reference {
+			df.reference.clone_from(reference);
+		}
+		if let Some(ref default_always) = &self.default_always {
+			df.default_always = *default_always;
+		}
+
+		// Set the table definition
+		let key = crate::key::table::fd::new(ns, db, &self.what, &name);
+		txn.set(key, revision::to_vec(&df)?, None).await?;
+		// Clear the cache
+		txn.clear();
+		// Ok all good
+		Ok(Value::None)
+	}
+}
+
+impl Display for AlterFieldStatement {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "ALTER FIELD")?;
+		if self.if_exists {
+			write!(f, " IF EXISTS")?
+		}
+		write!(f, " {} ON {}", self.name, self.what)?;
+		if let Some(ref flex) = self.flex {
+			if *flex {
+				write!(f, " FLEXIBLE")?;
+			} else {
+				write!(f, " DROP FLEXIBLE")?;
+			}
+		}
+		if let Some(ref kind) = self.kind {
+			if let Some(ref kind) = kind {
+				write!(f, " TYPE {kind}")?;
+			} else {
+				write!(f, " DROP TYPE")?;
+			}
+		}
+		if let Some(ref readonly) = self.readonly {
+			if *readonly {
+				write!(f, " READONLY")?;
+			} else {
+				write!(f, " DROP READONLY")?;
+			}
+		}
+		if let Some(ref value) = self.value {
+			if let Some(ref value) = value {
+				write!(f, " VALUE {value}")?;
+			} else {
+				write!(f, " DROP VALUE")?;
+			}
+		}
+		if let Some(ref assert) = self.assert {
+			if let Some(ref assert) = assert {
+				write!(f, " ASSERT {assert}")?;
+			} else {
+				write!(f, " DROP ASSERT")?;
+			}
+		}
+		if let Some(ref default) = self.default {
+			if let Some(ref default) = default {
+				write!(f, " DEFAULT")?;
+				if self.default_always.is_some_and(|x| x) {
+					write!(f, " ALWAYS")?;
+				}
+
+				write!(f, " {default}")?;
+			} else {
+				write!(f, " DROP DEFAULT")?;
+			}
+		}
+		if let Some(permissions) = &self.permissions {
+			write!(f, "{permissions}")?;
+		}
+		if let Some(comment) = &self.comment {
+			if let Some(ref comment) = comment {
+				write!(f, " COMMENT {comment}")?;
+			} else {
+				write!(f, " DROP COMMENT")?;
+			}
+		}
+		if let Some(reference) = &self.reference {
+			if let Some(ref reference) = reference {
+				write!(f, " REFERENCE {reference}")?;
+			} else {
+				write!(f, " DROP REFERENCE")?;
+			}
+		}
+		Ok(())
+	}
+}
--- crates/core/src/syn/parser/stmt/alter.rs
+++ crates/core/src/syn/parser/stmt/alter.rs
@@ -1,6 +1,7 @@
 use reblessive::Stk;
 
-use crate::sql::statements::alter::AlterSequenceStatement;
+use crate::sql::statements::alter::{AlterFieldStatement, AlterSequenceStatement};
+use crate::syn::error::bail;
 use crate::{
 	sql::{
 		statements::{AlterStatement, AlterTableStatement},
@@ -20,6 +21,7 @@ impl Parser<'_> {
 		let next = self.next();
 		match next.kind {
 			t!("TABLE") => self.parse_alter_table(ctx).await.map(AlterStatement::Table),
+			t!("FIELD") => self.parse_alter_field(ctx).await.map(AlterStatement::Field),
 			t!("SEQUENCE") => self.parse_alter_sequence().await.map(AlterStatement::Sequence),
 			_ => unexpected!(self, next, "a alter statement keyword"),
 		}
@@ -98,6 +100,110 @@ impl Parser<'_> {
 					} else {
 						res.changefeed = Some(Some(self.parse_changefeed()?));
 					}
+				}
+				_ => break,
+			}
+		}
+
+		Ok(res)
+	}
+
+	pub(crate) async fn parse_alter_field(
+		&mut self,
+		ctx: &mut Stk,
+	) -> ParseResult<AlterFieldStatement> {
+		let if_exists = if self.eat(t!("IF")) {
+			expected!(self, t!("EXISTS"));
+			true
+		} else {
+			false
+		};
+		let name = self.parse_local_idiom(ctx).await?;
+		expected!(self, t!("ON"));
+		self.eat(t!("TABLE"));
+		let what = self.next_token_value()?;
+		let mut res = AlterFieldStatement {
+			name,
+			what,
+			if_exists,
+			..Default::default()
+		};
+
+		loop {
+			let drop = self.eat(t!("DROP"));
+			let peek = self.peek();
+			match peek.kind {
+				t!("FLEXIBLE") => {
+					self.pop_peek();
+					res.flex = Some(!drop)
+				}
+				t!("TYPE") => {
+					self.pop_peek();
+					res.kind = Some(if drop {
+						None
+					} else {
+						Some(ctx.run(|ctx| self.parse_inner_kind(ctx)).await?)
+					});
+				}
+				t!("READONLY") => {
+					self.pop_peek();
+					res.flex = Some(!drop)
+				}
+				t!("VALUE") => {
+					self.pop_peek();
+					res.value = Some(if drop {
+						None
+					} else {
+						Some(ctx.run(|ctx| self.parse_value_field(ctx)).await?)
+					});
+				}
+				t!("ASSERT") => {
+					self.pop_peek();
+					res.value = Some(if drop {
+						None
+					} else {
+						Some(ctx.run(|ctx| self.parse_value_field(ctx)).await?)
+					});
+				}
+				t!("DEFAULT") => {
+					self.pop_peek();
+
+					if drop {
+						res.default = Some(None);
+					} else {
+						res.default_always = Some(self.eat(t!("ALWAYS")));
+						res.default = Some(Some(ctx.run(|ctx| self.parse_value_field(ctx)).await?));
+					}
+				}
+				t!("PERMISSIONS") if !drop => {
+					self.pop_peek();
+					res.permissions = Some(ctx.run(|ctx| self.parse_permission(ctx, false)).await?);
+				}
+				t!("COMMENT") => {
+					self.pop_peek();
+					res.comment = Some(if drop {
+						None
+					} else {
+						Some(self.next_token_value()?)
+					})
+				}
+				t!("REFERENCE") => {
+					if !self.settings.references_enabled {
+						bail!(
+							"Experimental capability `record_references` is not enabled",
+							@self.last_span() => "Use of `REFERENCE` keyword is still experimental"
+						)
+					}
+
+					self.pop_peek();
+					res.reference = Some(if drop {
+						None
+					} else {
+						Some(self.parse_reference(ctx).await?)
+					});
+				}
+				_ if drop => {
+					unexpected!(self, peek, "`FLEXIBLE`, `TYPE`, `READONLY`, `VALUE`, `ASSERT`, `DEFAULT`, `COMMENT`, or `REFERENCE`")
 				}
 				_ => break,
 			}
