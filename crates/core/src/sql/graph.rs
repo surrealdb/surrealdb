89947ca9b49a0c01cbd2d89042eb3396dd5ab907
Drew Ridley <drew@ridley.family>
2025-05-09 22:19:27 -0400

    fix visibility of graph subject

--- crates/core/src/sql/graph.rs
+++ crates/core/src/sql/graph.rs
@@ -197,8 +197,8 @@ impl Display for GraphSubjects {
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 #[non_exhaustive]
 pub enum GraphSubject {
-	Table(Table),
-	Range(Table, IdRange),
+	Table(pub Table),
+	Range(pub Table, pub IdRange),
 }
 
 impl GraphSubject {
