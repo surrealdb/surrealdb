258aa2c212cca2ac094445b24a76888d8b31b7b2
Stu Schwartz <stu.schwartz@surrealdb.com>
2025-05-12 13:03:21 -0700

    Update publishing to support many crates.

--- .github/scripts/prepare_vars.sh
+++ .github/scripts/prepare_vars.sh
@@ -2,45 +2,44 @@
 
 set -ux
 
-RELEASE_TYPE=$1
-GITHUB_REF=$2
+RELEASE_TYPE="${RELEASE_TYPE}"
+GITHUB_REF="${GITHUB_REF}"
 GITHUB_OUTPUT="${GITHUB_OUTPUT}"
 
 suffix=$(echo ${RELEASE_TYPE} | tr "." "\n" | sed -n 1p)
 patch=$(echo ${RELEASE_TYPE} | tr "." "\n" | sed -n 2p)
-version=$(cargo metadata --format-version 1 --no-deps | jq -r '.packages | map(select(.name == "surrealdb"))[0].version')
-echo "current-version=${version}" >> $GITHUB_OUTPUT
+current_version=$(cargo metadata --format-version 1 --no-deps | jq -r '.packages | map(select(.name == "surrealdb"))[0].version')
 
-if [[ $version == *"-"* ]]; then
-echo "Found an unexpected pre-release version, '${version}', in ${GITHUB_REF}"
-exit 400
+if [[ $current_version == *"-"* ]]; then
+    echo "Found an unexpected pre-release version, '${current_version}', in ${GITHUB_REF}"
+    exit 400
 fi
 
 if [[ "${RELEASE_TYPE}" != "release" && "${RELEASE_TYPE}" != "patch" && "${RELEASE_TYPE}" != "nightly" && $suffix != "alpha" && $suffix != "beta" && $suffix != "rc" ]]; then
-echo "'${RELEASE_TYPE}' is not a supported release type"
-exit 400
+    echo "'${RELEASE_TYPE}' is not a supported release type"
+    exit 400
 fi
 
 if [[ $suffix == "alpha" || $suffix == "beta" || $suffix == "rc" ]]; then
-if [[ -z $patch ]]; then
-    echo "Pre-releases require a patch number, e.g. beta.3"
-    exit 400
-elif ! [[ $patch =~ ^[0-9]+$ ]]; then
-    echo "The patch number should be an integer, found ${patch}"
-    exit 400
-fi
+    if [[ -z $patch ]]; then
+        echo "Pre-releases require a patch number, e.g. beta.3"
+        exit 400
+    elif ! [[ $patch =~ ^[0-9]+$ ]]; then
+        echo "The patch number should be an integer, found ${patch}"
+        exit 400
+    fi
 fi
 
 
 case $suffix in
     "release")
-        patch=$(echo ${version} | tr "." "\n" | sed -n 3p)
+        patch=$(echo ${current_version} | tr "." "\n" | sed -n 3p)
         environment=stable
         ;;
     "patch")
-        major=$(echo ${version} | tr "." "\n" | sed -n 1p)
-        minor=$(echo ${version} | tr "." "\n" | sed -n 2p)
-        currentPatch=$(echo ${version} | tr "." "\n" | sed -n 3p)
+        major=$(echo ${current_version} | tr "." "\n" | sed -n 1p)
+        minor=$(echo ${current_version} | tr "." "\n" | sed -n 2p)
+        currentPatch=$(echo ${current_version} | tr "." "\n" | sed -n 3p)
         patch=$(($currentPatch + 1))
         version=${major}.${minor}.${patch}
         environment=stable
@@ -51,21 +50,24 @@ case $suffix in
         patch=$(git show --no-patch --format=%ad --date=format:%Y%m%d%H%M%S)
         rev=$(git rev-parse --short HEAD)
         buildMetadata=${date}.${rev}
-        version=${version}-${RELEASE_TYPE}
+        version=${current_version}-${RELEASE_TYPE}
         environment=${RELEASE_TYPE}
         ;;
     "rc")
-        version=${version}-${RELEASE_TYPE}
+        version=${current_version}-${RELEASE_TYPE}
         patch=$(($patch - 1))
         environment=release-candidate
         ;;
     *)
-        version=${version}-${RELEASE_TYPE}
+        version=${current_version}-${RELEASE_TYPE}
         patch=$(($patch - 1))
         environment=${suffix}
         ;;
 esac
 
+# Output the variables to the GitHub Actions environment.
+
+echo "current-version=${current_version}" >> $GITHUB_OUTPUT
 echo "version=${version}" >> $GITHUB_OUTPUT
 echo "patch=${patch}" >> $GITHUB_OUTPUT
 echo "environment=${environment}" >> $GITHUB_OUTPUT
@@ -73,7 +75,7 @@ echo "build-metadata=${buildMetadata}" >> $GITHUB_OUTPUT
 echo "release-branch=releases/${version}" >> $GITHUB_OUTPUT
 
 if [[ "${environment}" == "nightly" ]]; then
-echo "name=${environment}" >> $GITHUB_OUTPUT
+    echo "name=${environment}" >> $GITHUB_OUTPUT
 else
-echo "name=v${version}" >> $GITHUB_OUTPUT
+    echo "name=v${version}" >> $GITHUB_OUTPUT
 fi
