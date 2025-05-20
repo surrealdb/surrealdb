1d935236eda95352996b9653a44f8d7f288fc987
Roms1383 <romain.kelifa@gmail.com>
2025-05-20 18:05:14 +0700

    Internalize code

--- crates/core/src/fnc/crypto.rs
+++ crates/core/src/fnc/crypto.rs
@@ -218,3 +218,63 @@ pub mod scrypt {
 		Ok(hash.into())
 	}
 }
+
+/// Code borrowed from [joaat-rs](https://github.com/Pocakking/joaat-rs).
+/// All credits to its author.
+mod joaat {
+	use std::{default::Default, hash::Hasher};
+
+	pub struct JoaatHasher(u32);
+
+	impl Default for JoaatHasher {
+		#[inline]
+		fn default() -> Self {
+			Self(0)
+		}
+	}
+
+	impl Hasher for JoaatHasher {
+		#[inline]
+		fn finish(&self) -> u64 {
+			let mut hash = self.0;
+			hash = hash.wrapping_add(hash.wrapping_shl(3));
+			hash ^= hash.wrapping_shr(11);
+			hash = hash.wrapping_add(hash.wrapping_shl(15));
+			hash as _
+		}
+
+		#[inline]
+		fn write(&mut self, bytes: &[u8]) {
+			for byte in bytes.iter() {
+				self.0 = self.0.wrapping_add(u32::from(*byte));
+				self.0 = self.0.wrapping_add(self.0.wrapping_shl(10));
+				self.0 ^= self.0.wrapping_shr(6);
+			}
+		}
+	}
+
+	/// Hashes a slice of bytes.
+	#[inline]
+	#[must_use]
+	pub fn hash_bytes(bytes: &[u8]) -> u32 {
+		let mut hasher = JoaatHasher::default();
+		hasher.write(bytes);
+		hasher.finish() as _
+	}
+
+	#[cfg(test)]
+	#[allow(clippy::unreadable_literal)]
+	mod tests {
+		use super::*;
+
+		#[test]
+		fn test() {
+			assert_eq!(hash_bytes(b""), 0);
+			assert_eq!(hash_bytes(b"a"), 0xCA2E9442);
+			assert_eq!(hash_bytes(b"b"), 0x00DB819B);
+			assert_eq!(hash_bytes(b"c"), 0xEEBA5D59);
+			assert_eq!(hash_bytes(b"The quick brown fox jumps over the lazy dog"), 0x519E91F5);
+			assert_eq!(hash_bytes(b"The quick brown fox jumps over the lazy dog."), 0xAE8EF3CB);
+		}
+	}
+}
