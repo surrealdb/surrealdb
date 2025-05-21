ec9057c8d824aeb0597f42e958cfaaab347a0026
Stu Schwartz <stu.schwartz@surrealdb.com>
2025-05-21 10:10:02 -0700

    Cleanup

--- .github/scripts/patch_crate.sh
+++ .github/scripts/patch_crate.sh
@@ -11,7 +11,7 @@ COMMAND="${1}"
 
 # Note: Keep these in sync with the `members` array in the `Cargo.toml` file.
 members=("surrealdb-catalog" "surrealdb-common" "surrealdb-expr" "surrealdb-core" "surrealdb-sql" "surrealdb")
-member_paths=("crates/catalog", "crates/common" "crates/expr" "crates/core" "crates/sql" "crates/sdk")
+member_paths=("crates/catalog" "crates/common" "crates/expr" "crates/core" "crates/sql" "crates/sdk")
 
 # Get the path of a crate based on its name
 # Usage: get_member_path <crate_name>
