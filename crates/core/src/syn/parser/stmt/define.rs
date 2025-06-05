7713472eab7ec1e61caef101abb6763364fd9676
Mees Delzenne <mees.delzenne@gmail.com>
2025-06-05 17:54:58 +0200

    Remove deprecated login statements

--- crates/core/src/syn/parser/stmt/define.rs
+++ crates/core/src/syn/parser/stmt/define.rs
@@ -11,14 +11,11 @@ use crate::sql::statements::define::config::graphql::{GraphQLConfig, TableConfig
 use crate::sql::statements::define::config::{ConfigInner, graphql};
 use crate::sql::statements::define::user::PassType;
 use crate::sql::statements::define::{
-	ApiAction, DefineBucketStatement, DefineConfigStatement, DefineDefault, DefineKind,
-	DefineSequenceStatement,
-};
-use crate::sql::statements::{
-	DefineAccessStatement, DefineAnalyzerStatement, DefineApiStatement, DefineDatabaseStatement,
+	ApiAction, DefineAccessStatement, DefineAnalyzerStatement, DefineApiStatement,
+	DefineBucketStatement, DefineConfigStatement, DefineDatabaseStatement, DefineDefault,
 	DefineEventStatement, DefineFieldStatement, DefineFunctionStatement, DefineIndexStatement,
-	DefineNamespaceStatement, DefineParamStatement, DefineStatement, DefineTableStatement,
-	DefineUserStatement,
+	DefineKind, DefineNamespaceStatement, DefineParamStatement, DefineSequenceStatement,
+	DefineStatement, DefineTableStatement, DefineUserStatement,
 };
 use crate::sql::tokenizer::Tokenizer;
 use crate::sql::{
@@ -38,12 +35,10 @@ impl Parser<'_> {
 	) -> ParseResult<DefineStatement> {
 		let next = self.next();
 		match next.kind {
-			t!("NAMESPACE") => {
-				self.parse_define_namespace(stk).await.map(DefineStatement::Namespace)
-			}
-			t!("DATABASE") => self.parse_define_database(stk).await.map(DefineStatement::Database),
+			t!("NAMESPACE") => self.parse_define_namespace().map(DefineStatement::Namespace),
+			t!("DATABASE") => self.parse_define_database(stk).map(DefineStatement::Database),
 			t!("FUNCTION") => self.parse_define_function(stk).await.map(DefineStatement::Function),
-			t!("USER") => self.parse_define_user(stk).await.map(DefineStatement::User),
+			t!("USER") => self.parse_define_user(stk).map(DefineStatement::User),
 			t!("PARAM") => self.parse_define_param(stk).await.map(DefineStatement::Param),
 			t!("TABLE") => self.parse_define_table(stk).await.map(DefineStatement::Table),
 			t!("API") => self.parse_define_api(stk).await.map(DefineStatement::Api),
