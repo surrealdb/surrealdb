ceccd7e337a115f5bdc24c3854965b5642c656c4
Dave MacLeod <56599343+Dhghomon@users.noreply.github.com>
2025-06-10 14:50:01 +0900

    Ah, it's an aggregate thing

--- crates/core/src/fnc/count.rs
+++ crates/core/src/fnc/count.rs
@@ -9,7 +9,7 @@ pub fn count((Optional(arg),): (Optional<Value>,)) -> Result<Value> {
 			Value::Array(v) => v.iter().filter(|v| v.is_truthy()).count().into(),
 			v => (v.is_truthy() as i64).into(),
 		})
-		.unwrap_or_else(|| 0.into()))
+		.unwrap_or_else(|| 1.into()))
 }
 
 // Counts all values, even if they are not truthy
@@ -19,5 +19,5 @@ pub fn count_value((Optional(arg),): (Optional<Value>,)) -> Result<Value> {
 			Value::Array(v) => v.iter().count().into(),
 			_ => 1.into(),
 		})
-		.unwrap_or_else(|| 0.into()))
+		.unwrap_or_else(|| 1.into()))
 }
