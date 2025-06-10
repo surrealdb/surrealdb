aa2d69be71af7960eac12d1114fc5abe5dd3c646
arriqaaq <farhan.khan@surrealdb.com>
2025-06-10 12:29:42 +0530

    avoid using commit pool for wasm in skv memory

--- crates/core/src/kvs/mem/mod.rs
+++ crates/core/src/kvs/mem/mod.rs
@@ -5,15 +5,17 @@ use crate::key::debug::Sprintable;
 use crate::kvs::{Key, Val, Version};
 use std::fmt::Debug;
 use std::ops::Range;
-use std::sync::OnceLock;
 use surrealkv::Options;
 use surrealkv::Store;
 use surrealkv::Transaction as Tx;
 
 use super::{Check, KeyEncode};
 
+#[cfg(not(target_family = "wasm"))]
+use std::sync::OnceLock;
+#[cfg(not(target_family = "wasm"))]
 pub(crate) static SKV_COMMIT_POOL: OnceLock<affinitypool::Threadpool> = OnceLock::new();
-
+#[cfg(not(target_family = "wasm"))]
 pub(crate) fn commit_pool() -> &'static affinitypool::Threadpool {
 	SKV_COMMIT_POOL.get_or_init(|| {
 		affinitypool::Builder::new()
@@ -152,7 +154,10 @@ impl super::api::Transaction for Transaction {
 			self.inner.take().ok_or_else(|| Error::Tx("Transaction inner is None".into()))?;
 
 		// Commit this transaction in the pool
+		#[cfg(not(target_family = "wasm"))]
 		commit_pool().spawn(move || inner.commit()).await?;
+		#[cfg(target_family = "wasm")]
+		inner.commit()?;
 
 		// Continue
 		Ok(())
