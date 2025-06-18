c967d837b5a749f0262b5d37973684895d83028c
Stu Schwartz <stu.schwartz@surrealdb.com>
2025-06-18 19:33:03 +0100

    tidy

--- crates/core/build.rs
+++ crates/core/build.rs
@@ -16,31 +16,6 @@ fn main() {
 		println!("cargo::rustc-check-cfg=cfg(storage)");
 	}
 
-	let mut config = prost_build::Config::new();
-	config.btree_map(["."]);
-	config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
-	config.compile_well_known_types();
-
-	if let Err(err) = config.compile_protos(&[
-			"protocol/ast.proto",
-			"protocol/rpc.proto",
-			"protocol/value.proto",
-		],
-                                &["protocol/"]) {
-		eprintln!("Failed to compile protobufs: {}", err);
-		std::process::exit(1);
-	}
-
-	::capnpc::CompilerCommand::new()
-		.file("protocol/expr.capnp")
-        .file("protocol/rpc.capnp")
-		.import_path("protocol/")
-        .no_standard_import()
-		.default_parent_module(vec!["protocol".into()])
-        .run()
-        .expect("compiling schema");
-
-
 	BuilderOptions::new_with_files([
 		"protocol/common.fbs",
 		"protocol/expr.fbs",
