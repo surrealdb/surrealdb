094ab45bff2ce1d59945c1424ce680157974824c
Rushmore Mushambi <rushmore@surrealdb.com>
2025-06-24 16:11:52 +0100

    Tokio Console integration for improved observability

---
+++ src/telemetry/console/mod.rs
@@ -0,0 +1,13 @@
+use std::time::Duration;
+use tracing::Subscriber;
+use tracing_subscriber::Layer;
+
+pub fn new<S>() -> impl Layer<S> + Send + Sync
+where
+	S: Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a> + Send + Sync,
+{
+	console_subscriber::ConsoleLayer::builder()
+		.retention(Duration::from_secs(60))
+		.server_addr(([127, 0, 0, 1], 5555))
+		.spawn()
+}
