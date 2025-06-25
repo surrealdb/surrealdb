7f528ce0e7b284b6747b55283aae0085a01d5dfa
Micha de Vries <micha@devrie.sh>
2025-06-25 13:38:12 +0200

    BP-2.3: Ensure NS/DB is set; Ensure transaction is committed or cancelled (#5962) (#6123)

--- src/net/api.rs
+++ src/net/api.rs
@@ -57,6 +57,8 @@ async fn handler(
 	let url = format!("/api/{ns}/{db}/{path}");
 	// Get a database reference
 	let ds = &state.datastore;
+	// Update the session with the NS & DB
+	let session = session.with_ns(&ns).with_db(&db);
 	// Check if the experimental capability is enabled
 	if !state.datastore.get_capabilities().allows_experimental(&ExperimentalTarget::DefineApi) {
 		warn!("Experimental capability for API routes is not enabled");
@@ -84,8 +86,8 @@ async fn handler(
 	let apis = tx.all_db_apis(&ns, &db).await.map_err(Error::from)?;
 	let segments: Vec<&str> = path.split('/').filter(|x| !x.is_empty()).collect();
 
-	let (mut res, res_instruction) =
-		if let Some((api, params)) = apis.as_ref().find_api(segments, method) {
+	let res = match apis.as_ref().find_api(segments, method) {
+		Some((api, params)) => {
 			let invocation = ApiInvocation {
 				params,
 				method,
@@ -103,16 +105,23 @@ async fn handler(
 				)
 				.await
 			{
-				Ok(Some(v)) => v,
-				Err(e) => return Err(Error::from(e)),
-				_ => return Err(Error::NotFound(url)),
+				Ok(Some(v)) => Ok(v),
+				Ok(None) => Err(Error::NotFound(url)),
+				Err(e) => Err(Error::from(e)),
 			}
-		} else {
-			return Err(Error::NotFound(url));
-		};
+		}
+		_ => Err(Error::NotFound(url)),
+	};
 
-	// Commit the transaction
-	tx.commit().await.map_err(Error::from)?;
+	// Handle committing or cancelling the transaction
+	if res.is_ok() {
+		tx.commit().await.map_err(Error::from)?;
+	} else {
+		tx.cancel().await.map_err(Error::from)?;
+	}
+
+	// Process the result
+	let (mut res, res_instruction) = res?;
 
 	let res_body: Vec<u8> = if let Some(body) = res.body {
 		match res_instruction {
