682bf2223a5229734ca18b8859949d1ab1f2e595
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2025-06-25 15:11:01 +0100

    [Backport 2.2] Add support for log files, json log format, and advanced filtering (#6125)
    Co-authored-by: Tobie Morgan Hitchcock <tobie@surrealdb.com>
    (cherry picked from commit 3f900ca82fae8d1282aac32ac9de2dd178b98425)

--- src/telemetry/logs/mod.rs
+++ src/telemetry/logs/mod.rs
@@ -1,41 +1,133 @@
-use crate::cli::validator::parser::env_filter::CustomEnvFilter;
+use crate::cli::validator::parser::tracing::CustomFilter;
+use crate::cli::LogFormat;
 use crate::err::Error;
+use tracing::Level;
 use tracing::Subscriber;
+use tracing_appender::non_blocking::NonBlocking;
 use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::fmt::writer::MakeWriterExt;
 use tracing_subscriber::Layer;
 
-pub fn new<S>(filter: CustomEnvFilter) -> Result<Box<dyn Layer<S> + Send + Sync>, Error>
+pub fn file<S>(
+	filter: CustomFilter,
+	file: NonBlocking,
+	format: LogFormat,
+) -> Result<Box<dyn Layer<S> + Send + Sync>, Error>
 where
 	S: Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a> + Send + Sync,
 {
-	#[cfg(not(debug_assertions))]
-	{
-		Ok(tracing_subscriber::fmt::layer()
-			.compact()
-			.with_ansi(true)
+	// Log ERROR, WARN, INFO, DEBUG, TRACE to rotating file
+	let writer = file.with_max_level(Level::TRACE);
+	// Configure the log file tracer
+	let layer = tracing_subscriber::fmt::layer();
+	// Configure the log file format
+	match format {
+		LogFormat::Json => Ok(layer
+			.json()
+			.with_ansi(false)
 			.with_file(false)
 			.with_target(true)
 			.with_line_number(false)
 			.with_thread_ids(false)
 			.with_thread_names(false)
 			.with_span_events(FmtSpan::NONE)
-			.with_writer(std::io::stderr)
-			.with_filter(filter.0)
-			.boxed())
-	}
-	#[cfg(debug_assertions)]
-	{
-		Ok(tracing_subscriber::fmt::layer()
+			.with_writer(writer)
+			.with_filter(filter.env())
+			.with_filter(filter.span_filter::<S>())
+			.boxed()),
+		LogFormat::Text => Ok(layer
 			.compact()
-			.with_ansi(true)
-			.with_file(true)
+			.with_ansi(false)
+			.with_file(false)
 			.with_target(true)
-			.with_line_number(true)
+			.with_line_number(false)
 			.with_thread_ids(false)
 			.with_thread_names(false)
 			.with_span_events(FmtSpan::NONE)
-			.with_writer(std::io::stderr)
-			.with_filter(filter.0)
-			.boxed())
+			.with_writer(writer)
+			.with_filter(filter.env())
+			.with_filter(filter.span_filter::<S>())
+			.boxed()),
+	}
+}
+
+pub fn output<S>(
+	filter: CustomFilter,
+	stdout: NonBlocking,
+	stderr: NonBlocking,
+	format: LogFormat,
+) -> Result<Box<dyn Layer<S> + Send + Sync>, Error>
+where
+	S: Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a> + Send + Sync,
+{
+	// Log INFO, DEBUG, TRACE to stdout, WARN, ERROR to stderr
+	let writer = stderr.with_max_level(Level::WARN).or_else(stdout);
+	// Configure the log tracer for production
+	#[cfg(not(debug_assertions))]
+	{
+		let layer = tracing_subscriber::fmt::layer();
+		// Configure the log console writer
+		match format {
+			LogFormat::Json => Ok(layer
+				.json()
+				.with_ansi(true)
+				.with_file(false)
+				.with_target(true)
+				.with_line_number(false)
+				.with_thread_ids(false)
+				.with_thread_names(false)
+				.with_span_events(FmtSpan::NONE)
+				.with_writer(writer)
+				.with_filter(filter.env())
+				.with_filter(filter.span_filter::<S>())
+				.boxed()),
+			LogFormat::Text => Ok(layer
+				.compact()
+				.with_ansi(true)
+				.with_file(false)
+				.with_target(true)
+				.with_line_number(false)
+				.with_thread_ids(false)
+				.with_thread_names(false)
+				.with_span_events(FmtSpan::NONE)
+				.with_writer(writer)
+				.with_filter(filter.env())
+				.with_filter(filter.span_filter::<S>())
+				.boxed()),
+		}
+	}
+	// Configure the log tracer for development
+	#[cfg(debug_assertions)]
+	{
+		let layer = tracing_subscriber::fmt::layer();
+		// Configure the log console writer
+		match format {
+			LogFormat::Json => Ok(layer
+				.json()
+				.with_ansi(true)
+				.with_file(true)
+				.with_target(true)
+				.with_line_number(true)
+				.with_thread_ids(false)
+				.with_thread_names(false)
+				.with_span_events(FmtSpan::NONE)
+				.with_writer(writer)
+				.with_filter(filter.env())
+				.with_filter(filter.span_filter::<S>())
+				.boxed()),
+			LogFormat::Text => Ok(layer
+				.compact()
+				.with_ansi(true)
+				.with_file(true)
+				.with_target(true)
+				.with_line_number(true)
+				.with_thread_ids(false)
+				.with_thread_names(false)
+				.with_span_events(FmtSpan::NONE)
+				.with_writer(writer)
+				.with_filter(filter.env())
+				.with_filter(filter.span_filter::<S>())
+				.boxed()),
+		}
 	}
 }
