42d8f58abad43e146b8a0ccdf706df825ce9592b
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2025-06-30 11:44:26 -0300

    Fix tests

--- crates/core/src/kvs/tasklease.rs
+++ crates/core/src/kvs/tasklease.rs
@@ -198,7 +198,7 @@ mod tests {
 	use crate::kvs::clock::{FakeClock, SizedClock};
 	use crate::kvs::ds::{DatastoreFlavor, TransactionFactory};
 	use crate::kvs::tasklease::{LeaseHandler, TaskLeaseType};
-	use chrono::{DateTime, Utc};
+	use chrono::Utc;
 	use std::sync::Arc;
 	use std::time::{Duration, Instant};
 	#[cfg(feature = "kv-rocksdb")]
