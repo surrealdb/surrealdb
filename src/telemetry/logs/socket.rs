4afcd7edb4159d257539d1ec42172ab88207af4e
Michel Hollands <michel.hollands@surrealdb.com>
2025-07-17 11:23:58 +0100

    Add TODOs
    Signed-off-by: Michel Hollands <michel.hollands@surrealdb.com>

--- src/telemetry/logs/socket.rs
+++ src/telemetry/logs/socket.rs
@@ -2,6 +2,8 @@ use std::io::{Result, Write};
 use std::net::{TcpStream, ToSocketAddrs};
 
 /// Simple writer that outputs log lines to a TCP socket.
+/// TODO: reconnect when the connection fails
+/// TODO: metric when connection fails
 pub struct SocketWriter {
     stream: TcpStream,
 }
