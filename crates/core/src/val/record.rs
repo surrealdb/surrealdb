0bc7bcd9340929dde2d64082a12ee3baf5c80a82
Rushmore Mushambi <rushmore@surrealdb.com>
2025-08-15 16:38:56 +0100

    Refactor the record module

--- crates/core/src/val/record.rs
+++ crates/core/src/val/record.rs
@@ -7,8 +7,51 @@ use serde::{Deserialize, Serialize};
 
 use crate::val::Value;
 
-#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
-#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
+/// Represents a record stored in the database
+///
+/// `Data` is the type of the data stored in the record
+#[revisioned(revision = 1)]
+#[derive(Clone, Debug, Default, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
+pub struct Record {
+	pub(crate) metadata: Option<Metadata>,
+	// TODO (DB-655): Switch to `Object`.
+	pub(crate) data: Data,
+}
+
+impl Record {
+	pub(crate) fn new(data: Data) -> Self {
+		Self {
+			metadata: None,
+			data,
+		}
+	}
+
+	pub const fn is_edge(&self) -> bool {
+		matches!(
+			&self.metadata,
+			Some(Metadata {
+				record_type: Some(RecordType::Edge),
+				..
+			})
+		)
+	}
+
+	pub(crate) fn set_record_type(&mut self, rtype: RecordType) {
+		match &mut self.metadata {
+			Some(metadata) => {
+				metadata.record_type = Some(rtype);
+			}
+			metadata => {
+				*metadata = Some(Metadata {
+					record_type: Some(rtype),
+				});
+			}
+		}
+	}
+}
+
+#[derive(Clone, Debug, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
+#[serde(untagged)]
 pub(crate) enum Data {
 	Mutable(Value),
 	ReadOnly(Arc<Value>),
@@ -48,54 +91,10 @@ impl Default for Data {
 	}
 }
 
-/// Represents a record stored in the database
-///
-/// `Data` is the type of the data stored in the record
-#[derive(Clone, Debug, Default, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
-#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
-pub struct Record {
-	pub(crate) metadata: Option<Metadata>,
-	// TODO (DB-655): Switch to `Object`.
-	pub(crate) data: Data,
-}
-
-impl Record {
-	pub(crate) fn new(data: Data) -> Self {
-		Self {
-			metadata: None,
-			data,
-		}
-	}
-
-	pub const fn is_edge(&self) -> bool {
-		matches!(
-			&self.metadata,
-			Some(Metadata {
-				record_type: Some(RecordType::Edge),
-				..
-			})
-		)
-	}
-
-	pub(crate) fn set_record_type(&mut self, rtype: RecordType) {
-		match &mut self.metadata {
-			Some(metadata) => {
-				metadata.record_type = Some(rtype);
-			}
-			metadata => {
-				*metadata = Some(Metadata {
-					record_type: Some(rtype),
-				});
-			}
-		}
-	}
-}
-
-impl Revisioned for Record {
+impl Revisioned for Data {
 	#[inline]
 	fn serialize_revisioned<W: std::io::Write>(&self, writer: &mut W) -> Result<(), Error> {
-		self.metadata.serialize_revisioned(writer)?;
-		match &self.data {
+		match self {
 			Data::Mutable(v) => v.serialize_revisioned(writer),
 			Data::ReadOnly(v) => v.serialize_revisioned(writer),
 		}
@@ -103,12 +102,7 @@ impl Revisioned for Record {
 
 	#[inline]
 	fn deserialize_revisioned<R: std::io::Read>(reader: &mut R) -> Result<Self, Error> {
-		let metadata: Option<Metadata> = Revisioned::deserialize_revisioned(reader)?;
-		let data = Value::deserialize_revisioned(reader).map(Data::Mutable)?;
-		Ok(Self {
-			metadata,
-			data,
-		})
+		Value::deserialize_revisioned(reader).map(Self::Mutable)
 	}
 
 	fn revision() -> u16 {
@@ -118,14 +112,12 @@ impl Revisioned for Record {
 
 #[revisioned(revision = 1)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
-#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 pub(crate) struct Metadata {
 	record_type: Option<RecordType>,
 }
 
 #[revisioned(revision = 1)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, PartialOrd, Serialize, Deserialize, Hash)]
-#[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 pub(crate) enum RecordType {
 	Edge,
 }
