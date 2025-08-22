c829b86bae69645ba9600cb87afe1f3c4bf876ee
Micha de Vries <micha@devrie.sh>
2025-08-22 12:13:59 +0200

    DB-690 fix LQ doc (#6247) (#6261)

--- crates/sdk/tests/live.rs
+++ crates/sdk/tests/live.rs
@@ -1,5 +1,7 @@
 mod helpers;
 mod parse;
+use std::time::Duration;
+
 use helpers::new_ds;
 use helpers::skip_ok;
 use parse::Parse;
@@ -7,6 +9,7 @@ use surrealdb::dbs::Session;
 use surrealdb::err::Error;
 use surrealdb::sql::Thing;
 use surrealdb::sql::Value;
+use surrealdb_core::dbs::Action;
 
 #[tokio::test]
 async fn live_permissions() -> Result<(), Error> {
@@ -68,5 +71,174 @@ async fn live_permissions() -> Result<(), Error> {
 	.into();
 	assert_eq!(tmp, val);
 	//
+	Ok(())
+}
+
+#[tokio::test]
+async fn live_document_reduction() -> Result<(), Error> {
+	// Create a new datastore with notifications enabled
+	let dbs = new_ds().await?.with_auth_enabled(true).with_notifications();
+	let Some(channel) = dbs.notifications() else {
+		unreachable!("No notification channel");
+	};
+
+	// Create sessions for owner and record user
+	let ses_owner = Session::owner().with_ns("test").with_db("test").with_rt(true);
+	let ses_record =
+		Session::for_record("test", "test", "test", Thing::from(("user", "test")).into())
+			.with_rt(true);
+
+	// Setup the scenario
+	let sql = "
+			DEFINE TABLE test SCHEMAFULL PERMISSIONS FULL;
+			DEFINE FIELD visible ON test PERMISSIONS FULL;
+			DEFINE FIELD hidden ON test PERMISSIONS NONE;
+		";
+	let res = &mut dbs.execute(sql, &ses_owner, None).await?;
+	assert_eq!(res.len(), 3);
+	skip_ok(res, 3)?;
+
+	////////////////////////////////////////////////////////////
+
+	// Create a simple live query
+	let sql = "LIVE SELECT * FROM test;";
+	let res = &mut dbs.execute(sql, &ses_record, None).await?;
+	assert_eq!(res.len(), 1);
+	let lqid = res.remove(0).result?;
+	assert_eq!(lqid.kindof(), "uuid");
+
+	////////////////////////////////////////////////////////////
+
+	// Create a record
+	let sql = "CREATE test:1 SET hidden = 123, visible = 'abc';";
+	let res = &mut dbs.execute(sql, &ses_owner, None).await?;
+	assert_eq!(res.len(), 1);
+	//
+	let tmp = res.remove(0).result?;
+	let val = Value::parse(
+		"[
+			{
+				id: test:1,
+				visible: 'abc',
+				hidden: 123,
+			},
+		]",
+	)
+	.into();
+	assert_eq!(tmp, val);
+
+	// Receive the notification
+	let tmp = channel.recv().await?;
+	assert_eq!(tmp.action, Action::Create);
+
+	// Check the notification
+	let val = Value::parse(
+		"{
+			id: test:1,
+			visible: 'abc',
+		}",
+	)
+	.into();
+	assert_eq!(tmp.result, val);
+
+	////////////////////////////////////////////////////////////
+
+	// Update the record
+	let sql = "UPDATE test:1 SET hidden = 456, visible = 'def';";
+	let res = &mut dbs.execute(sql, &ses_owner, None).await?;
+	assert_eq!(res.len(), 1);
+	//
+	let tmp = res.remove(0).result?;
+	let val = Value::parse(
+		"[
+			{
+				id: test:1,
+				visible: 'def',
+				hidden: 456,
+			}
+		]",
+	)
+	.into();
+	assert_eq!(tmp, val);
+
+	// Receive the notification
+	let tmp = channel.recv().await?;
+	assert_eq!(tmp.action, Action::Update);
+
+	// Check the notification
+	let val = Value::parse(
+		"{
+			id: test:1,
+			visible: 'def',
+		}",
+	)
+	.into();
+	assert_eq!(tmp.result, val);
+
+	////////////////////////////////////////////////////////////
+
+	// Delete the record
+	let sql = "DELETE test:1;";
+	let res = &mut dbs.execute(sql, &ses_owner, None).await?;
+	assert_eq!(res.len(), 1);
+	skip_ok(res, 1)?;
+
+	// Receive the notification
+	let tmp = channel.recv().await?;
+	assert_eq!(tmp.action, Action::Delete);
+
+	// Check the notification
+	let val = Value::parse(
+		"{
+			id: test:1,
+			visible: 'def',
+		}",
+	)
+	.into();
+	assert_eq!(tmp.result, val);
+
+	////////////////////////////////////////////////////////////
+
+	// Kill the live query
+	let sql = "KILL $uuid";
+	let res = &mut dbs.execute(sql, &ses_owner, Some(map!("uuid".to_string() => lqid))).await?;
+	assert_eq!(res.len(), 1);
+	skip_ok(res, 1)?;
+
+	// Create a live query with a WHERE clause
+	let sql = "LIVE SELECT * FROM test WHERE hidden = 123;";
+	let res = &mut dbs.execute(sql, &ses_record, None).await?;
+	assert_eq!(res.len(), 1);
+	let lqid = res.remove(0).result?;
+	assert_eq!(lqid.kindof(), "uuid");
+
+	////////////////////////////////////////////////////////////
+
+	// Create a record
+	let sql = "CREATE test:2 SET hidden = 123, visible = 'abc';";
+	let res = &mut dbs.execute(sql, &ses_owner, None).await?;
+	assert_eq!(res.len(), 1);
+	//
+	let tmp = res.remove(0).result?;
+	let val = Value::parse(
+		"[
+			{
+				id: test:2,
+				visible: 'abc',
+				hidden: 123,
+			},
+		]",
+	)
+	.into();
+	assert_eq!(tmp, val);
+
+	// Assert no notification is received
+	tokio::time::sleep(Duration::from_secs(1)).await;
+	let res = channel.try_recv();
+	assert!(res.is_err());
+
+	////////////////////////////////////////////////////////////
+
+	// Test passed!
 	Ok(())
 }
