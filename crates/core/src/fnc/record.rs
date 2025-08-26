a299a41a0057cacff93d62eb2e48f8eb5e5e212f
Micha de Vries <micha@devrie.sh>
2025-08-26 14:18:02 +0200

    fmt

--- crates/core/src/fnc/record.rs
+++ crates/core/src/fnc/record.rs
@@ -132,11 +132,11 @@ pub mod is {
 
 				// We may already know if the record is an edge based on the current document
 				// As an example, we may use this function inside a select predicate or filter
-				// get_record() can potentially do a new fetch on the KV store, which at scale can be expensive
-				// Let's short circuit if the rid matches the current document
+				// get_record() can potentially do a new fetch on the KV store, which at scale can
+				// be expensive Let's short circuit if the rid matches the current document
 				if let Some(doc) = doc {
 					if doc.rid.as_ref().is_some_and(|x| x.as_ref() == &rid) {
-						return Ok(Value::Bool(doc.doc.is_edge()))
+						return Ok(Value::Bool(doc.doc.is_edge()));
 					}
 				}
 
