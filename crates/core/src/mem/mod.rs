a220fcb4aec493786dff487077ff3bae5ea19cad
Rushmore Mushambi <rushmore@surrealdb.com>
2025-09-15 19:21:33 +0100

    Drop `mimalloc`

--- crates/core/src/mem/mod.rs
+++ crates/core/src/mem/mod.rs
@@ -19,43 +19,15 @@ pub static ALLOC: track::TrackAlloc<std::alloc::System> =
 	track::TrackAlloc::new(std::alloc::System);
 
 #[cfg(feature = "allocator")]
-#[cfg(target_os = "android")]
-#[global_allocator]
-pub static ALLOC: track::TrackAlloc<jemallocator::Jemalloc> =
-	track::TrackAlloc::new(jemallocator::Jemalloc);
-
-#[cfg(feature = "allocator")]
-#[cfg(target_os = "freebsd")]
-#[global_allocator]
-pub static ALLOC: track::TrackAlloc<jemallocator::Jemalloc> =
-	track::TrackAlloc::new(jemallocator::Jemalloc);
-
-#[cfg(feature = "allocator")]
-#[cfg(target_os = "ios")]
-#[global_allocator]
-pub static ALLOC: track::TrackAlloc<mimalloc::MiMalloc> =
-	track::TrackAlloc::new(mimalloc::MiMalloc);
-
-#[cfg(feature = "allocator")]
-#[cfg(target_os = "linux")]
-#[global_allocator]
-pub static ALLOC: track::TrackAlloc<jemallocator::Jemalloc> =
-	track::TrackAlloc::new(jemallocator::Jemalloc);
-
-#[cfg(feature = "allocator")]
-#[cfg(target_os = "macos")]
-#[global_allocator]
-pub static ALLOC: track::TrackAlloc<mimalloc::MiMalloc> =
-	track::TrackAlloc::new(mimalloc::MiMalloc);
-
-#[cfg(feature = "allocator")]
-#[cfg(target_os = "netbsd")]
-#[global_allocator]
-pub static ALLOC: track::TrackAlloc<jemallocator::Jemalloc> =
-	track::TrackAlloc::new(jemallocator::Jemalloc);
-
-#[cfg(feature = "allocator")]
-#[cfg(target_os = "openbsd")]
+#[cfg(any(
+	target_os = "android",
+	target_os = "freebsd",
+	target_os = "ios",
+	target_os = "linux",
+	target_os = "macos",
+	target_os = "netbsd",
+	target_os = "openbsd"
+))]
 #[global_allocator]
 pub static ALLOC: track::TrackAlloc<jemallocator::Jemalloc> =
 	track::TrackAlloc::new(jemallocator::Jemalloc);
