b112c104bbdc0577d5bdad27bcbb049fb72db581
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-10-06 07:46:50 +0200

    Add `string::capitalize` function (#6452)

--- crates/core/src/fnc/mod.rs
+++ crates/core/src/fnc/mod.rs
@@ -337,6 +337,7 @@ pub fn synchronous(
 		"session::rd" => session::rd(ctx),
 		"session::token" => session::token(ctx),
 		//
+		"string::capitalize" => string::capitalize,
 		"string::concat" => string::concat,
 		"string::contains" => string::contains,
 		"string::ends_with" => string::ends_with,
@@ -1159,6 +1160,7 @@ pub async fn idiom(
 				args.clone(),
 				"no such method found for the string type",
 				//
+				"capitalize" => string::capitalize,
 				"concat" => string::concat,
 				"contains" => string::contains,
 				"ends_with" => string::ends_with,
