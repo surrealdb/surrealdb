511821939f1a8960dad7a4ad61eff12dd4aa76b0
Stu Schwartz <stu.schwartz@surrealdb.com>
2025-10-13 12:43:06 +0100

    Address feedback

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -72,7 +72,8 @@ pub(crate) mod types {
 		Number as PublicNumber, Object as PublicObject, Range as PublicRange,
 		RecordId as PublicRecordId, RecordIdKey as PublicRecordIdKey,
 		RecordIdKeyRange as PublicRecordIdKeyRange, Regex as PublicRegex, SurrealValue,
-		Uuid as PublicUuid, Value as PublicValue, Variables as PublicVariables,
+		Table as PublicTable, Uuid as PublicUuid, Value as PublicValue,
+		Variables as PublicVariables,
 	};
 }
 
