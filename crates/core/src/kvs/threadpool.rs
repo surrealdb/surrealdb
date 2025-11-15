b611933eff903695da5e04de8982f45425ac9577
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-11-15 21:37:38 +0000

    Use CPU affinity for threadpool

--- crates/core/src/kvs/threadpool.rs
+++ crates/core/src/kvs/threadpool.rs
@@ -2,13 +2,10 @@
 
 /// Create a new blocking threadpool
 pub(super) fn initialise() {
-	// Get a sensible number of worker threads
-	let size = std::cmp::max(8, num_cpus::get());
 	// Create the threadpool and ignore errors
 	let _ = affinitypool::Builder::new()
 		.thread_name("surrealdb-threadpool")
-		.thread_per_core(false)
-		.worker_threads(size)
+		.thread_per_core(true)
 		.build()
 		.build_global();
 }
