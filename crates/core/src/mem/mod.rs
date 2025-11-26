e2097e4a280ec632ca8f62b09806616d7573909c
emin <git@emin.sh>
2025-11-26 13:17:08 -0600

    use jemalloc only on x86_64 and x86

--- crates/core/src/mem/mod.rs
+++ crates/core/src/mem/mod.rs
@@ -5,28 +5,40 @@ mod track;
 pub static ALLOC: fake::FakeAlloc = fake::FakeAlloc::new();
 
 #[cfg(feature = "allocator")]
-#[cfg(not(any(
-	target_os = "android",
-	target_os = "freebsd",
-	target_os = "ios",
-	target_os = "linux",
-	target_os = "macos",
-	target_os = "netbsd",
-	target_os = "openbsd"
+#[cfg(not(all(
+	any(
+		target_os = "android",
+		target_os = "freebsd",
+		target_os = "ios",
+		target_os = "linux",
+		target_os = "macos",
+		target_os = "netbsd",
+		target_os = "openbsd"
+	),
+	any(
+		target_arch = "x86_64",
+		target_arch = "x86"
+	)
 )))]
 #[global_allocator]
 pub static ALLOC: track::TrackAlloc<std::alloc::System> =
 	track::TrackAlloc::new(std::alloc::System);
 
 #[cfg(feature = "allocator")]
-#[cfg(any(
-	target_os = "android",
-	target_os = "freebsd",
-	target_os = "ios",
-	target_os = "linux",
-	target_os = "macos",
-	target_os = "netbsd",
-	target_os = "openbsd"
+#[cfg(all(
+	any(
+		target_os = "android",
+		target_os = "freebsd",
+		target_os = "ios",
+		target_os = "linux",
+		target_os = "macos",
+		target_os = "netbsd",
+		target_os = "openbsd"
+	),
+	any(
+		target_arch = "x86_64",
+		target_arch = "x86"
+	)
 ))]
 #[global_allocator]
 pub static ALLOC: track::TrackAlloc<jemallocator::Jemalloc> =
