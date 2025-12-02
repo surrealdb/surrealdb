5be48ed9861c1879c083348528db364d70e3e980
Micha de Vries <micha@devrie.sh>
2025-12-02 16:22:34 +0100

    missed a return...

--- crates/core/src/fmt/escape.rs
+++ crates/core/src/fmt/escape.rs
@@ -125,7 +125,7 @@ impl fmt::Display for EscapeRid<'_> {
 			|| s.contains(|x: char| !x.is_ascii_alphanumeric() && x != '_')
 			|| !s.contains(|x: char| !x.is_ascii_digit() && x != '_')
 		{
-			f.write_fmt(format_args!("`{}`", Escape::escape_str(s, '`')))?;
+			return f.write_fmt(format_args!("`{}`", Escape::escape_str(s, '`')));
 		}
 
 		f.write_str(s)
