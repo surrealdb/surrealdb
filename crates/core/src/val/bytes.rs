f247566af66cd65cf5da1d749d6247ed3a80eddd
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-12-09 15:37:46 +0000

    Remove `Serialize` and `Deserialize` trait implementation for `Value::Bytes`

--- crates/core/src/val/bytes.rs
+++ crates/core/src/val/bytes.rs
@@ -1,9 +1,6 @@
-use std::fmt;
 use std::ops::Deref;
 
 use revision::revisioned;
-use serde::de::{self, SeqAccess, Visitor};
-use serde::{Deserialize, Serialize};
 use storekey::{BorrowDecode, Encode};
 use surrealdb_types::{SqlFormat, ToSql, write_sql};
 
@@ -66,60 +63,6 @@ impl ToSql for Bytes {
 	}
 }
 
-impl Serialize for Bytes {
-	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-	where
-		S: serde::Serializer,
-	{
-		serializer.serialize_bytes(&self.0)
-	}
-}
-
-impl<'de> Deserialize<'de> for Bytes {
-	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-	where
-		D: serde::Deserializer<'de>,
-	{
-		struct RawBytesVisitor;
-
-		impl<'de> Visitor<'de> for RawBytesVisitor {
-			type Value = Bytes;
-
-			fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-				formatter.write_str("bytes or sequence of bytes")
-			}
-
-			fn visit_byte_buf<E>(self, v: Vec<u8>) -> Result<Self::Value, E>
-			where
-				E: de::Error,
-			{
-				Ok(Bytes(bytes::Bytes::from(v)))
-			}
-
-			fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E>
-			where
-				E: de::Error,
-			{
-				Ok(Bytes(bytes::Bytes::copy_from_slice(v)))
-			}
-
-			fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-			where
-				A: SeqAccess<'de>,
-			{
-				let capacity = seq.size_hint().unwrap_or_default();
-				let mut vec = Vec::with_capacity(capacity);
-				while let Some(byte) = seq.next_element()? {
-					vec.push(byte);
-				}
-				Ok(Bytes(bytes::Bytes::from(vec)))
-			}
-		}
-
-		deserializer.deserialize_byte_buf(RawBytesVisitor)
-	}
-}
-
 #[cfg(test)]
 mod tests {
 	use crate::val::{Bytes, Value};
@@ -130,14 +73,6 @@ mod tests {
 		let serialized: Vec<u8> = revision::to_vec(&val).unwrap();
 		println!("{serialized:?}");
 		let deserialized: Value = revision::from_slice(&serialized).unwrap();
-		assert_eq!(val, deserialized);
-	}
-
-	#[test]
-	fn json_roundtrip() {
-		let val = Bytes::from(vec![1, 2, 3, 5]);
-		let json = serde_json::to_string(&val).unwrap();
-		let deserialized = serde_json::from_str(&json).unwrap();
 		assert_eq!(val, deserialized);
 	}
 }
