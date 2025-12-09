78cb116e6308f1e13e74dba9d6e2bb4fc6e2f950
Dave MacLeod <56599343+Dhghomon@users.noreply.github.com>
2025-12-09 13:08:11 +0900

    Fix this test too

--- crates/core/tests/typing.rs
+++ crates/core/tests/typing.rs
@@ -77,7 +77,7 @@ async fn strict_typing_inline() -> Result<()> {
 				age: 18,
 				enabled: true,
 				name: 'Tobie Morgan Hitchcock',
-				scores: [1.0, 2.0, 3.0, 4.0, 5.0],
+				scores: {1.0, 2.0, 3.0, 4.0, 5.0},
 			}
 		]",
 	)
