fd3cbcba8583b1389c11c7aa713a7325a1494e51
Rushmore Mushambi <rushmore@surrealdb.com>
2026-01-28 22:42:47 +0000

    Update deps

--- docker/builder-entrypoint.sh
+++ docker/builder-entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Append linker flags to help find static libraries for ONNX Runtime
-if [ -n "$RUSTFLAGS" ]; then
-    export RUSTFLAGS="$RUSTFLAGS -C link-arg=-ldl -C link-arg=-lm -C link-arg=-lpthread -C link-arg=-lrt"
-else
-    export RUSTFLAGS="-C link-arg=-ldl -C link-arg=-lm -C link-arg=-lpthread -C link-arg=-lrt"
-fi
-
 exec cargo build "$@"
