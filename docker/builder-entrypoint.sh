6a7933555585218ca023d32d8f37cef9fdbf0bc9
Rushmore Mushambi <rushmore@surrealdb.com>
2026-01-29 10:03:35 +0000

    Add comprehensive linker support for ONNX Runtime
    Add explicit library search paths and additional linker flags to ensure ONNX Runtime static library can find all required system dependencies. Also add clang/clang-devel for bindgen support.
    
    Changes:
    - Add clang and clang-devel packages
    - Set LD_LIBRARY_PATH and LIBRARY_PATH for gcc-toolset libraries
    - Add -lrt and -latomic linker flags

--- docker/builder-entrypoint.sh
+++ docker/builder-entrypoint.sh
@@ -5,7 +5,11 @@
 # Add system pkg-config paths so openssl-sys can find OpenSSL
 export PKG_CONFIG_PATH="/usr/lib64/pkgconfig:/usr/share/pkgconfig:${PKG_CONFIG_PATH}"
 
+# Ensure library paths include both system and gcc-toolset libraries
+export LD_LIBRARY_PATH="/opt/rh/gcc-toolset-13/root/usr/lib64:/usr/lib64:${LD_LIBRARY_PATH}"
+export LIBRARY_PATH="/opt/rh/gcc-toolset-13/root/usr/lib64:/usr/lib64:${LIBRARY_PATH}"
+
 # Add linker flags for ONNX Runtime static library
-export RUSTFLAGS="$RUSTFLAGS -C link-arg=-lstdc++ -C link-arg=-lm -C link-arg=-ldl -C link-arg=-lpthread"
+export RUSTFLAGS="$RUSTFLAGS -C link-arg=-lstdc++ -C link-arg=-lm -C link-arg=-ldl -C link-arg=-lpthread -C link-arg=-lrt -C link-arg=-latomic"
 
 exec cargo build "$@"
