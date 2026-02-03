20becb5087acaa21c1f550bf85542b68a7c338ca
Rushmore Mushambi <rushmore@surrealdb.com>
2026-02-03 16:31:50 +0000

    Fix Linux Docker builder: OpenSSL and ONNX Runtime linking (#6826)

--- docker/builder-entrypoint.sh
+++ docker/builder-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
 
 . /opt/rh/gcc-toolset-13/enable
+
+# When ORT_DOWNLOAD_URL is set, download ONNX Runtime in-container (avoids host mount issues).
+if [[ -n "${ORT_DOWNLOAD_URL:-}" ]]; then
+  mkdir -p /tmp/onnxruntime-build
+  curl -sSL "$ORT_DOWNLOAD_URL" | tar -xz -C /tmp/onnxruntime-build
+  export ORT_STRATEGY=system ORT_LIB_LOCATION=/tmp/onnxruntime-build/lib
+fi
 
 exec cargo build "$@"
