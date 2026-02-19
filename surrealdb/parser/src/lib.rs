d828926322e7baafecde273193ffb361c69caaef
Mees Delzenne <mees.delzenne@gmail.com>
2026-02-19 18:19:46 +0100

    Implement more of the language

--- surrealdb/parser/src/lib.rs
+++ surrealdb/parser/src/lib.rs
@@ -1,3 +1,16 @@
+//! # Surrealdb parser
+//!
+//! This crate is the internal library of SurrealDB. It contains a implemention of a surrealql
+//! parser
+//!
+//! <section class="warning">
+//! <h3>Unstable!</h3>
+//! This crate is <b>SurrealDB internal API</b>. It does not adhere to SemVer and its API is
+//! free to change and break code even between patch versions. If you are looking for a stable
+//! interface to the SurrealDB library please have a look at
+//! <a href="https://crates.io/crates/surrealdb">the Rust SDK</a>.
+//! </section>
+
 #![allow(dead_code)]
 
 pub mod parse;
