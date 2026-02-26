d88446cbb1a9f4321353476cf9f1ab5b8033c69f
Emmanuel Keller <emmanuel.keller@surrealdb.com>
2026-02-26 11:22:43 -0300

    Set seed

--- surrealdb/core/tests/index.rs
+++ surrealdb/core/tests/index.rs
@@ -1,5 +1,6 @@
 mod helpers;
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::sync::Arc;
@@ -7,27 +8,37 @@ use std::time::Duration;
 
 use anyhow::Result;
 use flate2::read::GzDecoder;
+use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng, random};
 use surrealdb_core::dbs::Session;
 use surrealdb_core::kvs::Datastore;
 use surrealdb_types::{Array, RecordId, RecordIdKey, ToSql, Value};
 use tokio::time::timeout;
-use tracing::info;
+use tracing::{error, info};
 
 use crate::helpers::new_ds;
 
 async fn concurrent_tasks(
 	dbs: Arc<Datastore>,
 	session: &Session,
-	batch: Vec<&'static str>,
+	task_count: usize,
+	sql_func: impl Fn(usize) -> &'static str,
 ) -> Result<()> {
-	let mut tasks = Vec::with_capacity(batch.len());
+	let mut tasks = Vec::with_capacity(task_count);
 
-	for sql in batch {
+	for i in 0..task_count {
 		let dbs = dbs.clone();
 		let session = session.clone();
+		let sql = sql_func(i);
 		tasks.push(tokio::spawn(async move {
 			let mut res = dbs.execute(sql, &session, None).await?;
-			res.remove(0).result?;
+			// Ignore errors from aborted/cancelled transactions.
+			if let Err(e) = res.remove(0).result {
+				if !sql.contains("aborting transaction test") && !sql.ends_with("CANCEL;") {
+					error!("Concurrent task error: {sql} - {e}");
+					panic!("Concurrent task error: {sql} - {e}")
+				}
+			}
 			Ok::<(), anyhow::Error>(())
 		}));
 	}
@@ -38,76 +49,118 @@ async fn concurrent_tasks(
 	Ok(())
 }
 
-async fn batch_ingestion(dbs: Arc<Datastore>, ses: &Session, batch_count: usize) -> Result<()> {
-	info!("Inserting {batch_count} batches concurrently");
-	// Create records and commit.
-	let sql_create_commit = "CREATE |aaa:10| CONTENT {
-			field1: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
-			field2: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
-			field3: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
-			field4: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
-			field5: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
-		} RETURN NONE;";
-	// Create records and cancel
-	let sql_create_throw = "
-        BEGIN;
-            CREATE |aaa:10| CONTENT
-                { field1: '-', field2: '-', field3: '-', field4: '-', field5: '-' }
-                RETURN NONE;
-			THROW 'Ooch!';
-	    COMMIT;";
-	let sql_create_cancel = "
+const SQL_CREATE_COMMIT: &str = "
+		BEGIN;
+			CREATE |aaa:10| CONTENT {
+				field1: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+				field2: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+				field3: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+				field4: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+				field5: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+			} RETURN NONE;
+			if (rand() < 0.1) {
+				THROW 'aborting transaction test';
+			};
+		COMMIT;
+	";
+const SQL_CREATE_CANCEL: &str = "
         BEGIN;
             CREATE |aaa:10| CONTENT
                 { field1: '-', field2: '-', field3: '-', field4: '-', field5: '-' }
                 RETURN NONE;
 	    CANCEL;";
-	let task_sequence = [
-		// 10 normal committed transaction
-		sql_create_commit,
-		sql_create_commit,
-		sql_create_commit,
-		sql_create_commit,
-		sql_create_commit,
-		sql_create_commit,
-		sql_create_commit,
-		sql_create_commit,
-		sql_create_commit,
-		sql_create_commit,
-		// 5 explicitely thrown transactions
-		sql_create_throw,
-		sql_create_throw,
-		sql_create_throw,
-		sql_create_throw,
-		sql_create_throw,
-		// 5 explicitely cancelled transactions
-		sql_create_cancel,
-		sql_create_cancel,
-		sql_create_cancel,
-		sql_create_cancel,
-		sql_create_cancel,
-	];
-	let mut batch = Vec::new();
+const SQL_UPDATE_COMMIT: &str = "
+        BEGIN;
+			LET $before = (SELECT * FROM (SELECT * FROM aaa LIMIT 10000) ORDER rand() LIMIT 10);
+			LET $ret = [
+				$before,
+				(UPDATE $before.id CONTENT {
+					field1: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+					field2: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+					field3: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+					field4: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+					field5: rand::enum(['cupcake', 'cakecup', 'cheese', 'pie', 'noms']),
+				}),
+			];
+			if (rand() < 0.1) {
+				THROW 'aborting transaction test';
+			};
+		COMMIT;";
+const SQL_DELETE_COMMIT: &str = "
+		BEGIN;
+			LET $before = (SELECT * FROM (SELECT * FROM aaa LIMIT 10000) ORDER rand() LIMIT 10);
+			LET $ret = [
+				(DELETE $before.id RETURN before),
+				[],
+			];
+			if (rand() < 0.1) {
+				THROW 'aborting transaction test';
+			};
+		COMMIT;";
+
+type SqlDistribution = [(f32, &'static str)];
+
+async fn batch_ingestion(
+	dbs: Arc<Datastore>,
+	ses: &Session,
+	batch_count: usize,
+	sql_distribution: &SqlDistribution,
+) -> Result<()> {
+	info!("Inserting {batch_count} batches concurrently");
+
+	let seed: u64 = std::env::var("SURREALDB_TEST_SEED")
+		.ok()
+		.and_then(|s| s.parse().ok())
+		.unwrap_or_else(random);
+	info!("Using random seed: {seed}");
+	let mut rng = SmallRng::seed_from_u64(seed);
+	let mut batch = Vec::with_capacity(batch_count);
+	let mut map = HashMap::with_capacity(batch_count);
 	for _ in 0..batch_count {
-		batch.extend(task_sequence);
+		let action = rng.r#gen::<f32>();
+		for (threshold, sql) in sql_distribution {
+			if action <= *threshold {
+				batch.push(*sql);
+				let count: &mut usize = map.entry(*sql).or_default();
+				*count += 1;
+				break;
+			}
+		}
 	}
-	concurrent_tasks(dbs.clone(), ses, batch).await
+	assert_eq!(
+		map.len(),
+		sql_distribution.len(),
+		"SQL distribution map should have same length as distribution"
+	);
+	concurrent_tasks(dbs.clone(), ses, batch.len(), |i| batch[i]).await
 }
 
-#[tokio::test(flavor = "multi_thread")]
+#[tokio::test(flavor = "multi_thread", worker_threads = 10)]
 #[test_log::test]
 // Regression test for https://github.com/surrealdb/surrealdb/issues/6837
-async fn multi_index_concurrent_test() -> Result<()> {
-	let dbs = Arc::new(new_ds("test", "test").await?);
-	let ses = Session::owner().with_ns("test").with_db("test");
+async fn multi_index_concurrent_test_create_update_delete() -> Result<()> {
+	#[cfg(not(debug_assertions))]
+	let count = 1000;
+	#[cfg(debug_assertions)]
+	let count = 200;
+	multi_index_concurrent_test(
+		count,
+		&[
+			(0.33, SQL_CREATE_COMMIT),
+			(0.40, SQL_CREATE_CANCEL),
+			(0.70, SQL_UPDATE_COMMIT),
+			(1.0, SQL_DELETE_COMMIT),
+		],
+	)
+	.await
+}
 
-	// Define the table.
-	dbs.execute("DEFINE TABLE aaa", &ses, None).await?;
-
-	// Ingest 500 records (including cancellation transaction)
-	batch_ingestion(dbs.clone(), &ses, 5).await?;
-
+async fn multi_index_concurrent_test(
+	batch_count: usize,
+	sql_distribution: &SqlDistribution,
+) -> Result<()> {
 	let sql = ";
+	DEFINE TABLE aaa;
 	DEFINE ANALYZER simple TOKENIZERS blank FILTERS lowercase, ascii, edgengram(1, 10);
 	DEFINE INDEX field1 ON aaa FIELDS field1 FULLTEXT ANALYZER simple BM25 HIGHLIGHTS CONCURRENTLY;
 	DEFINE INDEX field2 ON aaa FIELDS field2 FULLTEXT ANALYZER simple BM25 HIGHLIGHTS CONCURRENTLY;
@@ -115,16 +168,16 @@ async fn multi_index_concurrent_test() -> Result<()> {
 	DEFINE INDEX field4 ON aaa FIELDS field4 FULLTEXT ANALYZER simple BM25 HIGHLIGHTS CONCURRENTLY;
 	DEFINE INDEX field5 ON aaa FIELDS field5 FULLTEXT ANALYZER simple BM25 HIGHLIGHTS CONCURRENTLY;
 ";
+	let dbs = Arc::new(new_ds("test", "test").await?);
+	let ses = Session::owner().with_ns("test").with_db("test");
 	// Define analyzer and indexes.
 	dbs.execute(sql, &ses, None).await?;
 
-	// Ingest another 5 records (including cancellation transaction)
-	batch_ingestion(dbs.clone(), &ses, 5).await?;
-
-	let expected_total_count = 1000;
+	// Ingest records.
+	batch_ingestion(dbs.clone(), &ses, batch_count, sql_distribution).await?;
 
 	info!("Waiting for index to be built");
-	timeout(Duration::from_secs(30), async {
+	timeout(Duration::from_secs(600), async {
         loop {
             let res = &mut dbs
                 .execute(
@@ -146,8 +199,12 @@ async fn multi_index_concurrent_test() -> Result<()> {
                 panic!("Invalid result format: {}", building.to_sql_pretty())
             };
             let status = building.get("status").unwrap();
-            let Value::String(status) =  status else {
+            let Value::String(status) = status else {
                 panic!("Invalid result format: {}", status.to_sql_pretty())
+            };
+            let pending = building.get("pending").unwrap();
+            let Value::Number(pending) = pending else {
+                panic!("Invalid pending format: {}", building.to_sql_pretty())
             };
             // Ensure the status is valid (no error).
             if status != "ready" && status != "indexing" && status != "cleaning" {
@@ -175,12 +232,16 @@ async fn multi_index_concurrent_test() -> Result<()> {
                 };
                 real_total_count += count.into_int()?;
             }
-            if real_total_count == expected_total_count {
-                info!("Real count: {real_total_count} - Index: count: {index_count} - Index status: {status}");
-                if index_count.into_int()? == expected_total_count {
-                    // Success.
-                    break;
+            info!("Real count: {real_total_count} - Index: count: {index_count} - Index status: {status} - Pending: {pending}");
+            if index_count.into_int()? == real_total_count {
+                if status != "ready" {
+                    panic!("Invalid index status: {}", status.to_sql_pretty())
                 }
+                if pending.into_int()? != 0 {
+                    panic!("Invalid pending number: {}", pending.to_sql_pretty())
+                }
+                // Success.
+                break;
             }
             // Wait before retrying.
             tokio::time::sleep(Duration::from_millis(1000)).await;
@@ -245,11 +306,11 @@ async fn hnsw_concurrent_writes() -> Result<()> {
 
 	// Define the table and the index.
 	dbs.execute(
-		"DEFINE TABLE t; DEFINE INDEX ix ON t FIELDS v HNSW DIMENSION 20 DIST EUCLIDEAN TYPE F32 EFC 150 M 8; ",
-		&session,
-		None,
-	)
-	.await?;
+        "DEFINE TABLE t; DEFINE INDEX ix ON t FIELDS v HNSW DIMENSION 20 DIST EUCLIDEAN TYPE F32 EFC 150 M 8; ",
+        &session,
+        None,
+    )
+        .await?;
 
 	let vectors = new_vectors_from_file(INGESTING_SOURCE, 1000)?;
 	let chunks: Vec<_> = vectors.chunks(vectors.len() / 4).map(|s| s.to_vec()).collect();
