f1bb535aad38e8cb55b84cbb9c0849e72270a50f
Dave MacLeod <56599343+Dhghomon@users.noreply.github.com>
2026-03-16 11:42:26 +0900

    fmt

--- surrealdb/types/src/sql.rs
+++ surrealdb/types/src/sql.rs
@@ -245,10 +245,10 @@ impl ToSql for rust_decimal::Decimal {
 
 #[doc(hidden)]
 pub const fn fmt_non_finite_f64(v: f64) -> Option<&'static str> {
-    match v {
-        f64::INFINITY => Some("Infinity"),
-        f64::NEG_INFINITY => Some("-Infinity"),
-        float if float.is_nan() => Some("NaN"),
-        _ => None,
-    }
+	match v {
+		f64::INFINITY => Some("Infinity"),
+		f64::NEG_INFINITY => Some("-Infinity"),
+		float if float.is_nan() => Some("NaN"),
+		_ => None,
+	}
 }
